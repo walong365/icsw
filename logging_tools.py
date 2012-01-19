8a103aa9f742087321b4226e9c1f2ae7934a5e58
Andreas Lang-Nevyjel <lang@init.at>
2012-01-19 07:46:23 +0000

    fixed bug in samplefile, testbuild
    git-svn-id: svn+ssh://repository/svn/clustersoftware@15 44af9490-3b78-11e1-98d3-d193acd27c9c

--- logging_tools.py
+++ logging_tools.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python-init -Ot
 #
-# Copyright (C) 2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011 Andreas Lang-Nevyjel
+# Copyright (C) 2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012 Andreas Lang-Nevyjel
 #
 # this file is part of python-modules-base
 #
