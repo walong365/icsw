ff61876463d8ea3193031ac6c7de76e3e639d78f
Andreas Lang-Nevyjel <lang@init.at>
2012-02-01 10:14:31 +0000

    updated basesystem
    git-svn-id: svn+ssh://repository/svn/clustersoftware@31 44af9490-3b78-11e1-98d3-d193acd27c9c

--- meta-server.py
+++ meta-server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python-init -Ot
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2001,2002,2003,2004,2005,2006,2007,2008,2010,2011 Andreas Lang-Nevyjel
+# Copyright (C) 2001,2002,2003,2004,2005,2006,2007,2008,2010,2011,2012 Andreas Lang-Nevyjel
 #
 # Send feedback to: <lang-nevyjel@init.at>
 #
