894ac95d06dbc87de593e9a22df95e4eb29c2318
Andreas Lang-Nevyjel <lang@init.at>
2012-03-22 15:44:34 +0000

    fixes for special_commands
    git-svn-id: svn+ssh://repository/svn/clustersoftware@98 44af9490-3b78-11e1-98d3-d193acd27c9c

--- special_commands.py
+++ special_commands.py
@@ -75,13 +75,24 @@ class special_base(object):
         self.build_process.mach_log("[sc] %s" % (what), log_level)
     def _call_collrelay(self, command, *args):
         return self._call_server(command, "collrelay", *args)
-    def _call_server(self, command, server_name, *args):
-        self.log("calling server '%s', command is '%s', args is '%s'" % (
+    def _call_snmprelay(self, command, *args, **kwargs):
+        return self._call_server(command, "snmp_relay", *args, snmp_community=kwargs.get("snmp_community", "public"), snmp_version=kwargs.get("snmp_version", 1))
+    def _call_server(self, command, server_name, *args, **kwargs):
+        self.log("calling server '%s', command is '%s', %s, %s" % (
             server_name,
             command,
-            ", ".join([str(value) for value in args]) if args else "NO ARGS"))
+            "args is '%s'" % (", ".join([str(value) for value in args])) if args else "no arguments",
+            ", ".join(["%s='%s'" % (key, value) for key, value in kwargs.iteritems()]) if kwargs else "no kwargs"
+        ))
         try:
-            srv_reply = ipc_comtools.send_and_receive_zmq(self.valid_ip, command, *args, server="collrelay", zmq_context=self.build_process.zmq_context, port=2001)
+            srv_reply = ipc_comtools.send_and_receive_zmq(
+                self.valid_ip,
+                command,
+                *args,
+                server=server_name,
+                zmq_context=self.build_process.zmq_context,
+                port=2001,
+                **kwargs)
         except:
             self.log("error connecting to '%s' (%s, %s): %s" % (
                 server_name,
@@ -102,7 +113,30 @@ class special_base(object):
 
 class special_openvpn(special_base):
     def _call(self):
-        return []
+        sc_array = []
+        exp_dict = parse_expected()
+        if exp_dict.has_key(self.host["name"]):
+            exp_dict = {}#exp_dict[host["name"]]
+        else:
+            exp_dict = {}
+        if not exp_dict:
+            # no expected_dict found, try to get the actual config from the server
+            srv_result = self._call_collrelay("openvpn_status")
+            if srv_result is not None:
+                if "openvpn_instances" in srv_result:
+                    ovpn_dict = server_command.srv_command.tree_to_dict(srv_result["openvpn_instances"])
+                    # build exp_dict
+                    for inst_name in ovpn_dict:
+                        if ovpn_dict[inst_name]["type"] == "server":
+                            for c_name in ovpn_dict[inst_name]["dict"]:
+                                exp_dict.setdefault(inst_name, {})[c_name] = True
+        if exp_dict:
+            for inst_name in sorted(exp_dict):
+                for peer_name in sorted(exp_dict[inst_name]):
+                    sc_array.append(("OpenVPN peer %s on %s" % (peer_name, inst_name), [inst_name, peer_name]))
+        if not sc_array:
+            sc_array.append(("OpenVPN", ["ALL", "ALL"]))
+        return sc_array
     
 class special_disc_all(special_base):
     def _call(self):
@@ -230,58 +264,77 @@ class special_net(special_base):
             sc_array.append((name_with_descr, eth_opts))
         return sc_array
 
-def handle(s_check, host, dc, build_proc, valid_ip, **kwargs):
-    exp_dict = parse_expected()
-    if exp_dict.has_key(host["name"]):
-        exp_dict = exp_dict[host["name"]]
-    else:
-        exp_dict = {}
-    if not exp_dict:
-        # no expected_dict found, try to get the actual config from the server
-        srv_result = ipc_comtools.send_and_receive_zmq(valid_ip, "openvpn_status", server="collrelay", zmq_context=build_proc.zmq_context, port=2001)
-        print unicode(srv_result)
-        print "*" * 20
-        try:
-            res_ok, res_dict = ipc_comtools.send_and_receive(valid_ip, "openvpn_status", target_port=2001, decode=True)
-        except:
-            print "nogo"
-            build_proc.mach_log("error getting open_status from %s: %s" % (valid_ip,
-                                                                           process_tools.get_except_info()),
-                                logging_tools.LOG_LEVEL_CRITICAL)
-        else:
-            print "go"
-            if res_ok:
-                build_proc.mach_log("error calling openvpn_status: %s" % (str(res_dict)),
-                                    logging_tools.LOG_LEVEL_ERROR)
-            else:
-                # build exp_dict
-                for inst_name in res_dict:
-                    if res_dict[inst_name]["type"] == "server":
-                        for c_name in res_dict[inst_name]["dict"]:
-                            exp_dict.setdefault(inst_name, {})[c_name] = True
-    sc_array = []
-    for inst_name in sorted(exp_dict):
-        for peer_name in sorted(exp_dict[inst_name]):
-            sc_array.append(("OpenVPN peer %s on %s" % (peer_name, inst_name), [inst_name, peer_name]))
-    if not sc_array:
-        sc_array.append(("OpenVPN", ["ALL", "ALL"]))
-    return sc_array
-
 class special_libvirt(special_base):
     def _call(self):
-        act_com = "domain_overview"
         sc_array = []
-        srv_result = self._call_collrelay(act_com)
+        srv_result = self._call_collrelay("domain_overview")
         if srv_result is not None:
-            print unicode(srv_result)
-            if False:
-                if "running" in res_dict and "defined" in res_dict:
-                    res_dict = res_dict["running"]
+            if "domain_overview" in srv_result:
+                domain_info = server_command.srv_command.tree_to_dict(srv_result["domain_overview"])
+                if "running" in domain_info and "defined" in domain_info:
+                    domain_info = domain_info["running"]
                 # build sc_array
-                for inst_id in res_dict:
-                    d_dict = res_dict[inst_id]
+                for inst_id in domain_info:
+                    d_dict = domain_info[inst_id]
                     sc_array.append(("Domain %s" % (d_dict["name"]),
                                      [d_dict["name"]]))
+        return sc_array
+        
+class special_eonstor(special_base):
+    def _call(self):
+        sc_array = []
+        srv_reply = self._call_snmprelay("eonstor_get_counter",
+                                         snmp_community="public",
+                                         snmp_version="1")
+        if srv_reply and "eonstor_info" in srv_reply:
+            info_dict = server_command.srv_command.tree_to_dict(srv_reply.xpath(None, ".//ns:eonstor_info")[0])
+            # disks
+            for disk_id in sorted(info_dict.get("disc_ids", [])):
+                sc_array.append(("Disc %2d" % (disk_id), ["eonstor_disc_info", "%d" % (disk_id)]))
+            # lds
+            for ld_id in sorted(info_dict.get("ld_ids", [])):
+                sc_array.append(("LD %2d" % (ld_id), ["eonstor_ld_info", "%d" % (ld_id)]))
+            # env_dicts
+            for env_dict_name in sorted(info_dict.get("ent_dict", {}).keys()):
+                env_dict = info_dict["ent_dict"][env_dict_name]
+                for idx in sorted(env_dict.keys()):
+                    nag_name = env_dict[idx]
+                    add_check = True
+                    # get info for certain environment types
+                    if env_dict_name in ["ups", "bbu"]:
+                        act_com = "eonstor_%s_info" % (env_dict_name)
+                        srv_reply = self._call_snmprelay(act_com,
+                                                         "%d" % (idx),
+                                                         snmp_version="1",
+                                                         snmp_community="public")
+                        if srv_reply and "eonstor_info:state" in srv_reply:
+                            act_state = int(srv_reply["eonstor_info:state"].text)
+                            self.log("state for %s:%d is %d" % (act_com, idx, act_state))
+                            if act_state:
+                                self.log("error command %s gave (%d): %s" % (act_com,
+                                                                             act_state,
+                                                                             state_obj),
+                                         logging_tools.LOG_LEVEL_ERROR)
+                            else:
+                                if env_dict_name == "ups":
+                                    # check for inactive psus
+                                    if state_obj.state & 128:
+                                        self.log("disabling psu with idx %d because not present" % (idx),
+                                                 logging_tools.LOG_LEVEL_ERROR)
+                                        add_check = False
+                                elif env_dict_name == "bbu":
+                                    if state_obj.state & 128:
+                                        self.log("disabling bbu with idx %d because not present" % (idx),
+                                                 logging_tools.LOG_LEVEL_ERROR)
+                                        add_check = False
+                    if add_check:
+                        if not nag_name.lower().startswith(env_dict_name):
+                            nag_name = "%s %s" % (env_dict_name, nag_name)
+                        sc_array.append((nag_name,
+                                         ["eonstor_%s_info" % (env_dict_name), "%d" % (idx)]))
+        # rewrite sc_array to include community and version
+        sc_array = [(name, ["", ""] + var_list) for name, var_list in sc_array]
+        self.log("sc_array has %s" % (logging_tools.get_plural("entry", len(sc_array))))
         return sc_array
         
 if __name__ == "__main__":
