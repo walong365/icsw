3d33a6a4de4f6ff83bce36572302ef15a3cc299e
Andreas Lang-Nevyjel <lang@init.at>
2012-04-14 09:59:17 +0000

    moved webfrontend to build-extern
    git-svn-id: svn+ssh://repository/svn/clustersoftware@163 44af9490-3b78-11e1-98d3-d193acd27c9c

--- compile_httpd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apache_version=2.0.58
-
-cd httpd-${apache_version}
-
-./configure --prefix=/opt/apache-init --enable-v4-mapped --enable-ssl --enable-static-support --enable-shared-support --enable-so --enable-mods-shared --enable-http
-
-make
--- htdocs/php/3waredummy.php
+++
@@ -1,3 +0,0 @@
-<?php
-//-*ics*- ,CAP,name:'twa',enabled:1,defvalue:0,descr:'3ware access'
->
