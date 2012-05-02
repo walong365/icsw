400b00ee31981006d22023bc51eca8c92589b706
Christoph Sieghart <sieghart@init.at>
2012-05-02 12:28:56 +0200

    Media files added via os.walk

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python-init 
 
+import os
+import re
 from distutils.core import setup
+
+# Create list of media files
+media_files = []
+strip_leading = re.compile("src/initcore/")
+for root, dirs, files in os.walk("src/initcore/media"):
+    for i in files:
+        stripped_root = re.sub(strip_leading, "", root)
+        media_files.append(os.path.join(stripped_root, i))
 
 setup(name="initcore",
       version="1.0.0",
-      package_dir={"": "src"},
+      package_dir={"initcore": "src/initcore"},
       packages=["initcore", "initcore.management", "initcore.management.commands",
           "initcore.templatetags"],
+      package_data={"initcore": ["templates/*.html", "templates/initcore/*.html",
+      "templates/initcore/*.css"] +  media_files}
       )
