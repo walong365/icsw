818ca037e92a61544a7303104657fdc3dde15411
Andreas Lang-Nevyjel <lang@init.at>
2012-06-01 21:05:50 +0000

    added django files
    git-svn-id: svn+ssh://repository/svn/clustersoftware@268 44af9490-3b78-11e1-98d3-d193acd27c9c

---
+++ django/wf_urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+import sys
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+sys.path.append("/usr/local/share/home/local/development/clustersoftware/build-extern/cluster-backbone-sql/cluster/")
+
+transfer_patterns = patterns(
+    "backbone.transfer",
+    url(r"(.*)/", "views.transfer", name="transfer")
+)
+
+my_url_patterns = patterns(
+    "",
+    url(r"static/(?P<path>.*)$", "django.views.static.serve", {"document_root" : settings.MEDIA_ROOT}),
+    url(r"^", include(transfer_patterns, namespace="transfer"))
+)
+
+urlpatterns = patterns(
+    "",
+    # hack for icons
+    url(r"icons-init/(?P<path>.*)$", "django.views.static.serve", {"document_root" : settings.MEDIA_ROOT[:-14] + "/icons"}),
+    url(r"^%s/" % (settings.REL_SITE_ROOT), include(my_url_patterns)),
+)
+
