097ee666af07ba250ebbddcf5077e0936c6d9659
Andreas Lang-Nevyjel <lang@init.at>
2012-06-02 09:23:51 +0000

    updated urls.py, fixed 'truncated DOUBLE' error
    git-svn-id: svn+ssh://repository/svn/clustersoftware@273 44af9490-3b78-11e1-98d3-d193acd27c9c

--- init/cluster/urls.py
+++ init/cluster/urls.py
@@ -10,22 +10,3 @@ import init.cluster.sub_urls
 
 urlpatterns = init.cluster.sub_urls.sub_patterns
 
-print urlpatterns
-
-##transfer_patterns = patterns(
-##    "backbone.transfer",
-##    url(r"(.*)/", "views.transfer", name="transfer")
-##)
-##
-##my_url_patterns = patterns(
-##    "",
-##    url(r"static/(?P<path>.*)$", "django.views.static.serve", {"document_root" : settings.MEDIA_ROOT}),
-##    url(r"^", include(transfer_patterns, namespace="transfer"))
-##)
-##
-##urlpatterns = patterns(
-##    "",
-##    # hack for icons
-##    url(r"icons-init/(?P<path>.*)$", "django.views.static.serve", {"document_root" : settings.MEDIA_ROOT[:-14] + "/icons"}),
-##    url(r"^%s/" % (settings.REL_SITE_ROOT), include(my_url_patterns)),
-##)
