e16cf550f271881dd052df9829a0f7727a408a03
Christoph Sieghart <sieghart@init.at>
2012-06-28 08:53:10 +0200

    Moved media to static, bumped version.

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ import re
 from distutils.core import setup
 
 # Create list of media files
-media_files = []
+static_files = []
 strip_leading = re.compile("src/initcore/")
-for root, dirs, files in os.walk("src/initcore/media"):
+for root, dirs, files in os.walk("src/initcore/static"):
     for i in files:
         stripped_root = re.sub(strip_leading, "", root)
-        media_files.append(os.path.join(stripped_root, i))
+        static_files.append(os.path.join(stripped_root, i))
 
 setup(name="initcore",
-      version="1.0.4",
+      version="1.0.5",
       package_dir={"initcore": "src/initcore"},
       packages=["initcore", "initcore.templatetags"],
       package_data={"initcore": ["templates/*.html", "templates/initcore/*.html",
-      "templates/initcore/*.css"] +  media_files}
+      "templates/initcore/*.css"] +  static_files}
       )
