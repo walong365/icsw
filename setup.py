78975c6814550d5d3c17364bff2f76af4f90aaf2
Christoph Sieghart <sieghart@init.at>
2012-07-10 11:32:48 +0000

    New build system for cbc-tools.
    git-svn-id: svn+ssh://repository/svn/clustersoftware@401 44af9490-3b78-11e1-98d3-d193acd27c9c

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name="cbc-tools",
+      version="0.1",
+      description="cbc tools from the INIT(c) cluster software",
+      url="http://www.init.at",
+      author="Andreas Lang-Nevyjel",
+      author_email="lang@init.at",
+      py_modules=["compile_tools"],
+      scripts=["compile_libgoto.py", "compile_openmpi.py", "compile_hpl.py",
+               "compile_fftw.py", "read_bonnie.py", "bonnie.py", "n_from_mem.py",
+               "read_hpl_result.py"],
+      )
