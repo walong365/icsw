a8dac533683b2174fdfc7142fd24f6732f5cbb0e
Andreas Lang-Nevyjel <lang@init.at>
2012-07-17 05:45:21 +0000

    added missing file
    git-svn-id: svn+ssh://repository/svn/clustersoftware@501 44af9490-3b78-11e1-98d3-d193acd27c9c

---
+++ init/cluster/backbone/check_isolation_level.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python-init -Otu
+
+import os
+import sys
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "init.cluster.settings")
+
+from django.core.exceptions import ImproperlyConfigured
+
+def main():
+    # check isolation level
+    TARGET_IL = "READ-COMMITTED"
+    from django.db import connections
+    cursor = connections["default"].cursor()
+    cursor.execute("SELECT @@global.tx_isolation")
+    global_tx = cursor.fetchone()[0]
+    cursor.execute("SELECT @@tx_isolation")
+    local_tx = cursor.fetchone()[0]
+    if (global_tx, local_tx) != (TARGET_IL, TARGET_IL):
+        raise ImproperlyConfigured("wrong transaction-isolation level used (found: %s, need: %s)" % (global_tx, TARGET_IL))
+    
+if __name__ == "__main__":
+    main()
+    
