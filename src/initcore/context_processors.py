ce50a21c26673addbd41dddd7c91ed45ab95b4b9
Christoph Sieghart <sieghart@init.at>
2012-08-02 10:49:21 +0200

    Removed unused templates and themes. Moved the js to sane directories. HTML5 Boilerplate.

--- src/initcore/context_processors.py
+++ src/initcore/context_processors.py
@@ -1,18 +1,9 @@
 """ contex processor """
 from django.conf import settings
-from edmdb.models import OlimUser
 
 
 def add_session(request):
-    return {"session" : request.session}
-
-
-def add_svn_version(request):
-    return {"PROJECT_VERSION" : settings.PROJECT_VERSION}
-
-
-def add_project_name(request):
-    return {"PROJECT_NAME": settings.PROJECT_NAME}
+    return {"session": request.session}
 
 
 def add_settings(request):
