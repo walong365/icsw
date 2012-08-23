03b569cff19dfabea8dcc530465d82024b527d93
Christoph Sieghart <sieghart@init.at>
2012-08-23 11:59:18 +0200

    Removed convenience imports

--- src/initcore/alfresco/__init__.py
+++ src/initcore/alfresco/__init__.py
@@ -1,8 +1,3 @@
 import logging
 
-# Import some objects for convenience
-from initcore.alfresco.alfreso import AlfrescoHandler
-from initcore.alfresco.storage import AlfrescoStorage
-from initcore.alfresco.fields import AlfrescoFileField
-
 logger = logging.getLogger(__name__)
