25135372602e9e5ba93cac907f749aab1ae68924
Christoph Sieghart <sieghart@init.at>
2012-10-15 13:32:25 +0200

    Fixed version info

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ for root, dirs, files in os.walk("src/initcore/static"):
         static_files.append(os.path.join(stripped_root, i))
 
 setup(name="initcore",
-      version="1.1.0",
+      version="1.1.1",
       package_dir={"initcore": "src/initcore"},
       packages=["initcore", "initcore.templatetags", "initcore.alfresco",
                 "initcore.management.commands"],
