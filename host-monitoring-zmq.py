05ddd6f5b4f66529783b8878322a10d56987e45d
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2012-11-26 07:39:02 +0100

    force rebuild

--- host-monitoring-zmq.py
+++ host-monitoring-zmq.py
@@ -20,6 +20,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+
 """ host-monitoring, with 0MQ and twisted support """
 
 from twisted.internet import reactor
