cc1175f76c859eb90e46ea4c03d70ea90e976dbb
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2012-11-30 18:11:29 +0100

    small changes

--- logging-server.py
+++ logging-server.py
@@ -20,7 +20,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-""" logging server """
+""" logging server, central logging facility """
 
 from twisted.internet import reactor
 from twisted.internet.protocol import Protocol, DatagramProtocol
