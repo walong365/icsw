ee9167f362765a72cbf66025ae6d099eb53a2b54
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-02-08 20:31:26 +0100

    rewrote send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -19,84 +19,60 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+""" small tool for sending mails via commandline """
 
 import sys
 import argparse
-import process_tools
 import mail_tools
-import logging_tools
 
 def main():
     my_parser = argparse.ArgumentParser()
-    my_parser.add_argument("f", type=str, meta="from", help="from address [%(default)s]", default="root@localhost")
-    print my_parse.parse_args()
-    try:
-        args, opts = getopt.getopt(sys.argv[1:], "hf:t:s:m:G", ["help"])
-    except:
-        print "Error parsing options %s: %s" % (" ".join(sys.argv[1:]), process_tools.get_except_info())
-        sys.exit(2)
-    from_addr, subject, mailserver, to_addrs, group_mail = ("root@localhost", "unknown", "localhost", [], [])
-    for arg, opt in args:
-        if arg in ["-h", "--help"]:
-            print "Usage: %s [ -h|--help] [ -f FROMADDR ] [ -s SUBJECT ] [ -m MAILSERVER ] [ -t TOADDR|-G ] MESSAGE" % (sys.argv[0])
-            print "  default for FROMADDR is %s" % (from_addr)
-            print "  default for SUBJECT is %s" % (subject)
-            print "  default for MAILSERVER is %s" % (mailserver)
-            print "  -G sends mail to all valid users found in Database"
-            sys.exit(0)
-        if arg == "-f":
-            from_addr = opt
-        if arg == "-t":
-            to_addrs = [x.strip() for x in opt.split(",")]
-        if arg == "-s":
-            subject = opt
-        if arg == "-m":
-            mailserver = opt
-        if arg == "-G":
-            try:
-                import mysql_tools
-                import MySQLdb
-            except ImportError:
-                print "No mysql_tools found, exiting ..."
-                sys.exit(1)
-            else:
-                db_con = mysql_tools.dbcon_container(with_logging=False)
-                try:
-                    dc = db_con.get_connection("cluster_full_access")
-                except:
-                    print "Cannot connect to SQL-Server (%s)" % (process_tools.get_except_info())
-                    sys.exit(1)
-                dc.execute("SELECT u.login,u.useremail FROM user u WHERE u.useremail LIKE('%@%')")
-                user_dict = dict([(x["login"], x["useremail"]) for x in dc.fetchall()])
-                dc.release()
-                if user_dict:
-                    user_list = sorted(user_dict.keys())
-                    print "Sending mail to %s: %s" % (logging_tools.get_plural("user", len(user_list)),
-                                                      ", ".join(user_list))
-                    to_addrs = user_dict.values()
-                else:
-                    print "No users found, exiting ..."
-                    sys.exit(0)
-    if not opts:
-        try:
-            stdin_str = sys.stdin.read()
-        except:
-            stdin_str = ""
-    else:
-        stdin_str = ""
-    message = ("%s %s" % ((" ".join(opts)).replace("\\n", "\n"), stdin_str)).strip()
-    if not to_addrs:
+    my_parser.add_argument("-f", "--from", type=str, help="from address [%(default)s]", default="root@localhost")
+    my_parser.add_argument("-s", "--subject", type=str, help="subject [%(default)s]", default="mailsubject")
+    my_parser.add_argument("-m", "--server", type=str, help="mailserver to connect [%(default)s]", default="localhost")
+    my_parser.add_argument("-t", "--to", type=str, nargs="*", help="to address [%(default)s]", default="root@localhost")
+    my_parser.add_argument("message", nargs="+", help="message to send")
+    cur_opts = my_parser.parse_args()
+    # not implemented right now
+##        if arg == "-G":
+##            try:
+##                import mysql_tools
+##                import MySQLdb
+##            except ImportError:
+##                print "No mysql_tools found, exiting ..."
+##                sys.exit(1)
+##            else:
+##                db_con = mysql_tools.dbcon_container(with_logging=False)
+##                try:
+##                    dc = db_con.get_connection("cluster_full_access")
+##                except:
+##                    print "Cannot connect to SQL-Server (%s)" % (process_tools.get_except_info())
+##                    sys.exit(1)
+##                dc.execute("SELECT u.login,u.useremail FROM user u WHERE u.useremail LIKE('%@%')")
+##                user_dict = dict([(x["login"], x["useremail"]) for x in dc.fetchall()])
+##                dc.release()
+##                if user_dict:
+##                    user_list = sorted(user_dict.keys())
+##                    print "Sending mail to %s: %s" % (logging_tools.get_plural("user", len(user_list)),
+##                                                      ", ".join(user_list))
+##                    to_addrs = user_dict.values()
+##                else:
+##                    print "No users found, exiting ..."
+##                    sys.exit(0)
+    message = (" ".join(cur_opts.message)).replace("\\n", "\n").strip()
+    if not cur_opts.to:
         print "To-address(es) missing, exiting..."
         sys.exit(2)
     if not len(message):
         print "Need message text, exiting..."
         sys.exit(2)
-    my_mail = mail_tools.mail(subject, from_addr, to_addrs, message)
-    my_mail.set_server(mailserver)
+    my_mail = mail_tools.mail(cur_opts.subject, getattr(cur_opts, "from"), cur_opts.to, message)
+    my_mail.set_server(cur_opts.server)
     m_stat, m_ret_f = my_mail.send_mail()
     if m_stat:
-        print "Some error occured (%d): %s" % (m_stat,
-                                               "\n".join(m_ret_f))
+        print "Some error occured (%d): %s" % (
+            m_stat,
+            "\n".join(m_ret_f))
     else:
         print "Mail successfully sent"
     sys.exit(m_stat)
