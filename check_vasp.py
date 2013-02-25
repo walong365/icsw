6939ba3646ba5e41c24edf32f10c5cf4d78a95e1
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-02-25 07:57:01 +0100

    added check_vasp.py

---
+++ check_vasp.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python-init -Otu
+
+import os
+import sys
+from lxml import etree
+from lxml.builder import E
+
+outcar_name = "OUTCAR"
+vasprun_name = "vasprun.xml"
+
+class target_file(object):
+    def get_file_name(self, base_name):
+        if "JOB_ID" in os.environ:
+            f_name = "%s.%s.%s" % (base_name, os.environ["JOB_ID"], os.environ["SGE_TASK_ID"])
+        else:
+            f_name = "%s.%d" % (base_name, os.getpid())
+        add_iter = 0
+        while True:
+            c_file = "%s.xml" % (f_name if not add_iter else "%s.%d" % (f_name, add_iter))
+            if not os.path.isfile(c_file):
+                 break
+            add_iter += 1
+        return c_file
+        
+class outcar(target_file):
+    def __init__(self, f_name):
+        res = E.vasp_info(E.timing())
+        res.append(E.incar(file("INCAR", "r").read()))
+        loop_nodes = E.loop()
+        loopp_nodes = E.loopplus()
+        res.find("timing").extend([loop_nodes, loopp_nodes])
+        loop_times = []
+        for line_num, line in enumerate(file(f_name, "r").read().split("\n")):
+            s_line = line.strip()
+            s_parts = s_line.split()
+            if not line_num:
+                res.append(E.info(line.strip()))
+            elif s_line.startswith("running on"):
+                res.append(E.nodes(s_parts[2]))
+            elif s_parts:
+                if s_parts[0] == "distr:":
+                    res.append(E.distribution(nodes=s_parts[4], groups=s_parts[6]))
+                elif s_parts[0] == "LOOP:":
+                    loop_nodes.append(E.timing(cpu=s_parts[3][:-1], real=s_parts[6]))
+                elif s_parts[0] == "LOOP+:":
+                    loopp_nodes.append(E.timing(cpu=line[21:29].strip(), real=line[40:].strip()))
+        t_name = self.get_file_name("vasp_info")
+        print "saving to %s" % (t_name)
+        file(t_name, "w").write(etree.tostring(res, pretty_print=True))
+
+class vasprun(target_file):
+    def __init__(self, f_name):
+        src_xml = etree.fromstring(file(f_name, "r").read())
+        self._transform(src_xml)
+        t_name = self.get_file_name("vasp_run")
+        print "saving to %s" % (t_name)
+        file(t_name, "w").write(etree.tostring(src_xml, pretty_print=True))
+    def _transform(self, in_xml):
+        split_to_xyz = set(["v", "r"])
+        for node in in_xml.xpath(".//*"):
+            if node.text != None:
+                node.text = node.text.strip()
+                if node.tag in split_to_xyz:
+                    if len(node.text.split()) == 3:
+                        for val_name, val in zip(["x", "y", "z"], node.text.split()):
+                            node.append(getattr(E, val_name)(val))
+                    else:
+                        for val in node.text.split():
+                            node.append(E.val(val))
+                    node.text = ""
+        
+def main():
+    if os.path.isfile(outcar_name):
+        outcar(outcar_name)
+    else:
+        print "cannot find '%s'" % (outcar_name)
+    if os.path.isfile(vasprun_name):
+        vasprun(vasprun_name)
+    else:
+        print "cannot find '%s'" % (vasprun_name)
+
+if __name__ == "__main__":
+    main()
