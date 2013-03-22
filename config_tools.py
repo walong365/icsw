0e2dc5c431b6bca3face05a3faea75c6fb2f8517
treutner <treutner@init.at>
2013-03-22 14:56:02 +0100

    Added method for creating network topology

--- config_tools.py
+++ config_tools.py
@@ -140,6 +140,77 @@ class router_object(object):
     def map_path_to_device(self, in_path):
         return [self.nd_lut[value] for value in in_path]
 
+
+class topology_object(object):
+    def __init__(self, log_com):
+        self.__log_com = log_com
+        self.__cur_gen = 0
+        self.nx = None
+        self._update()
+    def add_nodes(self):
+        self.nx.add_nodes_from(self.nd_dict.keys())
+    def add_edges(self):
+        for node_pair, networkindex in self.simple_peer_dict.iteritems():
+            src_node, dst_node = node_pair
+            self.nx.add_edge(src_node, dst_node, networkidx=networkindex)
+    def _update(self):
+        latest_gen = route_generation.objects.all().order_by("-generation")
+        if latest_gen:
+            latest_gen = latest_gen[0].generation
+        else:
+            latest_gen = route_generation(generation=1)
+            latest_gen.save()
+            latest_gen = latest_gen.generation
+        if latest_gen != self.__cur_gen:
+            s_time = time.time()
+            self.all_nds = device.objects.exclude(Q(device_type__identifier="MD")).values_list("idx", "name")
+#             for cur_nd in self.all_nds:
+#                 if cur_nd[1] not in self.dev_dict:
+#                     self.dev_dict[cur_nd[1]] = []
+#                 self.dev_dict[cur_nd[1]].append(cur_nd)
+#             self.nd_lut = dict([(value[0], value[1]) for value in netdevice.objects.all().values_list("pk", "device")])
+            self.nd_dict = dict([(cur_nd[0], cur_nd) for cur_nd in self.all_nds])
+            self.log("init topology helper object, %s / %s" % (
+                logging_tools.get_plural("netdevice", len(self.all_nds)),
+                logging_tools.get_plural("peer information", peer_information.objects.count())))
+            # peer dict
+            self.peer_dict, self.simple_peer_dict = ({}, {})
+            all_peers = peer_information.objects.all().values_list("s_netdevice_id", "d_netdevice_id")
+            for s_nd_id, d_nd_id in all_peers:
+                src_netdevice = netdevice.objects.select_related().get(pk=s_nd_id)
+                src_device_id = src_netdevice.device.pk;
+                
+                dst_netdevice = netdevice.objects.select_related().get(pk=d_nd_id)
+                dst_device_id = dst_netdevice.device.pk;
+                try:
+                    network_id = net_ip.objects.get(Q(netdevice__exact=s_nd_id) & Q(network__network_type__identifier__in=["p", "o", "b"])).network.pk
+                    print(network_id)
+                    self.simple_peer_dict[(src_device_id, dst_device_id)] = network_id
+                except net_ip.DoesNotExist:
+                    print "netdevice " + str(s_nd_id) + " not in any network" 
+            if self.nx:
+                del self.nx
+            self.nx = networkx.Graph()
+            self.add_nodes()
+            self.add_edges()
+            if self.__cur_gen:
+                self.log("update generation from %d to %d in %s" % (
+                    self.__cur_gen,
+                    latest_gen,
+                    logging_tools.get_diff_time_str(time.time() - s_time),
+                ))
+            else:
+                self.log("init with generation %d in %s" % (
+                    latest_gen,
+                    logging_tools.get_diff_time_str(time.time() - s_time),
+                ))
+            self.__cur_gen = latest_gen
+    def check_for_update(self):
+        self._update()
+    def log(self, what, log_level=logging_tools.LOG_LEVEL_OK):
+        self.__log_com("[router] %s" % (what), log_level)
+
+
 def get_config_var_list(config_obj, config_dev):
     r_dict = {}
     # dict of local vars without specified host
