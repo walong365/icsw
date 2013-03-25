d38574974f5539e1c2e319e061ded0227c1882cd
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-03-25 20:46:26 +0100

    set LINGER time for loggers to 0

--- logging_tools.py
+++ logging_tools.py
@@ -240,6 +240,7 @@ def get_logger(name, destination, **kwargs):
             if kwargs.get("zmq", False):
                 cur_context = kwargs["context"]
                 pub = cur_context.socket(zmq.PUSH)
+                pub.setsockopt(zmq.LINGER, 0)
                 pub.connect(rewrite_log_destination(act_dest if act_dest.endswith("_zmq") else "%s_zmq" % (act_dest)))
                 act_logger.addHandler(zmq_handler(pub, act_logger))
             else:
