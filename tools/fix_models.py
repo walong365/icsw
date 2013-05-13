40663e76e987e0ef010c667acf3c03fb1933f7df
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-05-13 09:48:33 +0200

    updated migration tools

--- tools/fix_models.py
+++ tools/fix_models.py
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-""" restore users / groups from old CSW """
+""" copy monitoring settings from old to new db schema """
 
 import sys
 import os
@@ -25,11 +25,13 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "initat.cluster.settings")
 
 import datetime
+import pprint
 from django.conf import settings
 from django.db.models import Q
 from initat.cluster.backbone.models import device, device_class, device_group, \
      mon_contact, mon_contactgroup, mon_check_command_type, mon_check_command, \
-     config, mon_service_templ, user, mon_period
+     config, mon_service_templ, user, mon_period, mon_ext_host, mon_service_templ, \
+     mon_device_templ
 from django.db.models.base import ModelBase
      
 def _parse_value(in_str):
@@ -66,12 +68,26 @@ def main():
         "ng_check_command"      : (mon_check_command, ["pk", None, ("config", config), ("mon_check_command_type", mon_check_command_type), ("mon_service_templ", mon_service_templ), "name", "command_line", "description", None, None], ["name",],),
         "ng_contact"            : (mon_contact, ["pk", ("user", user), ("snperiod", mon_period), ("hnperiod", mon_period), "snrecovery", "sncritical", "snwarning", "snunknown", "hnrecovery", "hndown", "hnunreachable", "sncommand", "hncommand"], [],),
         "ng_contactgroup"       : (mon_contactgroup, ["pk", "name", "alias"], [],),
+        "ng_service_templ"      : (mon_service_templ, ["pk", "name", "volatile", ("nsc_period", mon_period), "max_attempts", "check_interval", "retry_interval", "ninterval", ("nsn_period", mon_period), "nrecovery", "ncritical", "nwarning", "nunknown"], []),
+        "ng_device_templ"       : (mon_device_templ, ["pk", "name", ("mon_service_templ", mon_service_templ), "ccommand", "max_attempts", "ninterval", ("mon_period", mon_period), "nrecovery", "ndown", "nunreachable", "is_default"], []),
     }
+    copy_dict = {
+        "device" : (device, [(11, "mon_ext_host", "mon_ext_host"), (10, "mon_device_templ", mon_device_templ)])
+    }
+    lut_dict = {
+        "ng_ext_host" : ("mon_ext_host", mon_ext_host, 1, "name"),
+    }
+    lut_table = {}
     for line in file(data_file, "r").xreadlines():
         line = line.strip()
         l_line = line.lower()
         if l_line.startswith("insert into"):
             t_obj_name = l_line.split()[2][1:-1]
+            if t_obj_name in lut_dict:
+                values = [_parse_value(val) for val in line.split(None, 4)[-1][1:-2].split("),(")]
+                t_name, obj_class, lut_idx, lut_name = lut_dict[t_obj_name]
+                for value in values:
+                    lut_table.setdefault(t_name, {})[value[0]] = obj_class.objects.get(Q(**{lut_name : value[lut_idx]}))
             if t_obj_name in transfer_dict:
                 values = [_parse_value(val) for val in line.split(None, 4)[-1][1:-2].split("),(")]
                 obj_class, t_list, unique_list = transfer_dict[t_obj_name]
@@ -104,6 +120,26 @@ def main():
                                     setattr(new_obj, t_info, cur_val)
                         if create:
                             new_obj.save()
+            if t_obj_name in copy_dict:
+                values = [_parse_value(val) for val in line.split(None, 4)[-1][1:-2].split("),(")]
+                obj_class, c_list = copy_dict[t_obj_name]
+                for value in values:
+                    cur_obj = obj_class.objects.get(Q(pk=value[0]))
+                    for val_idx, attr_name, val_obj_name in c_list:
+                        val = value[val_idx]
+                        if val == 0:
+                            val = None
+                        else:
+                            if type(val_obj_name) in [str, unicode]:
+                                val = lut_table[val_obj_name].get(val, None)
+                            else:
+                                try:
+                                    val = val_obj_name.objects.get(Q(pk=val))
+                                except:
+                                    print "object not found (%s, %s)" % (val_obj_name, val)
+                                    val = None
+                        setattr(cur_obj, attr_name, val)
+                    cur_obj.save()
     fix_dict = {
         "device" : {
             "zero_to_null" : [
