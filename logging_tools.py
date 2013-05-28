8e53fb3951c14819fbbae2344035d716925e8beb
Konrad Podloucky <podloucky@init.at>
2013-05-28 11:07:34 +0200

    Bugfix for formatting messages in beautify_record()
    * now a method of zmq_handler (to have access to the handler's formatter)
    * uses the formatter to format the error message before using it

--- logging_tools.py
+++ logging_tools.py
@@ -370,6 +370,34 @@ class zmq_handler(logging.Handler):
                 # remove from handler
                 self.__logger.removeHandler(self)
 
+    def beautify_record(self, record):
+        # format message, so that record.message gets the properly formatted message
+        self.format(record)
+        if getattr(record, "exc_info", None):
+            tb_object = record.exc_info[2]
+            frame_info = []
+            for file_name, line_no, name, line in traceback.extract_tb(tb_object):
+                frame_info.append("File '%s', line %d, in %s" % (file_name, line_no, name))
+                if line:
+                    frame_info.append(" - %d : %s" % (line_no, line))
+            frame_info.append("%s (%s)" % (unicode(record.exc_info[0]),
+                                           unicode(record.exc_info[1])))
+            record.error_str = record.message + "\n" + "\n".join(frame_info)
+            var_list = []
+            req_var = inspect.trace()[-1][0].f_locals.get("request", None)
+            if req_var:
+                # print get / post variables
+                for key in ["GET", "POST"]:
+                    v_dict = getattr(req_var, key, None)
+                    if v_dict:
+                        var_list.append("")
+                        var_list.append("%s (%d):" % (key, len(v_dict)))
+                        for s_num, s_key in enumerate(sorted(v_dict.keys())):
+                            var_list.append("  %3d %s: %s" % (s_num + 1, s_key, v_dict[s_key]))
+            record.exc_text = "\n".join(frame_info + var_list)
+        else:
+            record.error_str = record.message
+
 class init_handler(zmq_handler):
     zmq_context = None
     def __init__(self, filename=None):
@@ -386,32 +414,6 @@ class init_handler(zmq_handler):
             record.request = "request"
         zmq_handler.emit(self, record)
 
-def beautify_record(record):
-    if getattr(record, "exc_info", None):
-        tb_object = record.exc_info[2]
-        frame_info = []
-        for file_name, line_no, name, line in traceback.extract_tb(tb_object):
-            frame_info.append("File '%s', line %d, in %s" % (file_name, line_no, name))
-            if line:
-                frame_info.append(" - %d : %s" % (line_no, line))
-        frame_info.append("%s (%s)" % (unicode(record.exc_info[0]),
-                                       unicode(record.exc_info[1])))
-        record.error_str = record.msg % ("\n".join(frame_info))
-        var_list = []
-        req_var = inspect.trace()[-1][0].f_locals.get("request", None)
-        if req_var:
-            # print get / post variables
-            for key in ["GET", "POST"]:
-                v_dict = getattr(req_var, key, None)
-                if v_dict:
-                    var_list.append("")
-                    var_list.append("%s (%d):" % (key, len(v_dict)))
-                    for s_num, s_key in enumerate(sorted(v_dict.keys())):
-                        var_list.append("  %3d %s: %s" % (s_num + 1, s_key, v_dict[s_key]))
-        record.exc_text = "\n".join(frame_info + var_list)
-    else:
-        record.error_str = record.msg
-    
 class init_email_handler(zmq_handler):
     zmq_context = None
     def __init__(self, filename=None, *args, **kwargs):
@@ -426,7 +428,7 @@ class init_email_handler(zmq_handler):
                        "lineno"     : 1}
     def emit(self, record):
         record.IOS_type = "error"
-        beautify_record(record)
+        self.beautify_record(record)
         record.uid = os.getuid()
         record.gid = os.getgid()
         record.pid = os.getpid()
@@ -450,7 +452,7 @@ class init_handler_unified(zmq_handler):
     def emit(self, record):
         if record.name.startswith("init.at."):
             record.name = record.name[8:]
-        beautify_record(record)
+        self.beautify_record(record)
         for key in self.__lens.keys():
             cur_val = getattr(record, key)
             if type(cur_val) in [int, long]:
