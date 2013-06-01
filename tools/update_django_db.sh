b44df57001aca4e631858b6590e5f6aa190495d7
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-06-01 19:25:09 +0200

    added migration scripts to update script

--- tools/update_django_db.sh
+++ tools/update_django_db.sh
@@ -6,6 +6,7 @@ if [ -d ${MIG_DIR} ] ; then
     echo "migration schema, migration directory is ${MIG_DIR}"
     ${C_DIR}/manage.py schemamigration backbone --auto
     ${C_DIR}/manage.py migrate --no-initial-data backbone 
+    /opt/cluster/bin/migrate_to_domain_name.py
 else
     echo "no migration directory ${MIG_DIR} present, refuse to operate"
 fi
