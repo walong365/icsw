9677574f7452a6cc9890402b670ea69c26a1bed9
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-06-04 10:55:22 +0200

    added missing __init__.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name="host-monitoring",
       author="Andreas Lang-Nevyjel",
       author_email="lang@init.at",
       py_modules=["initat.snmp_relay.snmp_relay_schemes"],
-      packages=["initat.host_monitoring", "initat.host_monitoring.modules"],
+      packages=["initat.host_monitoring", "initat.host_monitoring.modules", "initat.host_monitoring.exe"],
       )
