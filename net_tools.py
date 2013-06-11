935a111096d7659e7ac9fbcd23dff37e17581a6a
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-06-11 20:13:54 +0200

    removed files from module

--- net_tools.py
+++ net_tools.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python-init -Ot
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2006,2007,2009,2010,2011,2012 Andreas Lang-Nevyjel init.at
+# Copyright (C) 2006,2007,2009,2010,2011,2012,2013 Andreas Lang-Nevyjel init.at
 #
 # Send feedback to: <lang-nevyjel@init.at>
 #
