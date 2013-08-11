70e2e8692b074b61aa4eeca8697eb4c8bb66e2a7
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-08-11 10:47:23 +0200

    removed code for clustersoftware

--- src/initat/core/render.py
+++ src/initat/core/render.py
@@ -4,12 +4,8 @@ import django.template
 import logging
 
 from django.conf import settings
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, redirect
 from django.template.loader import render_to_string
-from django.utils.decorators import method_decorator
-
-logger = logging.getLogger("cluster.render")
 
 class render_me(object):
     """
@@ -47,27 +43,3 @@ def render_string(request, template_name, in_dict=None):
         in_dict if in_dict is not None else {},
         django.template.RequestContext(request)))
 
-class permission_required_mixin(object):
-    all_required_permissions = ()
-    any_required_permissions = ()
-    @method_decorator(login_required)
-    def dispatch(self, request, *args, **kwargs):
-        if self.all_required_permissions:
-            if not request.user.has_object_perms(self.all_required_permissions):
-                logger.error("user %s has not the required permissions %s" % (
-                    unicode(request.user),
-                    str(self.all_required_permissions),
-                    ))
-                return redirect(settings.LOGIN_URL)
-        if self.any_required_permissions:
-            if not request.user.has_any_object_perms(self.any_required_permissions):
-                logger.error("user %s has not any of the required permissions %s" % (
-                    unicode(request.user),
-                    str(self.any_required_permissions),
-                    ))
-                return redirect(settings.LOGIN_URL)
-        return super(permission_required_mixin, self).dispatch(
-            request,
-            *args,
-            **kwargs
-            )
