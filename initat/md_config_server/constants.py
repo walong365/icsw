704ff538e4b6635d40bf7541f79271bad7b2b872
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-08-12 21:27:15 +0200

    reorganized source files

---
+++ initat/md_config_server/constants.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python-init -Otu
+#
+# Copyright (C) 2013 Andreas Lang-Nevyjel, init.at
+#
+# this file is part of md-config-server
+#
+# Send feedback to: <lang-nevyjel@init.at>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License Version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+""" icinga constants """
+
+# icinga constants
+NAG_HOST_UNKNOWN = -1
+NAG_HOST_UP = 0
+NAG_HOST_DOWN = 1
+NAG_HOST_UNREACHABLE = 2
+
+# default port
+SERVER_COM_PORT = 8010
+TEMPLATE_NAME = "t"
+
+# maps to transfer for single_build
+SINGLE_BUILD_MAPS = set(["device.d", "command"])
+
+IDOMOD_PROCESS_PROCESS_DATA = 2 ** 0
+IDOMOD_PROCESS_TIMED_EVENT_DATA = 2 ** 1
+IDOMOD_PROCESS_LOG_DATA = 2 ** 2
+IDOMOD_PROCESS_SYSTEM_COMMAND_DATA = 2 ** 3
+IDOMOD_PROCESS_EVENT_HANDLER_DATA = 2 ** 4
+IDOMOD_PROCESS_NOTIFICATION_DATA = 2 ** 5
+IDOMOD_PROCESS_SERVICE_CHECK_DATA = 2 ** 6
+IDOMOD_PROCESS_HOST_CHECK_DATA = 2 ** 7
+IDOMOD_PROCESS_COMMENT_DATA = 2 ** 8
+IDOMOD_PROCESS_DOWNTIME_DATA = 2 ** 9
+IDOMOD_PROCESS_FLAPPING_DATA = 2 ** 10
+IDOMOD_PROCESS_PROGRAM_STATUS_DATA = 2 ** 11
+IDOMOD_PROCESS_HOST_STATUS_DATA = 2 ** 12
+IDOMOD_PROCESS_SERVICE_STATUS_DATA = 2 ** 13
+IDOMOD_PROCESS_ADAPTIVE_PROGRAM_DATA = 2 ** 14
+IDOMOD_PROCESS_ADAPTIVE_HOST_DATA = 2 ** 15
+IDOMOD_PROCESS_ADAPTIVE_SERVICE_DATA = 2 ** 16
+IDOMOD_PROCESS_EXTERNAL_COMMAND_DATA = 2 ** 17
+IDOMOD_PROCESS_OBJECT_CONFIG_DATA = 2 ** 18
+IDOMOD_PROCESS_MAIN_CONFIG_DATA = 2 ** 19
+IDOMOD_PROCESS_AGGREGATED_STATUS_DATA = 2 ** 20
+IDOMOD_PROCESS_RETENTION_DATA = 2 ** 21
+IDOMOD_PROCESS_ACKNOWLEDGEMENT_DATA = 2 ** 22
+IDOMOD_PROCESS_STATECHANGE_DATA = 2 ** 23
+IDOMOD_PROCESS_CONTACT_STATUS_DATA = 2 ** 24
+IDOMOD_PROCESS_ADAPTIVE_CONTACT_DATA = 2 ** 25
+
+BROKER_PROGRAM_STATE = 2 ** 0
+BROKER_TIMED_EVENTS = 2 ** 1
+BROKER_SERVICE_CHECKS = 2 ** 2
+BROKER_HOST_CHECKS = 2 ** 3
+BROKER_EVENT_HANDLERS = 2 ** 4
+BROKER_LOGGED_DATA = 2 ** 5
+BROKER_NOTIFICATIONS = 2 ** 6
+BROKER_FLAPPING_DATA = 2 ** 7
+BROKER_COMMENT_DATA = 2 ** 8
+BROKER_DOWNTIME_DATA = 2 ** 9
+BROKER_SYSTEM_COMMANDS = 2 ** 10
+BROKER_OCP_DATA = 2 ** 11
+BROKER_STATUS_DATA = 2 ** 12
+BROKER_ADAPTIVE_DATA = 2 ** 13
+BROKER_EXTERNALCOMMAND_DATA = 2 ** 14
+BROKER_RETENTION_DATA = 2 ** 15
+BROKER_ACKNOWLEDGEMENT_DATA = 2 ** 16
+BROKER_STATECHANGE_DATA = 2 ** 17
+BROKER_RESERVED18 = 2 ** 18
+BROKER_RESERVED19 = 2 ** 19
+
