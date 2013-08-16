b52878294b4c546a7b82ed825e04f36594cc45fd
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-08-16 22:42:24 +0200

    improved package install

--- django/initat/cluster/frontend/pack_views.py
+++ django/initat/cluster/frontend/pack_views.py
@@ -3,6 +3,7 @@
 
 import logging
 import logging_tools
+import pprint
 import re
 import server_command
 import time
@@ -80,7 +81,7 @@ class create_search(View):
 def reload_searches(request):
     srv_com = server_command.srv_command(command="reload_searches")
     return contact_server(request, "tcp://localhost:8007", srv_com, timeout=5, log_result=False)
-    
+
 class retry_search(View):
     @method_decorator(transaction.commit_manually)
     @method_decorator(login_required)
@@ -162,7 +163,7 @@ class unuse_package(View):
             else:
                 cur_p.delete()
                 request.xml_response.info("removed package", logger)
-    
+
 class install(View):
     @method_decorator(login_required)
     def get(self, request):
@@ -200,39 +201,67 @@ class refresh(View):
         )
         request.xml_response["response"] = xml_resp
 
+def get_listlist(q_dict, key, default):
+    # f_keys = [key for key in q_dict.]
+    list_re = re.compile("^%s\[(?P<idx>\d+)\]\[\]$" % (key))
+    res_list = None
+    for l_name, l_values in q_dict.lists():
+        l_m = list_re.match(l_name)
+        if l_m:
+            if res_list is None:
+                res_list = []
+            res_list.insert(int(l_m.group("idx")), l_values)
+    if res_list is None:
+        res_list = default
+    return res_list
+
 class add_package(View):
     @method_decorator(login_required)
     @method_decorator(xml_wrapper)
     def post(self, request):
         _post = request.POST
-        dev_pk, pack_pk = (
-            int(_post["dev_key"].split("__")[1]),
-            int(_post["pack_key"].split("__")[1]))
-        try:
-            _cur_pdc = package_device_connection.objects.get(Q(device=dev_pk) & Q(package=pack_pk))
-        except package_device_connection.DoesNotExist:
-            new_pdc = package_device_connection(
-                device=device.objects.get(Q(pk=dev_pk)),
-                package=package.objects.get(Q(pk=pack_pk)))
-            new_pdc.save()
-            request.xml_response["new_entry"] = new_pdc.get_xml()
-            request.xml_response.info("added connection", logger)
-        else:
-            request.xml_response.error("connection already exists", logger)
+        num_ok, num_error = (0, 0)
+        new_entries = E.entries()
+        for dev_pk_s, pack_pk_s in get_listlist(_post, "add_list", []):
+            dev_pk, pack_pk = (
+                int(dev_pk_s.split("__")[1]),
+                int(pack_pk_s.split("__")[1]))
+            try:
+                _cur_pdc = package_device_connection.objects.get(Q(device=dev_pk) & Q(package=pack_pk))
+            except package_device_connection.DoesNotExist:
+                new_pdc = package_device_connection(
+                    device=device.objects.get(Q(pk=dev_pk)),
+                    package=package.objects.get(Q(pk=pack_pk)))
+                new_pdc.save()
+                new_entries.append(new_pdc.get_xml())
+                num_ok += 1
+            else:
+                num_error += 1
+        if num_ok:
+            request.xml_response.info("added %s" % (logging_tools.get_plural("connection", num_ok)), logger)
+        if num_error:
+            request.xml_response.error("%s already existed" % (logging_tools.get_plural("connection", num_error)), logger)
+        request.xml_response["result"] = new_entries
 
 class remove_package(View):
     @method_decorator(login_required)
     @method_decorator(xml_wrapper)
     def post(self, request):
         _post = request.POST
-        pdc_pk = int(_post["pdc_key"].split("__")[1])
-        try:
-            cur_pdc = package_device_connection.objects.get(Q(pk=pdc_pk))
-        except package_device_connection.DoesNotExist:
-            request.xml_response.error("connection doest not exists", logger)
-        else:
-            cur_pdc.delete()
-            request.xml_response.info("connection removed", logger)
+        num_ok, num_error = (0, 0)
+        for pdc_pk_s in _post.getlist("rem_list[]"):
+            pdc_pk = int(pdc_pk_s.split("__")[1])
+            try:
+                cur_pdc = package_device_connection.objects.get(Q(pk=pdc_pk))
+            except package_device_connection.DoesNotExist:
+                num_error += 1
+            else:
+                cur_pdc.delete()
+                num_ok += 1
+        if num_ok:
+            request.xml_response.info("%s removed" % (logging_tools.get_plural("connection", num_ok)), logger)
+        if num_error:
+            request.xml_response.error("%s not exists" % (logging_tools.get_plural("connection", num_error)), logger)
 
 class change_target_state(View):
     @method_decorator(login_required)
@@ -251,7 +280,7 @@ class change_package_flag(View):
         _post = request.POST
         cur_pdc = package_device_connection.objects.select_related("package").get(Q(pk=_post["pdc_key"].split("__")[1]))
         flag_name = _post["pdc_key"].split("__")[-1]
-        #print flag_name
+        # print flag_name
         value = True if int(_post["value"]) else False
         sflag_name = flag_name[:-5] if flag_name.endswith("_flag") else flag_name
         request.xml_response.info(
@@ -278,6 +307,6 @@ class synchronize(View):
         srv_com = server_command.srv_command(command="new_config")
         result = contact_server(request, "tcp://localhost:8007", srv_com, timeout=10, log_result=False)
         if result:
-            #print result.pretty_print()
+            # print result.pretty_print()
             request.xml_response.info("sent sync to server", logger)
-    
+
