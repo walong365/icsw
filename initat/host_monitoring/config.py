b39f9d3bc5ced499cebcc89c358cb4211bf9f2f8
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-08-18 21:25:11 +0200

    build test

--- initat/host_monitoring/config.py
+++ initat/host_monitoring/config.py
@@ -21,7 +21,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-""" host-monitoring, global_config """
+""" host-monitoring, config """
 
 import configfile
 import process_tools
