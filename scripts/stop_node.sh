2c6d71dbf99340946e835036019a1dc8bd7dad1d
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-10-28 16:58:38 +0100

    moved scripts to check_scripts

--- scripts/stop_node.sh
+++ scripts/stop_node.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
+#
+# Copyright (C) 2001,2002,2003,2004,2005,2006,2007,2012,2013 Andreas Lang-Nevyjel
+#
+# Send feedback to: <lang-nevyjel@init.at>
+# 
+# this file is part of host-monitoring
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License Version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
 
-if [ "$#" -gt 0 ] ; then
-    stop_type="Force-stopping"
-    stop_arg="force-stop"
-else
-    stop_type="Stopping"
-    stop_arg="stop"
-fi
+/opt/cluster/sbin/check_scripts.py --node ALL --mode stop $*
 
-if [ -f /etc/rc.status ] ; then
-    . /etc/rc.status
-else
-    . /etc/rc.status_suse
-fi
-
-for node in hoststatus logging-server meta-server host-monitoring package-client ; do
-    rc_reset
-    if [ -f /etc/init.d/$node ] ; then
-	/etc/init.d/$node $stop_arg
-    else
-	echo -n "$stop_type $node ... "
-	rc_status -u
-    fi
-done
