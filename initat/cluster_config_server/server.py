e87f8e796b4ce889021229ae4a26e931334d58ee
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-11-09 17:32:38 +0100

    fuzzy upper ceiling for 0MQ-thread

--- initat/cluster_config_server/server.py
+++ initat/cluster_config_server/server.py
@@ -99,7 +99,7 @@ class server_process(threading_tools.process_pool):
         if not global_config["DEBUG"] or True:
             self.log("Initialising meta-server-info block")
             msi_block = process_tools.meta_server_info("cluster-config-server")
-            msi_block.add_actual_pid(mult=3)
+            msi_block.add_actual_pid(mult=3, fuzzy_ceiling=3)
             msi_block.add_actual_pid(act_pid=configfile.get_manager_pid(), mult=3)
             msi_block.start_command = "/etc/init.d/cluster-config-server start"
             msi_block.stop_command = "/etc/init.d/cluster-config-server force-stop"
