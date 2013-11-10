88cf261b470ae93d424f563aa39af9ac03eba588
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-11-10 10:04:29 +0100

    added fuzzy_ceiling

--- initat/md_config_server/server.py
+++ initat/md_config_server/server.py
@@ -403,7 +403,7 @@ class server_process(threading_tools.process_pool):
         if not global_config["DEBUG"] or True:
             self.log("Initialising meta-server-info block")
             msi_block = process_tools.meta_server_info("md-config-server")
-            msi_block.add_actual_pid(mult=3)
+            msi_block.add_actual_pid(mult=3, fuzzy_ceiling=3)
             msi_block.add_actual_pid(act_pid=configfile.get_manager_pid(), mult=4)
             msi_block.start_command = "/etc/init.d/md-config-server start"
             msi_block.stop_command = "/etc/init.d/md-config-server force-stop"
