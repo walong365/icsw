1292c3d41df950685a52b2c6176e9e07ffa8bdda
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-11-10 10:21:40 +0100

    added fuzzy_ceiling:

--- initat/rms/server.py
+++ initat/rms/server.py
@@ -197,7 +197,7 @@ class server_process(threading_tools.process_pool):
         if not global_config["DEBUG"] or True:
             self.log("Initialising meta-server-info block")
             msi_block = process_tools.meta_server_info("rms_server")
-            msi_block.add_actual_pid(mult=3)
+            msi_block.add_actual_pid(mult=3, fuzzy_ceiling=3)
             msi_block.add_actual_pid(act_pid=configfile.get_manager_pid(), mult=3)
             msi_block.start_command = "/etc/init.d/rms-server start"
             msi_block.stop_command = "/etc/init.d/rms-server force-stop"
