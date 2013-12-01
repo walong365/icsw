121ed58d82b4c1aa925aaa7ae9d1ba2131c02c37
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-12-01 15:24:51 +0100

    angular pagination

--- django/initat/cluster/frontend/templates/angular/common_functions.cs
+++ django/initat/cluster/frontend/templates/angular/common_functions.cs
@@ -14,6 +14,40 @@ array_to_dict = (array, key) ->
             res[value[key]] = value
     return res
 
+class paginator_class
+    constructor: () ->
+        @conf = {
+            per_page    : 10
+            num_entries : 0
+            num_pages   : 0
+            start_idx   : 0
+            end_idx     : 0
+            act_page    : 0
+            page_list   : []
+            init        : false
+        }
+    activate_page: (num) =>
+        @conf.actpage = parseInt(num)
+        # indices start at zero
+        pp = @conf.per_page
+        @conf.start_idx = (@conf.actpage - 1 ) * pp
+        @conf.end_idx = (@conf.actpage - 1) * pp + pp - 1
+        if @conf.end_idx >= @conf.num_entries
+            @conf.end_idx = @conf.num_entries - 1
+    set_num_entries: (num) =>
+        @conf.init = true
+        @conf.num_entries = num
+        pp = @conf.per_page
+        @conf.num_pages = parseInt((@conf.num_entries + pp - 1) / pp)
+        if @conf.num_pages > 0
+            @conf.page_list = (idx for idx in [1..@conf.num_pages])
+        else
+            @conf.page_list = []
+        if @conf.act_page == 0
+            @activate_page(1)
+        else
+            @activate_page(@conf.act_page)
+    
 angular_module_setup = (module_list) ->
     $(module_list).each (idx, cur_mod) ->
         cur_mod.config(['$httpProvider', 
@@ -21,6 +55,14 @@ angular_module_setup = (module_list) ->
                 $httpProvider.defaults.xsrfCookieName = 'csrftoken'
                 $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken'
         ])
+        cur_mod.service("paginatorSettings", (paginator_class))
+        cur_mod.filter("paginator", () ->
+            return (arr, scope) ->
+                if scope.pag_settings.conf.init 
+                    return arr.slice(scope.pag_settings.conf.start_idx, scope.pag_settings.conf.end_idx + 1)
+                else
+                    return arr
+        )
         cur_mod.config(["RestangularProvider", 
             (RestangularProvider) ->
                 RestangularProvider.setRestangularFields({
@@ -48,6 +90,25 @@ angular_module_setup = (module_list) ->
                     return true
                 )
         ])
+        cur_mod.directive("paginator", ($templateCache, paginatorSettings) ->
+            link = (scope, element, attrs) ->
+                scope.pag_settings = paginatorSettings
+                scope.pag_settings.conf.per_page = parseInt(attrs.perPage)
+                # console.log "init", scope.pag_settings
+                scope.activate_page = (page_num) ->
+                    scope.pag_settings.activate_page(page_num)
+                scope.$watch("entries", (new_el) ->
+                    scope.pag_settings.set_num_entries(new_el.length)
+                )
+            return {
+                restrict : "EA"
+                scope:
+                    entries : "="
+                template : $templateCache.get("paginator.html")
+                link     : link
+            }
+        )
+        
         
 handle_reset = (data, e_list, idx) ->
     # console.log "HR", data, e_list, idx
@@ -66,9 +127,10 @@ angular_add_simple_list_controller = (module, name, settings) ->
                 template : $templateCache.get(t_name)
             }
         )
-    module.controller(name, ["$scope", "$compile", "$templateCache", "Restangular",
-        ($scope, $compile, $templateCache, Restangular) ->
+    module.controller(name, ["$scope", "$compile", "$templateCache", "Restangular", "paginatorSettings"
+        ($scope, $compile, $templateCache, Restangular, paginatorSettings) ->
             $scope.settings = settings
+            $scope.pag_settings = paginatorSettings
             $scope.entries = []
             $scope.rest = Restangular.all($scope.settings.rest_url.slice(1))
             $scope.reload = () ->
@@ -76,6 +138,10 @@ angular_add_simple_list_controller = (module, name, settings) ->
                     $scope.entries = response
                 )
             $scope.reload()
+            $scope.get_entries = () ->
+                pp = $scope.pag_settings.conf
+                r_list = (obj for obj in $scope.entries[pp.start_idx .. pp.end_idx])
+                return r_list
             $scope.modify = () ->
                 if not $scope.form.$invalid
                     if $scope.create_mode
