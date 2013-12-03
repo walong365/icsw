9b221e6a9e384c615e8994fe5cc9b3acf23c6b06
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-12-03 18:11:37 +0100

    symlink handling

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from distutils.core import setup
 static_files = []
 strip_leading = re.compile("src/initat/core/")
 for root, dirs, files in os.walk("src/initat/core/static"):
+    print dirs, files
     for i in files:
-        print i
         stripped_root = re.sub(strip_leading, "", root)
         static_files.append(os.path.join(stripped_root, i))
 
