ac56b56c01fe6f6c70b2af5a987d23a490df20e9
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2013-12-08 19:57:43 +0100

    set names for meta-server

--- initat/meta_server/server.py
+++ initat/meta_server/server.py
@@ -89,9 +89,9 @@ class main_process(threading_tools.process_pool):
         if True: # not self.__options.DEBUG:
             self.log("Initialising meta-server-info block")
             msi_block = process_tools.meta_server_info("meta-server")
-            msi_block.add_actual_pid(mult=3, fuzzy_ceiling=3)
+            msi_block.add_actual_pid(mult=3, fuzzy_ceiling=3, process_name="main")
             if not _spm:
-                msi_block.add_actual_pid(act_pid=configfile.get_manager_pid(), mult=2)
+                msi_block.add_actual_pid(act_pid=configfile.get_manager_pid(), mult=2, process_name="manager")
             msi_block.start_command = "/etc/init.d/meta-server start"
             msi_block.stop_command = "/etc/init.d/meta-server force-stop"
             msi_block.kill_pids = True
