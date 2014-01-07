98ee66ba2c8e3577d207f39125709c2a7aeca8c0
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-01-07 07:36:50 +0100

    reenabled migration of static_precompiler

--- tools/create_database.sh
+++ tools/create_database.sh
@@ -19,12 +19,13 @@ if [ "${UID:-X}" = "0" ] ; then
         ${C_DIR}/manage.py schemamigration django.contrib.auth --initial
         ${C_DIR}/manage.py schemamigration backbone --initial
         ${C_DIR}/manage.py schemamigration reversion --initial
-        # not working with south, strange ...
-	#${C_DIR}/manage.py schemamigration static_precompiler --initial
+        # not working with south, strange ..., reenabled 
+	${C_DIR}/manage.py schemamigration static_precompiler --initial
         ${C_DIR}/manage.py migrate auth
         ${C_DIR}/manage.py migrate backbone --no-initial-data
         ${C_DIR}/manage.py migrate reversion
-        #${C_DIR}/manage.py migrate static_precompiler
+	# see above
+        ${C_DIR}/manage.py migrate static_precompiler
         ${C_DIR}/manage.py syncdb --noinput ${ID_FLAGS}
         ${C_DIR}/manage.py migrate ${ID_FLAGS}
         if [ -z "$1" ]; then
