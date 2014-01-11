b243f6481f447f801490a98732ecdd5a4132f132
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-01-11 15:33:53 +0100

    autodetection of corvus

--- nginx/webfrontend_post_install.sh
+++ nginx/webfrontend_post_install.sh
@@ -7,6 +7,13 @@ fi
 
 sed -i sX/usr/bin/env\ nodeX/opt/cluster/bin/nodeXg /opt/cluster/lib/node_modules/yuglify/bin/yuglify
 
+# check for product version
+
+if [ -f /etc/init.d/mother ] ; then
+    # seems to be a cluster
+    touch /etc/sysconfig/cluster/.is_corvus
+fi
+
 /opt/python-init/lib/python/site-packages/initat/cluster/manage.py collectstatic --noinput
 
 if [ -d /opt/cluster/etc/uwsgi/reload ] ; then
