5fe831f0bc333d19c667f0afbbe5ed0c49493ff6
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-01-29 13:44:55 +0100

    added some options to set correct node-path

--- initat/cluster/runserver.sh
+++ initat/cluster/runserver.sh
@@ -4,10 +4,14 @@ export DEBUG_WEBFRONTEND=1
 
 if [ "$1" != "--nostatic" ] ; then
      echo "collecting static files ..."
-    ./manage.py collectstatic --noinput
+    ./manage.py collectstatic --noinput -c
     echo "done"
 fi
 
+export NODE_PATH=$(/opt/cluster/bin/npm -g root)
+export NODE_PATH=${NODE_PATH}:${NODE_PATH}/npm/node_modules
+
+echo $NODE_PATH
 
 ./manage.py runserver $* --traceback 0.0.0.0:8080 
 
