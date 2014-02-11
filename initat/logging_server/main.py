476efa92014c5f0c3a82f8cb961126a02dbdbf93
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-02-11 14:34:01 +0100

    moved to single-process mode

--- initat/logging_server/main.py
+++ initat/logging_server/main.py
@@ -48,14 +48,23 @@ if PYTHON3:
 
 SEP_STR = "-" * 50
 
-class log_receiver(threading_tools.process_obj):
-    def process_init(self):
+class main_process(threading_tools.process_pool):
+    def __init__(self, options):
+        self.__options = options
+        # log structures
         self.__log_cache = []
         self.__handles = {}
-        threading_tools.process_obj.process_init(self)
-        self.register_func("log_recv", self._log_recv)
-        self.register_func("log", self.log)
-        self.register_func("update", self._update)
+        threading_tools.process_pool.__init__(self, "main", stack_size=2 * 1024 * 1024, zmq=True, zmq_debug=global_config["ZMQ_DEBUG"])
+        process_tools.delete_lockfile(global_config["LOCKFILE_NAME"])
+        self.register_exception("int_error", self._int_error)
+        self.register_exception("term_error", self._int_error)
+        self.register_func("startup_error", self._startup_error)
+        self.renice()
+        self._init_msi_block()
+        # self.add_process(log_receiver("receiver", priority=50), start=True)
+        self._log_config()
+        self._init_network_sockets()
+        self.register_timer(self._update, 60)
         os.umask(2)
         self.__num_write, self.__num_close, self.__num_open = (0, 0, 0)
         self.log("logging_process %s is now awake (pid %d)" % (self.name, self.pid))
@@ -69,31 +78,104 @@ class log_receiver(threading_tools.process_obj):
         self.__eg_dict = {}
         self.__stat_timer = global_config["STATISTICS_TIMER"]
     def log(self, what, level=logging_tools.LOG_LEVEL_OK, dst="log", **kwargs):
-        if dst in self.__handles:
-            cur_dst = self.__handles[dst]
-            # check for open handles
-            if dst != "log":
-                for cur_handle in cur_dst.handlers:
-                    if not os.path.exists(cur_handle.baseFilename):
-                        self.log(
-                            "reopening file %s for %s" % (
-                                cur_handle.baseFilename,
-                                dst))
-                        cur_handle.stream = cur_handle._open()
-            # print dir(cur_dst)
-            if "src_thread" in kwargs or "src_process" in kwargs:
-                # build record to log src_thread
-                cur_record = logging.makeLogRecord({
-                    "threadName" : kwargs.get("src_thread", kwargs.get("src_process", "???")),
-                    "process"    : kwargs.get("src_pid", 0),
-                    "msg"        : what,
-                    "levelno"    : level,
-                    "levelname"  : logging_tools.get_log_level_str(level)})
-                cur_dst.handle(cur_record)
+        if not self["exit_requested"]:
+            if dst in self.__handles:
+                cur_dst = self.__handles[dst]
+                # check for open handles
+                if dst != "log":
+                    for cur_handle in cur_dst.handlers:
+                        if not os.path.exists(cur_handle.baseFilename):
+                            self.log(
+                                "reopening file %s for %s" % (
+                                    cur_handle.baseFilename,
+                                    dst))
+                            cur_handle.stream = cur_handle._open()
+                # print dir(cur_dst)
+                if "src_thread" in kwargs or "src_process" in kwargs:
+                    # build record to log src_thread
+                    cur_record = logging.makeLogRecord({
+                        "threadName" : kwargs.get("src_thread", kwargs.get("src_process", "???")),
+                        "process"    : kwargs.get("src_pid", 0),
+                        "msg"        : what,
+                        "levelno"    : level,
+                        "levelname"  : logging_tools.get_log_level_str(level)})
+                    cur_dst.handle(cur_record)
+                else:
+                    cur_dst.log(level, what) # , extra={"threadName" : kwargs.get("src_thread", "bla")})
             else:
-                cur_dst.log(level, what) # , extra={"threadName" : kwargs.get("src_thread", "bla")})
+                self.__log_cache.append((dst, what, level))
         else:
-            self.__log_cache.append((dst, what, level))
+            logging_tools.my_syslog(what, level)
+    def _startup_error(self, src_name, src_pid, num_errors):
+        self.log("%s during startup, exiting" % (logging_tools.get_plural("bind error", num_errors)),
+                 logging_tools.LOG_LEVEL_ERROR)
+        self._int_error("bind problem")
+    def _int_error(self, err_cause):
+        if self["exit_requested"]:
+            self.log("exit already requested, ignoring", logging_tools.LOG_LEVEL_WARN)
+        else:
+            self.log("exit requested", logging_tools.LOG_LEVEL_WARN)
+            self["exit_requested"] = True
+    def _log_config(self):
+        self.log("Config info:")
+        for line, log_level in global_config.get_log(clear=True):
+            self.log(" - clf: [%d] %s" % (log_level, line))
+        conf_info = global_config.get_config_info()
+        self.log("Found %d valid config-lines:" % (len(conf_info)))
+        for conf in conf_info:
+            self.log("Config : %s" % (conf))
+    def _remove_handles(self):
+        any_removed = False
+        for act_hname in self.__open_handles:
+            if os.path.exists(act_hname):
+                self.log("removing previous handle %s" % (act_hname))
+                os.unlink(act_hname)
+                any_removed = True
+        if any_removed:
+            time.sleep(0.5)
+    def _init_network_sockets(self):
+        self.__open_handles = [io_stream_helper.zmq_socket_name(global_config[h_name]) for h_name in ["LOG_HANDLE", "ERR_HANDLE", "OUT_HANDLE"]] + \
+            [global_config[h_name] for h_name in ["LOG_HANDLE", "ERR_HANDLE", "OUT_HANDLE"]]
+        self._remove_handles()
+        client = self.zmq_context.socket(zmq.PULL)
+        for h_name in ["LOG_HANDLE", "ERR_HANDLE", "OUT_HANDLE"]:
+            client.bind(io_stream_helper.zmq_socket_name(global_config[h_name], check_ipc_prefix=True))
+            os.chmod(io_stream_helper.zmq_socket_name(global_config[h_name]), stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+        self.register_poller(client, zmq.POLLIN, self._recv_data)
+        self.std_client = client
+    def process_start(self, src_process, src_pid):
+        process_tools.append_pids("logserver/logserver", src_pid, mult=3)
+        if self.__msi_block:
+            self.__msi_block.add_actual_pid(src_pid, mult=3, process_name=src_process)
+            self.__msi_block.save_block()
+    def _init_msi_block(self):
+        process_tools.save_pids("logserver/logserver", mult=3)
+        if not self.__options.DEBUG:
+            self.log("Initialising meta-server-info block")
+            msi_block = process_tools.meta_server_info("logserver")
+            msi_block.add_actual_pid(mult=3, process_name="main")
+            msi_block.start_command = "/etc/init.d/logging-server start"
+            msi_block.stop_command = "/etc/init.d/logging-server force-stop"
+            msi_block.kill_pids = True
+            msi_block.save_block()
+        else:
+            msi_block = None
+        self.__msi_block = msi_block
+    def loop_end(self):
+        self._check_error_dict(force=True)
+        self.__num_write += 3
+        self.log("closing %d handles" % (len(self.__handles.keys())))
+        self.log("logging process exiting (pid %d)" % (self.pid))
+        self.log("statistics (open/close/written): %d / %d / %d" % (self.__num_open, self.__num_close, self.__num_write))
+        key_list = list(self.__handles.keys())
+        for close_key in key_list:
+            self.remove_handle(close_key)
+    def loop_post(self):
+        self._remove_handles()
+        process_tools.delete_pid("logserver/logserver")
+        if self.__msi_block:
+            self.__msi_block.remove_meta_block()
+        self.std_client.close()
     def _flush_log_cache(self):
         for dst, what, level in self.__log_cache:
             self.log(what, level, dst=dst)
@@ -225,11 +307,6 @@ class log_receiver(threading_tools.process_obj):
         if c_handles:
             self.log("temporarily closing %s: %s" % (logging_tools.get_plural("handle", len(c_handles)),
                                                      ", ".join(c_handles)))
-        # check for close
-        # c_handles = []
-        # for key in self.__handles.keys():
-        #    if not os.path.isdir("/proc/%d" % (self.__handles[key].process_id)):
-        #        c_handles.append(key)
         for c_handle in c_handles:
             self.remove_handle(c_handle)
         self._check_error_dict()
@@ -269,9 +346,10 @@ class log_receiver(threading_tools.process_obj):
                 record.host = process_tools.get_machine_name()
             sub_dirs = [record.host]
             record_host = record.host
-            record_name, record_process, record_parent_process = (record.name,
-                                                                  record.process,
-                                                                  getattr(record, "ppid", 0))
+            record_name, record_process, record_parent_process = (
+                record.name,
+                record.process,
+                getattr(record, "ppid", 0))
         init_logger = record_name.startswith("init.at.")
         if init_logger:
             # init.at logger, create subdirectories
@@ -353,7 +431,9 @@ class log_receiver(threading_tools.process_obj):
                 base_dir,
                 logging_tools.get_plural("handle", len(self.__handles.keys()))))
         return self.__handles[h_name]
-    def _log_recv(self, in_str, **kwargs):
+    def _recv_data(self, zmq_socket):
+        # zmq_socket.recv()
+        in_str = zmq_socket.recv()
         self.any_message_received()
         # print "received from %s: %s" % (str(addr), str(data))
         # self.transport.write("ok")
@@ -431,108 +511,8 @@ class log_receiver(threading_tools.process_obj):
             else:
                 # error_dict
                 pass
-    def loop_end(self):
-        self._check_error_dict(force=True)
-        self.__num_write += 3
-        self.log("closing %d handles" % (len(self.__handles.keys())))
-        self.log("logging process exiting (pid %d)" % (self.pid))
-        self.log("statistics (open/close/written): %d / %d / %d" % (self.__num_open, self.__num_close, self.__num_write))
-        key_list = list(self.__handles.keys())
-        for close_key in key_list:
-            self.remove_handle(close_key)
 
-class main_process(threading_tools.process_pool):
-    def __init__(self, options):
-        self.__options = options
-        threading_tools.process_pool.__init__(self, "main", stack_size=2 * 1024 * 1024, zmq=True, zmq_debug=global_config["ZMQ_DEBUG"])
-        process_tools.delete_lockfile(global_config["LOCKFILE_NAME"])
-        self.register_exception("int_error", self._int_error)
-        self.register_exception("term_error", self._int_error)
-        self.register_func("startup_error", self._startup_error)
-        self.renice()
-        self._init_msi_block()
-        self.add_process(log_receiver("receiver", priority=50), start=True)
-        self._log_config()
-        self._init_network_sockets()
-        self.register_timer(self._update, 60)
-    def log(self, what, level=logging_tools.LOG_LEVEL_OK):
-        # print(what)
-        if not self["exit_requested"]:
-            pass
-            # self.send_to_process("receiver", "log", what, level)
-        else:
-            logging_tools.my_syslog(what, level)
-    def _startup_error(self, src_name, src_pid, num_errors):
-        self.log("%s during startup, exiting" % (logging_tools.get_plural("bind error", num_errors)),
-                 logging_tools.LOG_LEVEL_ERROR)
-        self._int_error("bind problem")
-    def _int_error(self, err_cause):
-        if self["exit_requested"]:
-            self.log("exit already requested, ignoring", logging_tools.LOG_LEVEL_WARN)
-        else:
-            self.log("exit requested", logging_tools.LOG_LEVEL_WARN)
-            self["exit_requested"] = True
-    def _log_config(self):
-        self.log("Config info:")
-        for line, log_level in global_config.get_log(clear=True):
-            self.log(" - clf: [%d] %s" % (log_level, line))
-        conf_info = global_config.get_config_info()
-        self.log("Found %d valid config-lines:" % (len(conf_info)))
-        for conf in conf_info:
-            self.log("Config : %s" % (conf))
-    def _remove_handles(self):
-        any_removed = False
-        for act_hname in self.__open_handles:
-            if os.path.exists(act_hname):
-                self.log("removing previous handle %s" % (act_hname))
-                os.unlink(act_hname)
-                any_removed = True
-        if any_removed:
-            time.sleep(0.5)
-    def _init_network_sockets(self):
-        self.__open_handles = [io_stream_helper.zmq_socket_name(global_config[h_name]) for h_name in ["LOG_HANDLE", "ERR_HANDLE", "OUT_HANDLE"]] + \
-            [global_config[h_name] for h_name in ["LOG_HANDLE", "ERR_HANDLE", "OUT_HANDLE"]]
-        self._remove_handles()
-        client = self.zmq_context.socket(zmq.PULL)
-        for h_name in ["LOG_HANDLE", "ERR_HANDLE", "OUT_HANDLE"]:
-            client.bind(io_stream_helper.zmq_socket_name(global_config[h_name], check_ipc_prefix=True))
-            os.chmod(io_stream_helper.zmq_socket_name(global_config[h_name]), stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
-        self.register_poller(client, zmq.POLLIN, self._recv_data)
-        self.std_client = client
-    def _update(self):
-        self.send_to_process("receiver", "update")
-    def _recv_data(self, zmq_socket):
-        # zmq_socket.recv()
-        in_data = zmq_socket.recv()
-        self.send_to_process("receiver", "log_recv", in_data)
-    def process_start(self, src_process, src_pid):
-        process_tools.append_pids("logserver/logserver", src_pid, mult=3)
-        if self.__msi_block:
-            self.__msi_block.add_actual_pid(src_pid, mult=3, process_name=src_process)
-            self.__msi_block.save_block()
-    def _init_msi_block(self):
-        process_tools.save_pids("logserver/logserver", mult=3)
-        process_tools.append_pids("logserver/logserver", pid=configfile.get_manager_pid(), mult=3)
-        if not self.__options.DEBUG:
-            self.log("Initialising meta-server-info block")
-            msi_block = process_tools.meta_server_info("logserver")
-            msi_block.add_actual_pid(mult=3, process_name="main")
-            msi_block.add_actual_pid(act_pid=configfile.get_manager_pid(), mult=3, process_name="manager")
-            msi_block.start_command = "/etc/init.d/logging-server start"
-            msi_block.stop_command = "/etc/init.d/logging-server force-stop"
-            msi_block.kill_pids = True
-            msi_block.save_block()
-        else:
-            msi_block = None
-        self.__msi_block = msi_block
-    def loop_post(self):
-        self._remove_handles()
-        process_tools.delete_pid("logserver/logserver")
-        if self.__msi_block:
-            self.__msi_block.remove_meta_block()
-        self.std_client.close()
-
-global_config = configfile.get_global_config("logging-server")
+global_config = configfile.get_global_config("logging-server", single_process=True)
 
 def main():
     long_host_name, _mach_name = process_tools.get_fqdn()
@@ -562,7 +542,7 @@ def main():
     global_config.parse_file()
     options = global_config.handle_commandline(description="logging server, version is %s" % (version.VERSION_STRING))
     if global_config["KILL_RUNNING"]:
-        process_tools.kill_running_processes(exclude=configfile.get_manager_pid())
+        process_tools.kill_running_processes()
     # daemon has to be a local variable, otherwise system startup can be severly damaged
     lockfile_name = global_config["LOCKFILE_NAME"]
     # attention: global_config is not longer present after the TERM signal
@@ -575,9 +555,9 @@ def main():
     global_config.write_file()
     # process_tools.renice()
     # not very beautiful ...
-    configfile.enable_config_access(global_config["USER"], global_config["GROUP"])
+    # configfile.enable_config_access(global_config["USER"], global_config["GROUP"])
     process_tools.change_user_group(global_config["USER"], global_config["GROUP"])
-    global_config.set_uid_gid(global_config["USER"], global_config["GROUP"])
+    # global_config.set_uid_gid(global_config["USER"], global_config["GROUP"])
     process_tools.create_lockfile(global_config["LOCKFILE_NAME"])
     if not options.DEBUG:
         process_tools.become_daemon(mother_hook=process_tools.wait_for_lockfile,
