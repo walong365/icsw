ce64a85b2da8dcfcc1ffd1a81613130fefa36296
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-02-15 11:59:20 +0100

    improved dist display

--- initat/cluster/frontend/templates/angular/monitoring_build_info.cs
+++ initat/cluster/frontend/templates/angular/monitoring_build_info.cs
@@ -17,7 +17,23 @@ monitoring_build_info_module.controller("info_ctrl", ["$scope", "$compile", "$fi
         access_level_service.install($scope)
         $scope.pagSettings = paginatorSettings.get_paginator("masters", $scope)
         $scope.masters = []
-        $scope.slaves = []
+        $scope.reload = () ->
+            wait_list = restDataSource.add_sources([
+                ["{% url 'rest:mon_dist_master_list' %}", {}]
+                #["{% url 'rest:mon_dist_slave_list' %}", {}]
+                ["{% url 'rest:device_tree_list' %}", {"all_monitoring_servers" : true}]
+            ])
+            $q.all(wait_list).then((data) ->
+                $scope.masters = data[0]
+                slave_list = []
+                for master in $scope.masters
+                    for slave in master.mon_dist_slave_set
+                        if slave.device not in slave_list
+                            slave_list.push(slave.device)
+                $scope.all_slaves = slave_list
+                $scope.servers = build_lut(data[1])
+                console.log $scope.servers
+            )
         $scope.get_diff_time = (dt) ->
             if dt
                 return moment(dt).fromNow()
@@ -28,30 +44,30 @@ monitoring_build_info_module.controller("info_ctrl", ["$scope", "$compile", "$fi
                 return moment(dt).format(DT_FORM)
             else
                 return "---"
+        $scope._runtime = (diff) ->
+            if diff
+                # seconds
+                return diff + "s"
+            else
+                return "< 1s"
         $scope.get_runtime = (master) ->
             if master.build_start and master.build_end
-                diff = moment(master.build_end).diff(moment(master.build_start), "seconds")
-                if diff
-                    # seconds
-                    return diff
-                else
-                    return "< 1"
+                return $scope._runtime(moment(master.build_end).diff(moment(master.build_start), "seconds"))
             else
                 return "---"
-        $scope.reload = () ->
-            wait_list = restDataSource.add_sources([
-                ["{% url 'rest:mon_dist_master_list' %}", {}]
-                #["{% url 'rest:mon_dist_slave_list' %}", {}]
-                ["{% url 'rest:device_tree_list' %}", {"all_monitoring_servers" : true}]
-            ])
-            $q.all(wait_list).then((data) ->
-                $scope.masters = data[0]
-                for master in $scope.masters
-                    console.log master.mon_dist_slave_set
-                #$scope.slaves = data[1]
-                $scope.servers = build_lut(data[1])
-                console.log $scope.servers
-            )
+        $scope.get_slaves = (build) ->
+            return ($scope.get_slave(build, idx) for idx in $scope.all_slaves)
+        $scope.get_slave = (build, idx) ->
+            _list = (entry for entry in build.mon_dist_slave_set when entry.device == idx)
+            return if _list.length then _list[0] else undefined 
+        $scope.get_sync_time = (slave) ->
+            if slave
+                if slave.sync_end and slave.sync_start
+                    return $scope._runtime(moment(slave.sync_end).diff(moment(slave.sync_start), "seconds"))
+                else
+                    return "---"
+            else
+                return "---"
         $scope.reload()
 ])
 
