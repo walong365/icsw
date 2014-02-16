6daa3e26fb2a8c6f03dcf069f2d95a954a8033a1
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-02-16 08:10:21 +0100

    updatede show_config_script, delete old stuff

--- cluster/bin/show_config_script.py
+++ cluster/bin/show_config_script.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python-init -Otu
 #
-# Copyright (C) 2001,2002,2003,2004,2005,2006 Andreas Lang
+# Copyright (C) 2001-2006,2014 Andreas Lang-Nevyjel
 #
 # this file is part of cluster-backbone
 #
-# Send feedback to: <lang@init.at>
-# 
+# Send feedback to: <lang-nevyjel@init.at>
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License Version 2 as
 # published by the Free Software Foundation.
@@ -20,38 +20,38 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-import sys
+""" show config script for simple use in CSW """
+
+import argparse
+import datetime
 import os
-import os.path
+import process_tools
 
 def main():
-    opt_dict = {"full_strip"    : False,
-                "remove_hashes" : False}
-    f_names = []
-    for f_name in sys.argv[1:]:
-        if f_name == "-s":
-            opt_dict["full_strip"] = True
-        elif f_name == "-c":
-            opt_dict["remove_hashes"] = True
-        elif f_name == "-h":
-            print "Usage: %s [ARGS] Files" % (os.path.basename(sys.argv[0]))
-            print "where ARGS is one or more of"
-            print " -h      this help"
-            print " -s      strip files (remove empty lines)"
-            print " -c      remove hashes from config files"
-            sys.exit(0)
-        else:
-            f_names.append(f_name)
-    for f_name in f_names:
-        obj_name = "%s_object" % (os.path.basename(f_name.replace(".", "_").replace("-", "_")))
-        f_lines = [x.rstrip() for x in file(f_name).read().split("\n") if x]
-        if opt_dict["full_strip"]:
-            f_lines = [x.strip() for x in f_lines]
-        if opt_dict["remove_hashes"]:
-            f_lines = [x for x in f_lines if not x.startswith("#")]
+    my_parser = argparse.ArgumentParser()
+    my_parser.add_argument("-s", dest="full_strip", default=False, action="store_true", help="strip all empty lines from file [%(default)s]")
+    my_parser.add_argument("-c", dest="remove_hashes", default=False, action="store_true", help="remove all lines starting with hashes from file [%(default)s]")
+    my_parser.add_argument("--full-path", dest="full_path", default=False, action="store_true", help="use full path for file objects [%(default)s]")
+    my_parser.add_argument("files", nargs="+", help="files to operate on")
+    options = my_parser.parse_args()
+    for f_name in options.files:
+        _obj_name = f_name if options.full_path else os.path.basename(f_name)
+        for _rc in ["/", ".", "-"]:
+            _obj_name = _obj_name.replace(_rc, "_")
+        while _obj_name.startswith("_"):
+            _obj_name = _obj_name[1:]
+        obj_name = "%s_object" % (_obj_name)
+        f_lines = [_line.rstrip() for _line in file(f_name).read().split("\n")]
+        if options.full_strip:
+            f_lines = [_line.strip() for _line in f_lines if _line.strip()]
+        if options.remove_hashes:
+            f_lines = [_line for _line in f_lines if not _line.startswith("#")]
+        p_line = " " * 4
+        print "# from %s (%s, host %s)" % (f_name, datetime.datetime.now(), process_tools.get_machine_name(short=False))
         print "%s = config.add_file_object('%s')" % (obj_name, f_name)
-        p_line = " " * (len(obj_name) + 5)
-        print "%s += [%s]" % (obj_name, (",\n%s" % (p_line)).join(["'%s'" % (x.replace("'", '"').replace("\\", "\\\\")) for x in f_lines]))
+        print "%s += [\n%s]\n" % (
+            obj_name,
+            "".join(["%s'%s',\n" % (p_line, _line.replace("'", '"').replace("\\", "\\\\")) for _line in f_lines]))
 
 if __name__ == "__main__":
     main()
