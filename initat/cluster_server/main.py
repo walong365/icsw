d2b537a87e318c524c604d4a62bbbf1668c5ef11
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-02-17 16:12:02 +0100

    changed default path

--- initat/cluster_server/main.py
+++ initat/cluster_server/main.py
@@ -87,7 +87,7 @@ def main():
         _log_lines = process_tools.kill_running_processes(prog_name + ".py", exclude=configfile.get_manager_pid())
     cluster_location.read_config_from_db(global_config, "server", [
         ("COM_PORT"             , configfile.int_c_var(SERVER_PORT)),
-        ("IMAGE_SOURCE_DIR"     , configfile.str_c_var("/usr/local/share/cluster/images")),
+        ("IMAGE_SOURCE_DIR"     , configfile.str_c_var("/opt/cluster/system/images")),
         # ("UPDATE_SITE"          , configfile.str_c_var("http://www.initat.org/cluster/RPMs/")),
         ("MAILSERVER"           , configfile.str_c_var("localhost")),
         ("FROM_NAME"            , configfile.str_c_var("quotawarning")),
