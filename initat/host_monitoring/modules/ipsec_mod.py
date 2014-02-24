d2f8979b4b75a16ce3056c5b5a3bbf2669331a38
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-02-24 11:27:31 +0100

    check ipsec-tunnel for strongswan 5.x.y

--- initat/host_monitoring/modules/ipsec_mod.py
+++ initat/host_monitoring/modules/ipsec_mod.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python-init -Ot
 #
-# Copyright (C) 2008,2009,2012,2013 Andreas Lang-Nevyjel init.at
+# Copyright (C) 2008-2009,2012-2014 Andreas Lang-Nevyjel init.at
 #
 # Send feedback to: <lang-nevyjel@init.at>
 #
@@ -18,22 +18,21 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
+from initat.host_monitoring import limits, hm_classes
 import commands
 import logging_tools
 import os
+import time
+import re
+import process_tools
 import pprint
 import sys
-from initat.host_monitoring import limits, hm_classes
 
 class _general(hm_classes.hm_module):
     def init_module(self):
         self._find_ipsec_command()
     def _find_ipsec_command(self):
-        self.__ipsec_command = ""
-        for s_dir in ["/bin", "/usr/bin", "/sbin", "/usr/sbin"]:
-            if os.path.isfile("%s/ipsec" % (s_dir)):
-                self.__ipsec_command = "%s/ipsec" % (s_dir)
-                break
+        self.__ipsec_command = process_tools.find_file("ipsec")
     def _exec_command(self, com):
         if com.startswith("."):
             if self.__ipsec_command:
@@ -53,40 +52,142 @@ class _general(hm_classes.hm_module):
         # for strongswan
         act_out = self._exec_command(". statusall")
         con_dict = {}
-        for line in act_out:
-            parts = line.strip().split()
-            if len(parts) > 1:
-                parts.pop(0)
-                first_key = parts.pop(0)
-                if first_key.startswith('"'):
-                    # connection related
-                    con_key = first_key[1:-2]
-                    con_dict.setdefault(con_key, {"flags" : [],
-                                                  "keys" : {},
-                                                  "sa_dict" : {}})
-                    parts = [part.strip() for part in (" ".join(parts)).split(";") if part.strip()]
-                    for part in parts:
-                        if part.count(": "):
-                            key, value = part.split(": ", 1)
-                            if key.endswith(" proposal") and value.replace("N/A", "N_A").count("/") == 2:
-                                value = [sub_value.replace("N_A", "N/A") for sub_value in value.replace("N/A", "N_A").split("/")]
-                            elif key == "prio" and value.count(",") == 1:
-                                value = value.split(",")
-                            con_dict[con_key]["keys"][key] = value
-                        else:
-                            con_dict[con_key]["flags"].append(part)
-                elif first_key.startswith("#"):
-                    sa_key = int(first_key[1:-1])
-                    con_key = parts.pop(0)
-                    if con_key not in ["pending"]:
-                        if con_key.count(":"):
-                            con_key, port_num = con_key.split(":")
-                        else:
-                            port_num = "0"
-                        con_key, port_num = (con_key[1:-1], int(port_num))
-                        parts = [part.strip() for part in (" ".join(parts)).split(";") if part.strip()]
-                        con_dict[con_key]["sa_dict"].setdefault(("con_%d.%d" % (sa_key, port_num)), []).extend(parts)
+        if act_out:
+            first_line = act_out[0]
+            if first_line.lower().count("strongswan 5"):
+                con_dict["lines"] = act_out
+            else:
+                for line in act_out:
+                    parts = line.strip().split()
+                    if len(parts) > 1:
+                        parts.pop(0)
+                        first_key = parts.pop(0)
+                        if first_key.startswith('"'):
+                            # connection related
+                            con_key = first_key[1:-2]
+                            con_dict.setdefault(con_key, {"flags" : [],
+                                                          "keys" : {},
+                                                          "sa_dict" : {}})
+                            parts = [part.strip() for part in (" ".join(parts)).split(";") if part.strip()]
+                            for part in parts:
+                                if part.count(": "):
+                                    key, value = part.split(": ", 1)
+                                    if key.endswith(" proposal") and value.replace("N/A", "N_A").count("/") == 2:
+                                        value = [sub_value.replace("N_A", "N/A") for sub_value in value.replace("N/A", "N_A").split("/")]
+                                    elif key == "prio" and value.count(",") == 1:
+                                        value = value.split(",")
+                                    con_dict[con_key]["keys"][key] = value
+                                else:
+                                    con_dict[con_key]["flags"].append(part)
+                        elif first_key.startswith("#"):
+                            sa_key = int(first_key[1:-1])
+                            con_key = parts.pop(0)
+                            if con_key not in ["pending"]:
+                                if con_key.count(":"):
+                                    con_key, port_num = con_key.split(":")
+                                else:
+                                    port_num = "0"
+                                con_key, port_num = (con_key[1:-1], int(port_num))
+                                parts = [part.strip() for part in (" ".join(parts)).split(";") if part.strip()]
+                                con_dict[con_key]["sa_dict"].setdefault(("con_%d.%d" % (sa_key, port_num)), []).extend(parts)
         return con_dict
+    def init_machine_vector(self, mv):
+        self.__ipsec_re = re.compile("^\s*(?P<conn_name>\S+){(?P<conn_id>\d+)}:\s+.*\s+(?P<bytes_in>\d+)\s+bytes_i.*\s+(?P<bytes_out>\d+)\s+bytes_o.*$")
+        self.__ipsec_conns = {}
+    def update_machine_vector(self, mv):
+        if self.__ipsec_command:
+            _lines = [(line, self.__ipsec_re.match(line)) for line in self._exec_command(". statusall") if line.count("bytes_i")]
+            _found = set()
+            for _line, _res in _lines:
+                if _res:
+                    _gd = _res.groupdict()
+                    _cn = _gd["conn_name"]
+                    _found.add(_cn)
+                    if _cn not in self.__ipsec_conns:
+                        self.log("registered IPSec connection %s" % (_cn))
+                        self.__ipsec_conns[_cn] = ipsec_con(_cn, mv)
+                    self.__ipsec_conns[_cn].feed(mv, int(_gd["bytes_in"]), int(_gd["bytes_out"]))
+                else:
+                    self.log("cannot parse line '%s'" % (line), logging_tools.LOG_LEVEL_WARN)
+            _to_del = set(self.__ipsec_conns.keys()) - _found
+            for _del in _to_del:
+                self.log("unregistered IPSec connection %s" % (_del))
+                self.__ipsec_conns[_del].close(mv)
+                del self.__ipsec_conns[_del]
+
+class ipsec_con(object):
+    def __init__(self, name, mv):
+        self.name = name
+        mv.register_entry(self.key("in"), 0, "bytes received for connection %s" % (self.name), "Byte/s", 1024)
+        mv.register_entry(self.key("out"), 0, "bytes transmitted for connection %s" % (self.name), "Byte/s", 1024)
+        self.__in, self.__out = (0, 0)
+        self.__last = None
+    def feed(self, mv, _in, _out):
+        cur_time = time.time()
+        if self.__last:
+            diff_time = max(1, cur_time - self.__last)
+            mv[self.key("in")] = (max(0, _in - self.__in) / diff_time)
+            mv[self.key("out")] = (max(0, _out - self.__out) / diff_time)
+        self.__last = cur_time
+        self.__in, self.__out = (_in, _out)
+    def key(self, key):
+        return "net.ipsec.%s.%s" % (self.name, key)
+    def close(self, mv):
+        mv.unregister_entry(self.key("in"))
+        mv.unregister_entry(self.key("out"))
+
+class ipsec_tunnel(object):
+    def __init__(self, t_id):
+        self.t_id = t_id
+        self.__parts = []
+        self.__installed = False
+    def feed(self, _parts):
+        _line = " ".join(_parts)
+        if _line.count("INSTALLED"):
+            self.__installed = True
+        self.__parts.append(_line)
+        # print "*", self.t_id, self.__parts
+    def is_ok(self):
+        return self.__installed
+
+class c_con(object):
+    # ipsec client connection
+    def __init__(self, name):
+        self.name = name
+        self.__master_connection = None
+        self.__con_dict = {}
+        self.__feedcount = 0
+        self.__tunnels = {}
+    @property
+    def con_dict(self):
+        return self.__con_dict
+    def get_master(self):
+        if self.__master_connection:
+            return self.__master_connection.get_master()
+        else:
+            return self
+    def feed(self, _parts, _prev_con):
+        _mode = _parts[0][:-1]
+        if _mode == "child" and self.__feedcount == 0:
+            # this connection lives inside another connection
+            self.__master_connection = _prev_con.get_master()
+        self.__feedcount += 1
+        if _mode in ["local", "remote"]:
+            self.__con_dict[_mode] = _parts[1:]
+        elif _mode in ["child"]:
+            self.__con_dict.setdefault(_mode, []).append(_parts[1:])
+        else:
+            self.__con_dict.setdefault("info", []).append(_parts)
+    def feed_tunnel(self, _id, _parts):
+        if _id not in self.__tunnels:
+            self.__tunnels[_id] = ipsec_tunnel(_id)
+        self.__tunnels[_id].feed(_parts)
+    def tunnel_ok(self):
+        if self.__tunnels:
+            return any([_tunnel.is_ok() for _tunnel in self.__tunnels.itervalues()])
+        else:
+            # no tunnels
+            return False
 
 class ipsec_status_command(hm_classes.hm_command):
     def __init__(self, name):
@@ -104,35 +205,96 @@ class ipsec_status_command(hm_classes.hm_command):
             first_arg = cur_ns.arguments[0]
         else:
             first_arg = None
-        if not first_arg:
-            # overview
-            if con_dict:
-                ret_state, ret_list = (limits.nag_STATE_OK, [])
-                for con_name in sorted(con_dict):
-                    if "erouted" in con_dict[con_name]["flags"]:
-                        ret_list.append("%s ok" % (con_name))
+        if "lines" in con_dict:
+            _con_dict = {}
+            # new version
+            line_mode = "v"
+            for line_num, line in enumerate(con_dict["lines"]):
+                if line_num == 0:
+                    line_mode = "v"
+                elif line.startswith("Listening"):
+                    line_mode = "l"
+                elif line.startswith("Connections:"):
+                    cur_con = None
+                    line_mode = "c"
+                elif line.startswith("Security Ass"):
+                    cur_con = None
+                    line_mode = "s"
+                else:
+                    if line_mode in ["c", "s"]:
+                        _parts = line.strip().split()
+                        if line_mode == "c":
+                            conn_name = _parts.pop(0)[:-1]
+                            if not cur_con:
+                                prev_con = cur_con
+                                cur_con = c_con(conn_name)
+                                _con_dict[conn_name] = cur_con
+                            elif cur_con.name != conn_name:
+                                prev_con = cur_con
+                                cur_con = c_con(conn_name)
+                                _con_dict[conn_name] = cur_con
+                            cur_con.feed(_parts, prev_con)
+                        else:
+                            if line.count("{"):
+                                conn_id = _parts.pop(0)
+                                conn_name = conn_id.split("{")[0]
+                                tunnel_id = conn_id.split("{")[1].split("}")[0]
+                                _con_dict[conn_name].feed_tunnel(tunnel_id, _parts)
+            if not first_arg:
+                if _con_dict:
+                    ret_state, ret_list = (limits.nag_STATE_OK, [])
+                    for con_name in sorted(_con_dict):
+                        if _con_dict[con_name].tunnel_ok():
+                            ret_list.append("%s ok" % (con_name))
+                        else:
+                            ret_list.append("no installed tunnel for %s" % (con_name))
+                            ret_state = max(ret_state, limits.nag_STATE_CRITICAL)
+                    return ret_state, ", ".join(ret_list)
+                else:
+                    return limits.nag_STATE_WARNING, "no connections defined"
+            else:
+                if first_arg in _con_dict:
+                    _conn = _con_dict[first_arg]
+                    if _conn.tunnel_ok():
+                        return limits.nag_STATE_OK, "connection %s is defined and tunnel is installed" % (_conn.name)
                     else:
-                        ret_list.append("%s is not erouted" % (con_name))
-                        ret_state = max(ret_state, limits.nag_STATE_CRITICAL)
-                return ret_state, ", ".join(ret_list)
-            else:
-                return limits.nag_STATE_WARNING, "no connections defined"
-        elif con_dict.has_key(first_arg):
-            con_stuff = con_dict[first_arg]
-            ret_state, ret_list = (limits.nag_STATE_OK, [])
-            if "erouted" in con_stuff["flags"]:
-                ret_list.append("is erouted")
-                for key in con_stuff["keys"]:
-                    if key.endswith("proposal"):
-                        ret_list.append("%s: %s" % (key, "/".join(con_stuff["keys"][key])))
-            else:
-                ret_list.append("is not erouted")
-                ret_state = max(ret_state, limits.nag_STATE_CRITICAL)
-            return ret_state, "connection %s: %s" % (first_arg,
-                                                     ", ".join(ret_list))
+                        return limits.nag_STATE_CRITICAL, "connection %s is defined but no tunnel installed" % (_conn.name)
+                else:
+                    return limits.nag_STATE_CRITICAL, "connection '%s' not found (defined: %s)" % (
+                        first_arg,
+                        ", ".join(sorted(_con_dict)) or "none")
         else:
-            return limits.nag_STATE_CRITICAL, "error connection '%s' not found (defined: %s)" % (first_arg,
-                                                                                                 ", ".join(sorted(con_dict)) or "none")
+            # old strongswans
+            if not first_arg:
+                # overview
+                if con_dict:
+                    ret_state, ret_list = (limits.nag_STATE_OK, [])
+                    for con_name in sorted(con_dict):
+                        if "erouted" in con_dict[con_name]["flags"]:
+                            ret_list.append("%s ok" % (con_name))
+                        else:
+                            ret_list.append("%s is not erouted" % (con_name))
+                            ret_state = max(ret_state, limits.nag_STATE_CRITICAL)
+                    return ret_state, ", ".join(ret_list)
+                else:
+                    return limits.nag_STATE_WARNING, "no connections defined"
+            elif con_dict.has_key(first_arg):
+                con_stuff = con_dict[first_arg]
+                ret_state, ret_list = (limits.nag_STATE_OK, [])
+                if "erouted" in con_stuff["flags"]:
+                    ret_list.append("is erouted")
+                    for key in con_stuff["keys"]:
+                        if key.endswith("proposal"):
+                            ret_list.append("%s: %s" % (key, "/".join(con_stuff["keys"][key])))
+                else:
+                    ret_list.append("is not erouted")
+                    ret_state = max(ret_state, limits.nag_STATE_CRITICAL)
+                return ret_state, "connection %s: %s" % (first_arg,
+                                                         ", ".join(ret_list))
+            else:
+                return limits.nag_STATE_CRITICAL, "error connection '%s' not found (defined: %s)" % (
+                    first_arg,
+                    ", ".join(sorted(con_dict)) or "none")
 
 if __name__ == "__main__":
     print "This is a loadable module."
