38475341b554de7982315401c0eb73f74ba9a323
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-03-03 19:02:20 +0100

    minimal code cleanup

--- migrate_repos.py
+++ migrate_repos.py
@@ -1,12 +1,36 @@
 #!/usr/bin/python-init -Otu
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012-2014 Andreas Lang-Nevyjel, init.at
+#
+# Send feedback to: <lang-nevyjel@init.at>
+#
+# This file is part of python-modules-base
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License Version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FTNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+"""
+
+modify Repositories of OpenSUSE-bases distributions
+
+"""
 
 import argparse
-import logging_tools
 import os
-import pprint
 import process_tools
 import re
-import sys
 
 REPO_DIR = "/etc/zypp/repos.d"
 KV_RE = re.compile("^\s*(?P<key>\S+)\s*=\s*(?P<value>.*)\s*$")
@@ -97,7 +121,7 @@ def main():
     # transform repos
     for r_name in os.listdir(REPO_DIR):
         try:
-            cur_repo = repo(os.path.join(REPO_DIR, r_name), opts)
+            _cur_repo = repo(os.path.join(REPO_DIR, r_name), opts)
         except:
             print "error handling repo %s: %s" % (
                 r_name,
