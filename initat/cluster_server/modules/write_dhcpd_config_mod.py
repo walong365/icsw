b6da41eda994c35c8e4b98ca93062189b6fcea6b
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-03-22 21:01:09 +0100

    cleanup of write_dhcpd_config

--- initat/cluster_server/modules/write_dhcpd_config_mod.py
+++ initat/cluster_server/modules/write_dhcpd_config_mod.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python -Ot
 #
-# Copyright (C) 2007,2008,2012,2013 Andreas Lang-Nevyjel
+# Copyright (C) 2007-2008,2012-2014 Andreas Lang-Nevyjel
 #
 # Send feedback to: <lang-nevyjel@init.at>
 #
@@ -28,7 +28,6 @@ import os
 import pprint
 import process_tools
 import server_command
-import sys
 
 class write_dhcpd_config(cs_base_class.server_com):
     class Meta:
@@ -38,28 +37,33 @@ class write_dhcpd_config(cs_base_class.server_com):
         my_c = config_tools.server_check(server_type="mother_server")
         boot_ips = my_c.identifier_ip_lut.get("b", [])
         if len(boot_ips) > 1:
-            cur_inst.srv_com["result"].attrib.update({
-                "reply" : "error more than one boot-net found for '%s'" % (global_config["SERVER_SHORT_NAME"]),
-                "state" : "%d" % (server_command.SRV_REPLY_STATE_ERROR)})
+            cur_inst.srv_com.set_result(
+                "error more than one boot-net found for '%s'" % (global_config["SERVER_SHORT_NAME"]),
+                server_command.SRV_REPLY_STATE_ERROR)
         elif not boot_ips:
-            cur_inst.srv_com["result"].attrib.update({
-                "reply" : "error no boot-net found for '%s'" % (global_config["SERVER_SHORT_NAME"]),
-                "state" : "%d" % (server_command.SRV_REPLY_STATE_ERROR)})
+            cur_inst.srv_com.set_result(
+                "error no boot-net found for '%s'" % (global_config["SERVER_SHORT_NAME"]),
+                server_command.SRV_REPLY_STATE_ERROR)
         else:
             boot_ip = boot_ips[0]
             boot_net = boot_ip.network
-            add_nets = list(network.objects.filter(Q(net_ip__netdevice__device=my_c.effective_device) &
-                                                   Q(network_type__identifier__in=["s", "p", "o"])))
-            if os.path.isdir("/etc/dhp3"):
-                dhcpd_f = file("/etc/dhcp3/dhcpd.conf", "w")
-            else:
-                dhcpd_f = file("/etc/dhcpd.conf", "w")
-            dhcpd_f.write("ddns-update-style none;\n")
-            dhcpd_f.write("omapi-port 7911;\n")
-            dhcpd_f.write("ddns-domainname \"%s\";\n" % (global_config["SERVER_SHORT_NAME"]))
-            dhcpd_f.write("allow booting;\nallow bootp;\n\n")
+            add_nets = list([(cur_net.network_type.identifier, cur_net) for cur_net in network.objects.exclude(
+                pk=boot_net.pk).filter(
+                Q(net_ip__netdevice__device=my_c.effective_device) &
+                Q(network_type__identifier__in=["s", "p", "o"])).distinct()])
+            add_nets = sum([[_sub_net for _value, _sub_net in add_nets if _value == _t_val] for _t_val in ["p", "s", "o"]], [])
+            dhcpd_c = [
+                "ddns-update-style none;",
+                "omapi-port 7911;",
+                "ddns-domainname \"%s\";" % (global_config["SERVER_SHORT_NAME"]),
+                "allow booting;\nallow bootp;",
+                ""
+            ]
             if cur_inst.srv_com["server_key:authoritative"].text.lower() in ["1", "true", "yes"]:
-                dhcpd_f.write("authoritative;\n")
+                dhcpd_c.extend([
+                    "authoritative;",
+                    "",
+                ])
             # get gateway and domain-servers for the various nets
             gw_pri, gateway = (-10000, "0.0.0.0")
             cur_dc = config_tools.device_with_config("%server%")
@@ -67,13 +71,9 @@ class write_dhcpd_config(cs_base_class.server_com):
             for act_net in [boot_net] + add_nets:
                 if act_net.gw_pri > gw_pri:
                     gw_pri, gateway = (act_net.gw_pri, act_net.gateway)
-
                 for key, configs, add_dict in [("domain-name-servers", ["name_server", "name_slave"], {}),
                                                ("ntp-servers", ["xntp_server"], {}),
                                                ("nis-servers", ["yp_server"], {"domainname" : "nis-domain"})]:
-# #                    dc.execute("SELECT d.name, i.ip FROM device d INNER JOIN netip i INNER JOIN netdevice n INNER JOIN device_config dc INNER JOIN new_config c INNER JOIN " + \
-# #                                           "device_group dg LEFT JOIN device d2 ON d2.device_idx=dg.device WHERE d.device_group=dg.device_group_idx AND n.device = d.device_idx AND i.netdevice=n.netdevice_idx AND " + \
-# #                                           "i.network=%d AND (dc.device=d.device_idx OR dc.device=d2.device_idx) AND dc.new_config=c.new_config_idx AND (%s)" % (act_net["network_idx"], " OR ".join(["c.name='%s'" % (x) for x in configs])))
                     found_confs = set(cur_dc.keys()) & set(configs)
                     if found_confs:
                         # some configs found
@@ -81,51 +81,53 @@ class write_dhcpd_config(cs_base_class.server_com):
                             for cur_srv in cur_dc[found_conf]:
                                 match_list = [cur_ip for cur_ip in cur_srv.ip_list if cur_ip.network.pk == act_net.pk]
                                 if match_list:
-                                    # FIXME: honor add_dict
-                                    # print cur_srv.config.config_str_set.all()
-# #                        for sql_key, dhcp_key in add_dict.iteritems():
-# #                            for tname in ["int", "str"]:
-# #                                sql_str = "SELECT cs.value FROM config_%s cs INNER JOIN new_config c INNER JOIN device_config dc INNER JOIN device d INNER JOIN device_group dg LEFT JOIN device d2 ON d2.device_idx=dg.device WHERE d.device_group=dg.device_group_idx AND cs.name='%s' AND cs.new_config=c.new_config_idx AND
-# # (%s) AND dc.new_config=c.new_config_idx AND (dc.device=d.device_idx OR dc.device=d2.device_idx) AND (%s)" % (tname, sql_key, " OR ".join(["c.name='%s'" % (x) for x in configs]), " OR ".join(["d.name='%s'" % (x) for x in [y[1] for y in hosts]]))
-# #                                dc.execute(sql_str)
-# #                                if dc.rowcount:
-# #                                    opts[dhcp_key] = dc.fetchone()["value"]
                                     found_dict.setdefault(act_net.pk, {}).setdefault(key, []).append((cur_srv.device, match_list))
-            # pprint.pprint(found_dict)
-# #                    if dc.rowcount:
-# #                        hosts, opts = ([(x["ip"], x["name"]) for x in dc.fetchall()], {})
-# #                        act_net[key] = {"hosts" : hosts, "opts" : opts}
-                # act_net["dns"] =
-            dhcpd_f.write("shared-network %s {\n" % (global_config["SERVER_SHORT_NAME"]))
-            dhcpd_f.write("  option routers %s;\n" % (gateway))
+            dhcpd_c.extend([
+                "shared-network %s {" % (global_config["SERVER_SHORT_NAME"]),
+                "    option routers %s;" % (gateway)
+                ])
             for act_net in [boot_net] + add_nets:
-                dhcpd_f.write("  subnet %s netmask %s {\n" % (act_net.network,
-                                                              act_net.netmask))
+                comment_sign = "" if act_net.network_type.identifier == "b" else "#"
+                dhcpd_c.extend([
+                    "",
+                    "    # network {} (identifier {})".format(unicode(act_net), act_net.network_type.identifier),
+                    "",
+                    "    {}subnet {} netmask {} {{".format(
+                        comment_sign,
+                        act_net.network,
+                        act_net.netmask)
+                ])
                 if act_net.network_type.identifier == "b":
-                    dhcpd_f.write("    authoritative ;\n")
-                dhcpd_f.write("    next-server %s;\n" % (my_c.identifier_ip_lut[act_net.network_type.identifier][0].ip))
+                    dhcpd_c.append("        {}authoritative;".format(comment_sign))
+                dhcpd_c.append("    {}    next-server {};".format(
+                    comment_sign,
+                    my_c.identifier_ip_lut[act_net.network_type.identifier][0].ip))
                 local_found_dict = found_dict.get(act_net.pk, {})
                 for key in ["domain-name-servers", "ntp-servers", "nis-servers"]:
                     if key in local_found_dict:
-                        dhcpd_f.write("    option %s %s;\n" % (key, ", ".join(["%s" % (cur_dev.name) for cur_dev, ip_list in local_found_dict[key]])))
-                        # FIXME
-                        # for o_key, o_value in act_net[key]["opts"].iteritems():
-                        #    dhcpd_f.write("    option %s %s;\n" % (o_key, o_value))
-                dhcpd_f.write("    server-identifier %s;\n" % (my_c.identifier_ip_lut[act_net.network_type.identifier][0].ip))
-                dhcpd_f.write("    option domain-name \"%s\";\n" % (act_net.name))
-                dhcpd_f.write("  }\n")
-            dhcpd_f.write("}\n")
-            # create netbotz-dhcp entries
-            nb_devs = net_ip.objects.filter(Q(netdevice__device__device_type__identifier="NB") & Q(netdevice__device__boot_server=my_c.effective_device))
-            if len(nb_devs):
-                dhcpd_f.write("\ngroup netbotz {\n")
-                for netbotz in nb_devs:
-                    dhcpd_f.write("  host %s {\n    hardware ethernet %s;\n    fixed-address %s;\n  }\n" % (
-                        netbotz.netdevice.device.name,
-                        netbotz.netdevice.macaddr,
-                        netbotz.ip))
-                dhcpd_f.write("}\n")
-            dhcpd_f.close()
+                        dhcpd_c.append("    {}option {} {};".format(
+                            comment_sign,
+                            key,
+                            ", ".join(["%s" % (cur_dev.name) for cur_dev, ip_list in local_found_dict[key]])))
+                dhcpd_c.extend([
+                    "    {}    server-identifier {};".format(
+                        comment_sign,
+                        my_c.identifier_ip_lut[act_net.network_type.identifier][0].ip),
+                    "    {}    option domain-name \"{}\";".format(
+                        comment_sign,
+                        act_net.name),
+                    "    {}}}".format(
+                        comment_sign
+                        )
+                    ])
+            dhcpd_c.extend([
+                "}",
+                "",
+            ])
+            if os.path.isdir("/etc/dhp3"):
+                file("/etc/dhcp3/dhcpd.conf", "w").write("\n".join(dhcpd_c))
+            else:
+                file("/etc/dhcpd.conf", "w").write("\n".join(dhcpd_c))
             ret_state = None
             for s_name in ["dhcp3-server", "dhcpd"]:
                 if os.path.isfile("/etc/init.d/%s" % (s_name)):
@@ -144,7 +146,7 @@ class write_dhcpd_config(cs_base_class.server_com):
                 ret_state, ret_str = (
                     server_command.SRV_REPLY_STATE_ERROR,
                     "error no valid dhcp-server found")
-            cur_inst.srv_com["result"].attrib.update({
-                "reply" : ret_str,
-                "state" : "%d" % (ret_state)})
-
+            cur_inst.srv_com.set_result(
+                ret_str,
+                ret_state,
+            )
