8d35d1d8388f7464928bbc178bfa7d30a28ff636
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-04-07 15:47:55 +0200

    test commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name="meta-server",
       version="2.0",
-      description="The meta-server part of the init(c) cluster software.",
+      description="The meta-server part of the INIT(c) cluster software.",
       url="http://www.init.at",
       author="Andreas Lang-Nevyjel",
       author_email="lang-nevyjel@init.at",
