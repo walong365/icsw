880e307221c2d6df594d2103a3507ba206fc9060
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-04-12 08:04:49 +0200

    improved show_config_script

--- cluster/bin/show_config_script.py
+++ cluster/bin/show_config_script.py
@@ -24,34 +24,54 @@
 
 import argparse
 import datetime
+import logging_tools
 import os
 import process_tools
+import stat
 
 def main():
     my_parser = argparse.ArgumentParser()
     my_parser.add_argument("-s", dest="full_strip", default=False, action="store_true", help="strip all empty lines from file [%(default)s]")
     my_parser.add_argument("-c", dest="remove_hashes", default=False, action="store_true", help="remove all lines starting with hashes from file [%(default)s]")
-    my_parser.add_argument("--full-path", dest="full_path", default=False, action="store_true", help="use full path for file objects [%(default)s]")
+    my_parser.add_argument("--short-path", dest="short_path", default=False, action="store_true", help="use short path for file objects [%(default)s]")
     my_parser.add_argument("files", nargs="+", help="files to operate on")
     options = my_parser.parse_args()
     for f_name in options.files:
-        _obj_name = f_name if options.full_path else os.path.basename(f_name)
+        _obj_name = f_name if not options.short_path else os.path.basename(f_name)
         for _rc in ["/", ".", "-"]:
             _obj_name = _obj_name.replace(_rc, "_")
         while _obj_name.startswith("_"):
             _obj_name = _obj_name[1:]
-        obj_name = "%s_object" % (_obj_name)
+        obj_name = "{}_object".format(_obj_name)
         f_lines = [_line.rstrip() for _line in file(f_name).read().split("\n")]
+        f_stat = os.stat(f_name)
         if options.full_strip:
             f_lines = [_line.strip() for _line in f_lines if _line.strip()]
         if options.remove_hashes:
             f_lines = [_line for _line in f_lines if not _line.startswith("#")]
         p_line = " " * 4
-        print "# from %s (%s, host %s)" % (f_name, datetime.datetime.now(), process_tools.get_machine_name(short=False))
-        print "%s = config.add_file_object('%s')" % (obj_name, f_name)
-        print "%s += [\n%s]\n" % (
-            obj_name,
-            "".join(["%s'%s',\n" % (p_line, _line.replace("'", '"').replace("\\", "\\\\")) for _line in f_lines]))
+        print(
+            "# from {} ({}, host {}, size was {} in {})".format(
+                f_name,
+                datetime.datetime.now(),
+                process_tools.get_machine_name(short=False),
+                logging_tools.get_size_str(f_stat[stat.ST_SIZE]),
+                logging_tools.get_plural("line", len(f_lines)),
+            )
+        )
+        print("{} = config.add_file_object('{}')".format(obj_name, f_name))
+        print(
+            "{} += [\n{}]\n".format(
+                obj_name,
+                "".join(["{}'{}',\n".format(p_line, _line.replace("'", '"').replace("\\", "\\\\")) for _line in f_lines])
+            )
+        )
+        print (
+            "{}.mode = 0{:o}".format(
+                _obj_name,
+                stat.S_IMODE(f_stat[stat.ST_MODE])
+            )
+        )
 
 if __name__ == "__main__":
     main()
