c2510cbc6e7efab032d12811f4f6a485a1430c79
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-04-16 23:12:43 +0200

    minor code cleanup

--- cluster/bin/device_info.py
+++ cluster/bin/device_info.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python-init -Ot
 #
-# Copyright (C) 2001,2002,2003,2004,2005,2006,2013 Andreas Lang, init.at
+# Copyright (C) 2001-2006,2013-2014 Andreas Lang-Nevyjel, init.at
 #
-# Send feedback to: <lang@init.at>
+# Send feedback to: <lang-nevyjel@init.at>
 # 
 # This file belongs to cluster-backbone-tools
 #
@@ -20,16 +20,14 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-import sys
 import os
+import sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "initat.cluster.settings")
 
-import logging_tools
-import argparse
-import types
 from django.db.models import Q
 from initat.cluster.backbone.models import device
+import argparse
 
 def main():
     my_parser = argparse.ArgumentParser()
@@ -38,14 +36,18 @@ def main():
     try:
         cur_dev = device.objects.get(Q(name=opts.dev))
     except device.DoesNotExist:
-        print "No device named 's' found" % (opts.dev)
+        print "No device named '{}' found".format(opts.dev)
         sys.exit(1)
     for cur_dev in [cur_dev]:
-        print "Information about device '%s' (devicegroup %s)" % (unicode(cur_dev), unicode(cur_dev.device_group))
-        print "UUID is '%s'" % (cur_dev.uuid)
+        print u"Information about device '{}' (devicegroup {})".format(
+            unicode(cur_dev),
+            unicode(cur_dev.device_group))
+        print "UUID is '{}'".format(cur_dev.uuid)
         net_devs = cur_dev.netdevice_set.all().order_by("devname")
         for cur_nd in net_devs:
-            print "   %s (%s)" % (cur_nd.devname, ", ".join([cur_ip.ip for cur_ip in cur_nd.net_ip_set.all().order_by("ip")]) or "no IPs")
+            print "   {} ({})".format(
+                cur_nd.devname,
+                ", ".join([cur_ip.ip for cur_ip in cur_nd.net_ip_set.all().order_by("ip")]) or "no IPs")
             
 if __name__ == "__main__":
     main()
