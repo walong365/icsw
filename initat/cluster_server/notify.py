cb10a889a5c81e86b24ee1fb55a95c4887ffe57f
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-04-22 15:55:18 +0200

    code cleanup

--- initat/cluster_server/notify.py
+++ initat/cluster_server/notify.py
@@ -56,7 +56,8 @@ class notify_mixin(object):
                 logging_tools.LOG_LEVEL_CRITICAL
             )
             self["exit_requested"] = True
-        self.register_timer(self.check_notify, 0, instant=True, oneshot=True)
+        # check for background jobs and register timer to check for every 10 minutes
+        self.register_timer(self.check_notify, 10 * 60, instant=True, oneshot=True)
     def check_notify(self):
         self.srv_routing.update()
         # step 1: delete pending jobs which are too old
