a1a549bb2c2a59cdd71764245c2d3c188ce033f0
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-04-29 10:44:50 +0200

    updated livestatus :-)

--- initat/cluster/frontend/templates/angular/device_livestatus.cs
+++ initat/cluster/frontend/templates/angular/device_livestatus.cs
@@ -51,6 +51,15 @@ livestatus_templ = """
 
         </tr>
         <tr>
+            <td colspan="99">
+                <div class="row">
+                    <div class="col-md-6">
+                        <tree treeconfig="cat_tree"></tree>
+                    </div>
+                </div>
+            </td>
+        </tr>
+        <tr>
             <th ng-repeat="entry in show_options"
                 ng-show="so_enabled[entry[0]]"
                 ng-click="entry[3] && toggle_order(entry[0])"
@@ -74,6 +83,7 @@ livestatus_templ = """
                 <span ng-show="is_passive_check(entry)" title="service is passive checked" class="glyphicon glyphicon-download pull-right"></span>
             </td>
             <td class="nowrap" ng-show="so_enabled['description']">{{ entry.description }}</td>
+            <td class="nowrap" ng-show="so_enabled['cats']">{{ get_categories(entry) }}</td>
             <td class="nowrap" ng-show="so_enabled['state_type']">{{ get_state_type(entry) }}</td>
             <td class="nowrap" ng-show="so_enabled['last_check']">{{ get_last_check(entry) }}</td>
             <td class="nowrap" ng-show="so_enabled['last_change']">{{ get_last_change(entry) }}</td>
@@ -119,6 +129,36 @@ device_livestatus_module = angular.module("icsw.device.livestatus", ["ngResource
 
 angular_module_setup([device_livestatus_module])
 
+add_tree_directive(device_livestatus_module)
+
+class category_tree extends tree_config
+    constructor: (@scope, args) ->
+        super(args)
+        #@show_selection_buttons = false
+        @show_icons = false
+        @show_select = true
+        @show_descendants = false
+        @show_childs = false
+    selection_changed: () =>
+        sel_list = @get_selected((node) ->
+            if node.selected
+                return [node.obj.idx]
+            else
+                return []
+        )
+        @scope.new_mc_selection(sel_list)
+    get_name : (t_entry) ->
+        cat = t_entry.obj
+        if cat.depth > 1
+            r_info = "#{cat.full_name} (#{cat.name})"
+            #if cat.num_refs
+            #    r_info = "#{r_info} (refs=#{cat.num_refs})"
+            return r_info# + "#{cat.idx}"
+        else if cat.depth
+            return cat.full_name
+        else
+            return "TOP"
+
 device_livestatus_module.controller("livestatus_ctrl", ["$scope", "$compile", "$filter", "$templateCache", "Restangular", "paginatorSettings", "restDataSource", "sharedDataSource", "$q", "$modal", "$timeout"
     ($scope, $compile, $filter, $templateCache, Restangular, paginatorSettings, restDataSource, sharedDataSource, $q, $modal, $timeout) ->
         $scope.entries = []
@@ -126,7 +166,14 @@ device_livestatus_module.controller("livestatus_ctrl", ["$scope", "$compile", "$
         $scope.order_dir = true
         $scope.md_filter_str = ""
         $scope.cur_timeout = undefined
+        # paginator settings
         $scope.pagSettings = paginatorSettings.get_paginator("device_tree_base", $scope)
+        # category tree
+        $scope.cat_tree = new category_tree($scope, {})
+        # selected categories
+        $scope.selected_mcs = []
+        $scope.master_cat_pk = 0
+        $scope.show_unspec_cat = true
         $scope.show_options = [
             # 1 ... option local name
             # 2 ... option display name
@@ -135,6 +182,7 @@ device_livestatus_module.controller("livestatus_ctrl", ["$scope", "$compile", "$
             ["host_name"    , "node name", true, true],
             ["state"        , "state", true, true],
             ["description"  , "description", true, true],
+            ["cats"         , "cats", false, false],
             ["state_type"   , "state type", false, false],
             ["last_check"   , "last check", true, false],
             ["last_change"  , "last change", false, false],
@@ -172,6 +220,10 @@ device_livestatus_module.controller("livestatus_ctrl", ["$scope", "$compile", "$
             else
                 $scope.order_name = name
                 $scope.order_dir = true
+        $scope.new_mc_selection = (new_sel) ->
+            $scope.selected_mcs = new_sel
+            $scope.show_unspec_cat = $scope.master_cat_pk in $scope.selected_mcs
+            $scope.md_filter_changed()
         $scope.get_header_class = (entry) ->
             return "nowrap"
         $scope.get_order = () ->
@@ -185,6 +237,30 @@ device_livestatus_module.controller("livestatus_ctrl", ["$scope", "$compile", "$
             else
                 _class = "glyphicon glyphicon-chevron-right"
             return _class
+        $scope.load_static_data = () ->
+            wait_list = [
+                restDataSource.reload(["{% url 'rest:category_list' %}", {}])
+            ]
+            $q.all(wait_list).then((data) ->
+                cat_tree_lut = {}
+                $scope.cat_tree.clear_root_nodes()
+                $scope.selected_mcs = []
+                for entry in data[0]
+                    if entry.full_name.match(/^\/mon/)
+                        entry.short_name = entry.full_name.substring(5)
+                        t_entry = $scope.cat_tree.new_node({folder:false, obj:entry, expand:entry.depth < 2, selected: true})
+                        cat_tree_lut[entry.idx] = t_entry
+                        if entry.parent and entry.parent of cat_tree_lut
+                            cat_tree_lut[entry.parent].add_child(t_entry)
+                        else
+                            # hide selection from root nodes
+                            #t_entry._show_select = false
+                            $scope.master_cat_pk = entry.idx
+                            $scope.cat_tree.add_root_node(t_entry)
+                        $scope.selected_mcs.push(entry.idx)
+                $scope.cat_tree_lut = cat_tree_lut
+                $scope.cat_tree.show_selected(false)
+            )
         $scope.load_data = () ->
             $scope.cur_timeout = $timeout($scope.load_data, 20000)
             call_ajax
@@ -203,11 +279,35 @@ device_livestatus_module.controller("livestatus_ctrl", ["$scope", "$compile", "$
                             host_entries = host_entries.concat(angular.fromJson($(node).text()))
                         $scope.$apply(
                             $scope.entries = service_entries
+                            for entry in service_entries
+                                entry.custom_variables = $scope.parse_custom_variables(entry.custom_variables)
                             $scope.host_entries = host_entries
                             $scope.host_lut = {}
                             for entry in host_entries
+                                entry.custom_variables = $scope.parse_custom_variables(entry.custom_variables)
                                 $scope.host_lut[entry.host_name] = entry
                         )
+        $scope.parse_custom_variables = (cvs) ->
+            _cv = {}
+            if cvs
+                first = true
+                for _entry in cvs.split("|")
+                    if first
+                        key = _entry.toLowerCase()
+                        first = false
+                    else
+                        parts = _entry.split(",")
+                        _cv[key] = parts
+                        key = parts.pop().toLowerCase()
+                # append key of last '|'-split to latest parts
+                parts.push(key)
+                for single_key in ["check_command_pk"]
+                    if single_key of _cv
+                        _cv[single_key] = parseInt(_cv[single_key][0])
+                for int_mkey in ["cat_pks"]
+                    if int_mkey of _cv
+                        _cv[int_mkey] = (parseInt(_sv) for _sv in _cv[int_mkey])
+            return _cv
         $scope.md_filter_changed = () ->
             $scope.pagSettings.set_entries(@entries)
         $scope.filter_mdr = (entry, scope) ->
@@ -217,7 +317,18 @@ device_livestatus_module.controller("livestatus_ctrl", ["$scope", "$compile", "$
             if scope.md_filter_str
                 if not $filter("filter")([entry], scope.md_filter_str).length
                     show = false
+            if show
+                if not scope.selected_mcs.length
+                   show = false
+                else
+                    if entry.custom_variables and entry.custom_variables.cat_pks?
+                        # only show if there is an intersection
+                        show = _.intersection(scope.selected_mcs, entry.custom_variables.cat_pks).length
+                    else
+                        # show entries with unset / empty category
+                        show = scope.show_unspec_cat
             return show
+        $scope.load_static_data()
         $scope.$on("$destroy", () ->
             if $scope.cur_timeout
                 $timeout.cancel($scope.cur_timeout)
@@ -252,6 +363,14 @@ device_livestatus_module.controller("livestatus_ctrl", ["$scope", "$compile", "$
                     2 : "Critical"
                     3 : "Unknown"
                 }[entry.state]
+            scope.get_categories = (entry) ->
+                if entry.custom_variables
+                    if entry.custom_variables.cat_pks?
+                        return (scope.cat_tree_lut[_pk].obj.short_name for _pk in entry.custom_variables.cat_pks).join(", ")
+                    else
+                        return ""
+                else
+                    return ""
             scope.get_state_type = (entry) ->
                 return {
                     ""  : "???"
@@ -297,6 +416,8 @@ device_livestatus_module.controller("livestatus_ctrl", ["$scope", "$compile", "$
             scope.get_host_attempt_info = (srv_entry) ->
                 return scope.get_attempt_info(scope.host_lut[srv_entry.host_name])
             scope.get_attempt_info = (entry) ->
+                if entry.max_check_attempts == null
+                    return "N/A"
                 try
                     max = parseInt(entry.max_check_attempts)
                     cur = parseInt(entry.current_attempt)
@@ -308,7 +429,7 @@ device_livestatus_module.controller("livestatus_ctrl", ["$scope", "$compile", "$
                         else
                             return "#{cur} / #{max}"
                 catch error
-                    return "??"
+                    return "e"
     }
 ).run(($templateCache) ->
     $templateCache.put("livestatus_template.html", livestatus_templ)
