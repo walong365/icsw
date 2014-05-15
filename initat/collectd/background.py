5bcbe80f05665d2edad0aab84765ff933d5866c3
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-05-15 18:10:34 +0200

    changed default timeout

--- initat/collectd/background.py
+++ initat/collectd/background.py
@@ -115,8 +115,8 @@ class bg_job(object):
         self.uuid = kwargs.get("uuid", "")
         self.comline = comline
         self.builder = builder
-        self.max_runtime = kwargs.get("max_runtime", 60)
-        self.run_every = kwargs.get("run_every", 2 * 60)
+        self.max_runtime = kwargs.get("max_runtime", 45)
+        self.run_every = kwargs.get("run_every", 60)
         self.counter = 0
         self.last_start = None
         self.running = False
