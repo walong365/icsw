b459f7450aa3ce59e54916c0733785149dac71f9
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-05-31 19:52:21 +0200

    added empty line

--- loadsensor.py
+++ loadsensor.py
@@ -20,6 +20,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+
 """ python interface to emulate a loadsensor for SGE """
 
 from initat.host_monitoring import hm_classes
