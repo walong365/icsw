200cef3184a2d2c1d749d06bbe0dac276f500704
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-06-17 16:48:43 +0200

    moved logic to monitoring.py (cluster-backbone.sql), store value

--- initat/md_config_server/dynconfig.py
+++ initat/md_config_server/dynconfig.py
@@ -28,13 +28,13 @@ dynamically creates config entries for devices (for devices queried via IPMI or 
 from django.db import connection
 from django.db.models import Q
 from initat.cluster.backbone.models import device, monitoring_hint
-from initat.md_config_server.config import global_config
 from initat.host_monitoring import limits
+from initat.md_config_server.config import global_config
+from lxml import etree # @UnresolvedImport @UnusedImport
 import logging_tools
+import pprint # @UnusedImport
 import server_command
 import threading_tools
-import pprint
-from lxml import etree # @UnresolvedImport
 
 class dynconfig_process(threading_tools.process_obj):
     def process_init(self):
@@ -66,32 +66,43 @@ class dynconfig_process(threading_tools.process_obj):
         cur_hints = {(cur_h.m_type, cur_h.key) : cur_h for cur_h in monitoring_hint.objects.filter(Q(device=cur_dev))}
         ocsp_lines = []
         # pprint.pprint(cur_hints)
-        updated, created = (0, 0)
+        n_updated, n_created = (0, 0)
         for _val in mon_info:
             _key = (_val.get("m_type"), _val.get("name"))
-            changed = False
+            updated, created = (False, False)
             if _key in cur_hints:
                 cur_hint = cur_hints[_key]
                 # update
-                for _sfn, _dfn in [("info", "info"), ]:
+                for _sfn, _dfn in [("info", "info"), ("v_type", "v_type")]:
                     if _val.get(_sfn) != getattr(cur_hint, _dfn):
                         setattr(cur_hint, _dfn, _val.get(_sfn))
-                        changed = True
-                if changed:
-                    updated += 1
+                        updated = True
             else:
                 cur_hint = monitoring_hint(
                     device=cur_dev,
                     m_type=_val.get("m_type"),
+                    v_type=_val.get("v_type"),
                     key=_val.get("name"),
                     info=_val.get("info"),
                 )
-                changed = True
-                created += 1
-            if changed:
+                created = True
+            limit_dict = {"{}_{}".format({"l" : "lower", "u" : "upper"}[_key[0]], {"w" : "warn", "c" : "crit"}[_key[1]]) : float(_val.attrib[_key]) for _key in ["lc", "uc", "lw", "uw"] if _key in _val.attrib}
+            _value = _val.get("value")
+            if _val.get("v_type") == "f":
+                _value = float(_val.get("value"))
+            else:
+                _value = int(_val.get("value"))
+            if cur_hint.update_limits(_value, limit_dict):
+                updated = True
+            if created or updated:
+                if created:
+                    n_created += 1
+                if updated:
+                    n_updated += 1
                 cur_hint.save()
             # create passive check result
-            ret_code, ret_str = self._check_status_ipmi(_val, cur_hint)
+            cur_hint.set_value(_value)
+            ret_code, ret_str = self._check_status_ipmi(_value, cur_hint)
             ocsp_line = "[{}] PROCESS_SERVICE_CHECK_RESULT;{};{};{:d};{}".format(
                 mon_info.get("time"),
                 cur_dev.full_name,
@@ -103,34 +114,25 @@ class dynconfig_process(threading_tools.process_obj):
         # pprint.pprint(ocsp_lines)
         self.send_pool_message("ocsp_results", ocsp_lines)
         if updated or created:
-            self.log("for {}: created {:d}, updated {:d}".format(unicode(cur_dev), created, updated))
-    def _check_status_ipmi(self, _xml, cur_hint):
+            self.log("for {}: created {:d}, updated {:d}".format(unicode(cur_dev), n_created, n_updated))
+    def _check_status_ipmi(self, _val, cur_hint):
         _ret = limits.nag_STATE_OK
-        _val_str = _xml.get("value")
-        if _xml.get("v_type") in ["f", "i"]:
-            _is_int = _xml.get("v_type") == "i"
-            if _is_int:
-                _val = int(float(_val_str))
+        if type(_val) in [int, long]:
+            form_str = "{:d}"
+        else:
+            form_str = "{:.2f}"
+        _val_str = form_str.format(_val)
+        for s_key, c_val in cur_hint.get_limit_list():
+            if s_key[1] == "w":
+                _sn, _retc = (1, limits.nag_STATE_WARNING)
             else:
-                _val = float(_val_str)
-            if _is_int:
-                _val_str = "{:d}".format(_val)
+                _sn, _retc = (2, limits.nag_STATE_CRITICAL)
+            if (s_key[0] == "l" and _val <= c_val) or (s_key[0] == "u" and _val >= c_val):
+                _ret = max(_ret, _retc)
+            if s_key[0] == "u":
+                _val_str = "{} {}{}{}".format(_val_str, "<" * _sn, form_str.format(c_val), ">" * _sn)
             else:
-                _val_str = "{:.2f}".format(_val)
-            for _ck, _rv, _ul, _add_strc in [
-                ("lw", limits.nag_STATE_WARNING, False, 1),
-                ("uw", limits.nag_STATE_WARNING, True, 1),
-                ("lc", limits.nag_STATE_CRITICAL, False, 2),
-                ("uc", limits.nag_STATE_CRITICAL, True, 2),
-                ]:
-                if _ck in _xml.attrib:
-                    _cval = float(_xml.get(_ck))
-                    if _ul:
-                        _val_str = "{} {}{:.2f}{}".format(_val_str, "<" * _add_strc, _cval, ">" * _add_strc)
-                    else:
-                        _val_str = "{}{:.2f}{} {}".format("<" * _add_strc, _cval, ">" * _add_strc, _val_str)
-                    if (_ul and _val > _cval) or (not _ul and _val < _cval):
-                        _ret = max(_ret, _rv)
+                _val_str = "{}{}{} {}".format("<" * _sn, form_str.format(c_val), ">" * _sn, _val_str)
         # print _val_str
         return _ret, _val_str
 
