1f6c20bfda24775a73ea1ceb67d775d65cd1f000
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-06-27 20:39:08 +0200

    added support for old clients (openvpn)

--- initat/host_monitoring/modules/openvpn_mod.py
+++ initat/host_monitoring/modules/openvpn_mod.py
@@ -437,6 +437,9 @@ class openvpn_status_command(hm_classes.hm_command):
         else:
             ret_dict = {}
         srv_com["openvpn_instances"] = ret_dict
+    def interpret_old(self, result, parsed_coms):
+        inst_dict = hm_classes.net_to_sys(result[3:])
+        return self._interpret(inst_dict, parsed_coms, self.NOGOOD_srv_com["host"].text)
     def interpret(self, srv_com, cur_ns):
         inst_dict = srv_com["openvpn_instances"]
         return self._interpret(inst_dict, cur_ns, srv_com["host"].text)
