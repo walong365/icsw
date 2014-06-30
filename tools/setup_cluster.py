32658d57f0122ab27ea1ffcc832a4a415e1cbdc5
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-06-30 08:14:19 +0200

    improved handling of initial syncs

--- tools/setup_cluster.py
+++ tools/setup_cluster.py
@@ -348,7 +348,11 @@ def migrate_db(opts):
         for _sync_app in SYNC_APPS:
             _app_dir = os.path.join(LIB_DIR, "initat", "cluster", _sync_app)
             if os.path.isdir(_app_dir):
+                print("found app {}".format(_sync_app))
                 _mig_dir = os.path.join(_app_dir, "migrations")
+                if not os.path.isdir(_mig_dir):
+                    print("initial migration for {}".format(_sync_app))
+                    call_manage(["migrate", _sync_app, "--noinput"])
                 if os.path.isdir(_mig_dir):
                     _py_files = [_entry for _entry in os.listdir(_mig_dir) if _entry.endswith(".py")]
                     if _py_files == ["__init__.py"]:
