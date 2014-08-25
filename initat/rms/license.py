b1ea289460bc9e9382bbd1dfaa5824f86d9dbd94
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-08-25 22:30:09 +0200

    added license parsing

--- initat/rms/license.py
+++ initat/rms/license.py
@@ -31,7 +31,7 @@ import os
 import process_tools
 import server_command
 import sge_license_tools
-import sge_tools
+import license_tool
 import threading_tools
 import time
 import zmq
@@ -79,6 +79,7 @@ class license_process(threading_tools.process_obj):
     def _init_sge_info(self):
         self._license_base = global_config["LICENSE_BASE"]
         self._track = global_config["TRACK_LICENSES"]
+        self.__lc_dict = {}
         self.log(
             "init sge environment for license tracking in {} ({})".format(
                 self._license_base,
@@ -118,7 +119,64 @@ class license_process(threading_tools.process_obj):
             ).lines,
             act_site
         )
+        act_conf = sge_license_tools.text_file(
+            sge_license_tools.get_site_config_file_name(self._license_base, act_site),
+            content=sge_license_tools.DEFAULT_CONFIG,
+            create=True,
+        ).dict
+        self._parse_actual_license_usage(actual_licenses, act_conf)
         self.write_ext_data(actual_licenses)
+
+    def _parse_actual_license_usage(self, actual_licenses, act_conf):
+        configured_lics = []
+        if not os.path.isfile(act_conf["LMUTIL_PATH"]):
+            self.log("Error: LMUTIL_PATH '{}' is not a file".format(act_conf["LMUTIL_PATH"]))
+        else:
+            # build different license-server calls
+            all_server_addrs = set(
+                [
+                    "{:d}@{}".format(act_lic.get_port(), act_lic.get_host()) for act_lic in actual_licenses.values() if act_lic.license_type == "simple"
+                ]
+            )
+            # print "asa:", all_server_addrs
+            q_s_time = time.time()
+            for server_addr in all_server_addrs:
+                if server_addr not in self.__lc_dict:
+                    self.__lc_dict[server_addr] = license_tool.license_check(
+                        lmutil_path=os.path.join(
+                            act_conf["LMUTIL_PATH"]
+                        ),
+                        port=int(server_addr.split("@")[0]),
+                        server=server_addr.split("@")[1],
+                        log_com=self.log)
+                srv_result = self.__lc_dict[server_addr].check()
+            q_e_time = time.time()
+            self.log(
+                "{} to query, took {}: {}".format(
+                    logging_tools.get_plural("license server", len(all_server_addrs)),
+                    logging_tools.get_diff_time_str(q_e_time - q_s_time),
+                    ", ".join(all_server_addrs)
+                )
+            )
+            for cur_lic in srv_result.xpath(".//license[@name]", smart_strings=False):
+                name = cur_lic.attrib["name"]
+                act_lic = actual_licenses.get(name, None)
+                if act_lic and act_lic.is_used:
+                    configured_lics.append(name)
+                    _total, used = (
+                        int(cur_lic.attrib["issued"]),
+                        int(cur_lic.attrib["used"]) - int(cur_lic.attrib.get("reserved", "0")))
+                    if act_lic.used_num != used:
+                        self.log(
+                            "attribute {}: use_count changed from {:d} to {:d}".format(
+                                act_lic.attribute,
+                                act_lic.used_num,
+                                used
+                            )
+                        )
+                        act_lic.used_num = int(used)
+                        act_lic.changed = True
+        return configured_lics
 
     def write_ext_data(self, actual_licenses):
         drop_com = server_command.srv_command(command="set_vector")
@@ -131,7 +189,7 @@ class license_process(threading_tools.process_obj):
         drop_com["vector_loadsensor"] = add_obj
         drop_com["vector_loadsensor"].attrib["type"] = "vector"
         send_str = unicode(drop_com)
-        self.log("sending %d bytes to vector_socket" % (len(send_str)))
+        self.log("sending {:d} bytes to vector_socket".format(len(send_str)))
         self.vector_socket.send_unicode(send_str)
 
     def log(self, what, log_level=logging_tools.LOG_LEVEL_OK):
