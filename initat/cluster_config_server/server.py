5428b60189258308e6a10bc262dafaec591a5beb
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-08-31 00:04:59 +0200

    code cleanup

--- initat/cluster_config_server/server.py
+++ initat/cluster_config_server/server.py
@@ -28,16 +28,16 @@ from initat.cluster.backbone.routing import get_server_uuid
 from initat.cluster_config_server.build_process import build_process
 from initat.cluster_config_server.config import global_config
 from initat.cluster_config_server.config_control import config_control
-from lxml import etree # @UnresolvedImport
-from lxml.builder import E # @UnresolvedImport
+from lxml import etree  # @UnresolvedImport
+from lxml.builder import E  # @UnresolvedImport
 import cluster_location
 import configfile
 import logging_tools
 import process_tools
 import server_command
 import threading_tools
-import uuid_tools
 import zmq
+
 
 class server_process(threading_tools.process_pool):
     def __init__(self):
@@ -58,6 +58,7 @@ class server_process(threading_tools.process_pool):
         self.register_func("complex_result", self._complex_result)
         self.__run_idx = 0
         self.__pending_commands = {}
+
     def log(self, what, lev=logging_tools.LOG_LEVEL_OK):
         if self.__log_template:
             while self.__log_cache:
@@ -65,16 +66,20 @@ class server_process(threading_tools.process_pool):
             self.__log_template.log(lev, what)
         else:
             self.__log_cache.append((lev, what))
+
     def _init_subsys(self):
         self.log("init subsystems")
         config_control.init(self)
+
     def _int_error(self, err_cause):
         if self["exit_requested"]:
             self.log("exit already requested, ignoring", logging_tools.LOG_LEVEL_WARN)
         else:
             self["exit_requested"] = True
+
     def _re_insert_config(self):
         cluster_location.write_config("config_server", global_config)
+
     def _log_config(self):
         self.log("Config info:")
         for line, log_level in global_config.get_log(clear=True):
@@ -83,12 +88,14 @@ class server_process(threading_tools.process_pool):
         self.log("Found %d valid config-lines:" % (len(conf_info)))
         for conf in conf_info:
             self.log("Config : %s" % (conf))
+
     def process_start(self, src_process, src_pid):
         mult = 3
         process_tools.append_pids(self.__pid_name, src_pid, mult=mult)
         if self.__msi_block:
             self.__msi_block.add_actual_pid(src_pid, mult=mult)
             self.__msi_block.save_block()
+
     def _init_msi_block(self):
         process_tools.save_pid(self.__pid_name, mult=3)
         process_tools.append_pids(self.__pid_name, pid=configfile.get_manager_pid(), mult=3)
@@ -104,24 +111,26 @@ class server_process(threading_tools.process_pool):
         else:
             msi_block = None
         return msi_block
+
     def loop_end(self):
         config_control.close_clients()
         process_tools.delete_pid(self.__pid_name)
         if self.__msi_block:
             self.__msi_block.remove_meta_block()
+
     def loop_post(self):
         for open_sock in self.socket_dict.itervalues():
             open_sock.close()
         self.__log_template.close()
+
     def _init_network_sockets(self):
         my_0mq_id = get_server_uuid("config")
         self.bind_id = my_0mq_id
         self.socket_dict = {}
         # get all ipv4 interfaces with their ip addresses, dict: interfacename -> IPv4
         for key, sock_type, bind_port, target_func in [
-            ("router", zmq.ROUTER, global_config["SERVER_PUB_PORT"] , self._new_com),
-            ("pull"  , zmq.PULL  , global_config["SERVER_PULL_PORT"], self._new_com),
-            ]:
+            ("router", zmq.ROUTER, global_config["SERVER_PORT"] , self._new_com),
+        ]:
             client = self.zmq_context.socket(sock_type)
             client.setsockopt(zmq.IDENTITY, my_0mq_id)
             client.setsockopt(zmq.LINGER, 100)
@@ -136,17 +145,25 @@ class server_process(threading_tools.process_pool):
             try:
                 client.bind(conn_str)
             except zmq.ZMQError:
-                self.log("error binding to %s{%d}: %s" % (
-                    conn_str,
-                    sock_type,
-                    process_tools.get_except_info()),
-                         logging_tools.LOG_LEVEL_CRITICAL)
+                self.log(
+                    "error binding to {}{{{:d}}}: {}".format(
+                        conn_str,
+                        sock_type,
+                        process_tools.get_except_info()
+                    ),
+                    logging_tools.LOG_LEVEL_CRITICAL
+                )
                 client.close()
             else:
-                self.log("bind to port %s{%d}" % (conn_str,
-                                                  sock_type))
+                self.log(
+                    "bind to port {}{{{:d}}}".format(
+                        conn_str,
+                        sock_type
+                    )
+                )
                 self.register_poller(client, zmq.POLLIN, target_func)
                 self.socket_dict[key] = client
+
     def _new_com(self, zmq_sock):
         data = [zmq_sock.recv_unicode()]
         while zmq_sock.getsockopt(zmq.RCVMORE):
@@ -202,7 +219,7 @@ class server_process(threading_tools.process_pool):
                         self._handle_wfe_command(zmq_sock, c_uid, srv_com)
                     else:
                         try:
-                            cur_client = None # client.get(c_uid)
+                            cur_client = None  # client.get(c_uid)
                         except KeyError:
                             self.log("unknown uid %s, not known" % (c_uid),
                                      logging_tools.LOG_LEVEL_CRITICAL)
@@ -214,6 +231,7 @@ class server_process(threading_tools.process_pool):
         else:
             self.log("wrong number of data chunks (%d != 2), data is '%s'" % (len(data), data[:20]),
                      logging_tools.LOG_LEVEL_ERROR)
+
     def _handle_wfe_command(self, zmq_sock, c_uid, srv_com):
         cur_com = srv_com["command"].text
         self.__run_idx += 1
@@ -236,6 +254,7 @@ class server_process(threading_tools.process_pool):
             cur_dev.attrib["short_name"] = dev_dict[int(cur_dev.attrib["pk"])].name
             cur_dev.attrib["name"] = dev_dict[int(cur_dev.attrib["pk"])].full_name
         self._handle_command(self.__run_idx)
+
     def create_config(self, queue_id, s_req):
         # create a build_config request
         cur_com = server_command.srv_command(command="build_config")
@@ -245,6 +264,7 @@ class server_process(threading_tools.process_pool):
         )
         cur_com["command"].attrib["source"] = "config_control"
         self._handle_wfe_command(None, str(queue_id), cur_com)
+
     def _handle_command(self, run_idx):
         cur_com = self.__pending_commands[run_idx]
         for cur_dev in cur_com["devices:devices"]:
@@ -260,15 +280,18 @@ class server_process(threading_tools.process_pool):
             self.log("nothing pending, sending return")
             self._send_return(cur_com)
             del self.__pending_commands[run_idx]
+
     def _send_return(self, cur_com):
         if cur_com["command"].attrib["source"] == "external":
             self._send_simple_return(cur_com["command"].attrib["uuid"], unicode(cur_com))
         else:
             config_control.complex_result(int(cur_com["command"].attrib["uuid"]), unicode(cur_com))
+
     def _send_simple_return(self, zmq_id, send_str):
         send_sock = self.socket_dict["router"]
         send_sock.send_unicode(zmq_id, zmq.SNDMORE)
         send_sock.send_unicode(unicode(send_str))
+
     def _client_update(self, *args, **kwargs):
         _src_proc, _src_id, upd_dict = args
         run_idx = upd_dict.get("run_idx", -1)
@@ -290,7 +313,7 @@ class server_process(threading_tools.process_pool):
                         if len(key_parts) == 1:
                             new_value = E.var(value=s_value, key=s_key)
                             new_tl.append(new_value)
-                            parent_value, parent_key = (new_value, key_parts[0]) # equal to s_key
+                            parent_value, parent_key = (new_value, key_parts[0])  # equal to s_key
                         else:
                             if key_parts[0] != parent_key:
                                 new_value = E.var(key=key_parts[0])
@@ -307,6 +330,6 @@ class server_process(threading_tools.process_pool):
         else:
             self.log("got client_update with unknown run_idx %d" % (upd_dict["run_idx"]),
                      logging_tools.LOG_LEVEL_ERROR)
+
     def _complex_result(self, src_proc, src_id, queue_id, result, **kwargs):
         config_control.complex_result(queue_id, result)
-
