ed7b57214fa55117012fc291aec85f5a0793a392
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-09-02 21:04:30 +0200

    fixed MSI setup

--- initat/rrd_grapher/server.py
+++ initat/rrd_grapher/server.py
@@ -335,7 +335,7 @@ class server_process(threading_tools.process_pool, threading_tools.operational_e
         if not global_config["DEBUG"] or True:
             self.log("Initialising meta-server-info block")
             msi_block = process_tools.meta_server_info("rrd-grapher")
-            msi_block.add_actual_pid(mult=4, fuzzy_ceiling=3, process_name="main")
+            msi_block.add_actual_pid(mult=3, fuzzy_ceiling=3, process_name="main")
             msi_block.add_actual_pid(act_pid=configfile.get_manager_pid(), mult=4, process_name="manager")
             msi_block.start_command = "/etc/init.d/rrd-grapher start"
             msi_block.stop_command = "/etc/init.d/rrd-grapher force-stop"
