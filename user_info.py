f52c10814212c4806381112e250ffbba263fc37a
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-09-03 21:05:46 +0200

    updated user_info

--- user_info.py
+++ user_info.py
@@ -21,42 +21,54 @@
 #
 """ change password from the commandline """
 
-import sys
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "initat.cluster.settings")
+
+django.setup()
+
+from django.db.models import Q
+from initat.cluster.backbone.models import user
 import argparse
 import logging_tools
-import getopt
-import os
 import pwd
+import sys
 import termios
-import crypt
-import random
-import server_command
-import process_tools
 
-def show_help(script):
-    print "Usage : %s [OPTIONS] username" % (os.path.basename(script))
-    print "  where OPTIONS is one or more of"
-    print " -h, --help      this help"
-    print " -p              change password"
-    print " -i              show information"
-    print " -l              show all users"
 
-def list_mode(dc):
-    dc.execute("SELECT u.*, g.* FROM user u, ggroup g WHERE u.ggroup=g.ggroup_idx ORDER BY u.login")
-    out_list = logging_tools.form_list()
-    out_list.set_header_string(0, ["login", "groupname", "uid", "gid", "title", "vname", "nname", "email", "phone"])
-    for db_rec in dc.fetchall():
-        out_list.add_line([db_rec["login"],
-                           db_rec["ggroupname"],
-                           db_rec["uid"],
-                           db_rec["gid"],
-                           db_rec["usertitan"],
-                           db_rec["uservname"],
-                           db_rec["usernname"],
-                           db_rec["useremail"],
-                           db_rec["usertel"]])
-    print out_list
+def list_mode():
+    all_users = user.objects.all().select_related("group").order_by("login")
+    out_list = logging_tools.new_form_list()
+    for _user in all_users:
+        out_list.append(
+            [
+                logging_tools.form_entry(_user.login, header="login"),
+                logging_tools.form_entry(_user.uid, header="uid"),
+                logging_tools.form_entry(_user.group.gid, header="gid"),
+                logging_tools.form_entry(_user.group.groupname, header="group"),
+            ]
+        )
+    print unicode(out_list)
     return 0
+
+
+def info_mode(user_name):
+    try:
+        _user = user.objects.select_related("group").get(Q(login=user_name))
+    except user.DoesNotExist:
+        print("Unknown user '{}'".format(user_name))
+        return 1
+    else:
+        print(
+            u"User with loginname '{}' ({}), uid={:d}".format(
+                _user.login,
+                unicode(_user),
+                _user.uid,
+            )
+        )
+        return 0
+
 
 def get_pass(prompt=">"):
     fd = sys.stdin.fileno()
@@ -74,14 +86,24 @@ def get_pass(prompt=">"):
     print
     return passwd
 
+
 def main():
     my_parser = argparse.ArgumentParser()
     my_parser.add_argument("--mode", dest="mode", choices=["info", "list", "change"], default="info", help="set mode [%(default)s]")
-    my_parser.add_argument("username", nargs="?", default=pwd.getpwuid(os.getuid())[0], help="set username [%(default)s]")
+    my_parser.add_argument("username", nargs="*", default=[pwd.getpwuid(os.getuid())[0]], help="set username [%(default)s]")
     options = my_parser.parse_args()
     if options.mode in ["info", "change"] and not options.username:
         print "Need username for %s mode" % (options.mode)
         sys.exit(-1)
+    if options.mode == "list":
+        ret_code = list_mode()
+    elif options.mode == "info":
+        ret_code = 0
+        for _user in options.username:
+            ret_code = max(ret_code, info_mode(_user))
+    else:
+        ret_code = 1
+    sys.exit(ret_code)
     print options
     # get name of directory server
     ds_file_name = "/etc/sysconfig/cluster/directory_server"
@@ -90,79 +112,50 @@ def main():
         sys.exit(1)
     print "functionality not ready, please contact lang-nevyjel@init.at"
     sys.exit(0)
-    if mode == "l":
-        errnum = list_mode(dc)
-    else:
-        print "Operating on user '%s'" % (user_name)
-        dc.execute("SELECT u.*, g.* FROM user u, ggroup g WHERE g.ggroup_idx=u.ggroup AND login='%s'" % (user_name))
-        if dc.rowcount == 0:
-            print "No user named '%s' found in database" % (user_name)
-            sys.exit(1)
-        elif dc.rowcount > 1:
-            print "More than one user named '%s' found in database" % (user_name)
-            sys.exit(1)
-        user_stuff = dc.fetchone()
-        if mode == "i":
-            print "User information:"
-            out_list = logging_tools.form_list()
-            for what, key in [("Login"             , "login"),
-                              ("User ID"           , "uid"),
-                              ("Primary Group Name", "ggroupname"),
-                              ("Group ID"          , "gid"),
-                              ("first name"        , "uservname"),
-                              ("second name"       , "usernname"),
-                              ("title"             , "usertitan")]:
-                out_list.add_line(("  %s" % (what), " : ", user_stuff[key] or "<not set>"))
-            print out_list
-            errnum = 0
-        else:
-            print "Change password"
-            if os.getuid():
-                # ask old password if not root
-                old_passwd = get_pass("please enter old password>")
-                if crypt.crypt(old_passwd, user_stuff["password"]) != user_stuff["password"]:
-                    print "Wrong password, exiting ..."
-                    sys.exit(1)
-            ok = False
-            while not ok:
-                new_passwd = get_pass("please enter new password>")
-                if len(new_passwd) < 6:
-                    print "minimum 6 characters"
-                else:
-                    ok = True
-            new_passwd_check = get_pass("please enter new password again>")
-            if new_passwd != new_passwd_check:
-                print "The passwords do not match, exiting ..."
-                sys.exit(1)
-            new_hash = crypt.crypt(new_passwd,
-                                   "".join([chr(random.randint(97, 122)) for x in range(16)]))
-            print "Updating database..."
-            dc.execute("UPDATE user SET password=%s WHERE login=%s", (new_hash,
-                                                                      user_name))
-            print "Signaling server..."
-            if ds_type == "yp":
-                send_com = server_command.server_command(command="write_yp_config")
-            elif ds_type == "ldap":
-                send_com = server_command.server_command(command="sync_ldap_config")
-            else:
-                print "Unknown directory_server_type '%s'" % (ds_type)
-                errnum = 1
-                send_com = None
-            if send_com:
-                errnum, data = net_tools.single_connection(host=ds_name,
-                                                           port=8004,
-                                                           command=send_com).iterate()
-                try:
-                    server_reply = server_command.server_reply(data)
-                except ValueError:
-                    print "Error: got no valid server_reply (got: '%s')" % (data)
-                else:
-                    errnum, result = server_reply.get_state_and_result()
-                    print "Got [%d]: %s" % (errnum, result)
-    dc.release()
-    del db_con
-    sys.exit(errnum)
+#         else:
+#             print "Change password"
+#             if os.getuid():
+#                 # ask old password if not root
+#                 old_passwd = get_pass("please enter old password>")
+#                 if crypt.crypt(old_passwd, user_stuff["password"]) != user_stuff["password"]:
+#                     print "Wrong password, exiting ..."
+#                     sys.exit(1)
+#             ok = False
+#             while not ok:
+#                 new_passwd = get_pass("please enter new password>")
+#                 if len(new_passwd) < 6:
+#                     print "minimum 6 characters"
+#                 else:
+#                     ok = True
+#             new_passwd_check = get_pass("please enter new password again>")
+#             if new_passwd != new_passwd_check:
+#                 print "The passwords do not match, exiting ..."
+#                 sys.exit(1)
+#             new_hash = crypt.crypt(new_passwd,
+#                                    "".join([chr(random.randint(97, 122)) for x in range(16)]))
+#             print "Updating database..."
+#             dc.execute("UPDATE user SET password=%s WHERE login=%s", (new_hash,
+#                                                                       user_name))
+#             print "Signaling server..."
+#             if ds_type == "yp":
+#                 send_com = server_command.server_command(command="write_yp_config")
+#             elif ds_type == "ldap":
+#                 send_com = server_command.server_command(command="sync_ldap_config")
+#             else:
+#                 print "Unknown directory_server_type '%s'" % (ds_type)
+#                 errnum = 1
+#                 send_com = None
+#             if send_com:
+#                 errnum, data = net_tools.single_connection(host=ds_name,
+#                                                            port=8004,
+#                                                            command=send_com).iterate()
+#                 try:
+#                     server_reply = server_command.server_reply(data)
+#                 except ValueError:
+#                     print "Error: got no valid server_reply (got: '%s')" % (data)
+#                 else:
+#                     errnum, result = server_reply.get_state_and_result()
+#                     print "Got [%d]: %s" % (errnum, result)
 
 if __name__ == "__main__":
     main()
-
