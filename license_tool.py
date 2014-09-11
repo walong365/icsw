cc49ba0b3498db7e4f2440af760aab5e640f9430
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-09-11 09:28:38 +0200

    updated license_tool

--- license_tool.py
+++ license_tool.py
@@ -20,29 +20,23 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-import sge_license_tools
-
 """ ask license server and return an XML-represenation of license situation """
 
 from lxml import etree  # @UnresolvedImport
-from lxml.builder import E  # @UnresolvedImport
 import argparse
-import datetime
 import logging_tools
-import os
-import subprocess
+import sge_license_tools
 import sys
-import time
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", type=int, default=1055, help="license server [%(default)d]")
     parser.add_argument("--server", type=str, default="localhost", help="license port [%(default)s]")
-    parser.add_argument("--mode", type=str, default="xml", choices=["xml", "check", "csv"], help="output mode [%(default)s]")
+    parser.add_argument("--mode", type=str, default="xml", choices=["xml", "check", "csv", "list"], help="output mode [%(default)s]")
     parser.add_argument("--check-eval", type=str, default="true", help="check string, should return true or false")
     opts = parser.parse_args()
-    my_lc = license_check(
+    my_lc = sge_license_tools.license_check(
         server=opts.server,
         port=opts.port,
     )
@@ -60,17 +54,32 @@ def main():
         if not ret_val:
             ret_code = 1
     elif opts.mode == "csv":
-        print ",".join(["name", "issued", "used", "free", "reserved"])
+        print(",".join(["name", "issued", "used", "free", "reserved"]))
         for cur_lic in xml_res.findall(".//license"):
-            print ",".join(
+            print(
+                ",".join(
+                    [
+                        cur_lic.attrib["name"],
+                        cur_lic.attrib["issued"],
+                        cur_lic.attrib["used"],
+                        cur_lic.attrib["free"],
+                        cur_lic.attrib["reserved"],
+                    ]
+                )
+            )
+    elif opts.mode == "list":
+        out_form = logging_tools.new_form_list()
+        for cur_lic in xml_res.findall(".//license"):
+            out_form.append(
                 [
-                    cur_lic.attrib["name"],
-                    cur_lic.attrib["issued"],
-                    cur_lic.attrib["used"],
-                    cur_lic.attrib["free"],
-                    cur_lic.attrib["reserved"],
+                    logging_tools.form_entry(cur_lic.attrib["name"], header="name"),
+                    logging_tools.form_entry_right(cur_lic.attrib["issued"], header="issued"),
+                    logging_tools.form_entry_right(cur_lic.attrib["used"], header="used"),
+                    logging_tools.form_entry_right(cur_lic.attrib["free"], header="free"),
+                    logging_tools.form_entry_right(cur_lic.attrib["reserved"], header="reserved"),
                 ]
             )
+        print(unicode(out_form))
     sys.exit(ret_code)
 
 if __name__ == "__main__":
