0c09479b6f59f416e248170cbfd69c4e22975143
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2014-10-04 21:18:05 +0200

    changed fuzzy ceiling for host-monitor main thread

--- initat/host_monitoring/server.py
+++ initat/host_monitoring/server.py
@@ -254,7 +254,7 @@ class server_code(threading_tools.process_pool):
         if True:  # not self.__options.DEBUG:
             self.log("Initialising meta-server-info block")
             msi_block = process_tools.meta_server_info("collserver")
-            msi_block.add_actual_pid(mult=3, fuzzy_ceiling=4, process_name="main")
+            msi_block.add_actual_pid(mult=3, fuzzy_ceiling=7, process_name="main")
             msi_block.add_actual_pid(act_pid=configfile.get_manager_pid(), mult=3 if global_config["NO_INOTIFY"] else 4, process_name="manager")
             msi_block.start_command = "/etc/init.d/host-monitoring start"
             msi_block.stop_command = "/etc/init.d/host-monitoring force-stop"
