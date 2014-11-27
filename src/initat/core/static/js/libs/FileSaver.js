2fa12e2f79029d43ceb2a11db6b7d0c343ad53ef
Bernhard Mallinger <mallinger@init.at>
2014-11-27 14:52:27 +0100

    Remove comment which breaks collecting static files

--- src/initat/core/static/js/libs/FileSaver.js
+++ src/initat/core/static/js/libs/FileSaver.js
@@ -7,11 +7,6 @@
  *   See https://github.com/eligrey/FileSaver.js/blob/master/LICENSE.md
  */
 
-/*global self */
-/*jslint bitwise: true, indent: 4, laxbreak: true, laxcomma: true, smarttabs: true, plusplus: true */
-
-/*! @source http://purl.eligrey.com/github/FileSaver.js/blob/master/FileSaver.js */
-
 var saveAs = saveAs
   // IE 10+ (native saveAs)
   || (typeof navigator !== "undefined" &&
