fe94bb5bf4004f14b1606c226bde185d565ea4a6
Bernhard Mallinger <mallinger@init.at>
2014-12-17 09:18:07 +0100

    Minor correction for last commit

--- initat/cluster/frontend/monitoring_views.py
+++ initat/cluster/frontend/monitoring_views.py
@@ -119,7 +119,7 @@ class call_icinga(View):
     @method_decorator(login_required)
     def get(self, request):
         resp = HttpResponseRedirect(
-            u"http://{}:{}@{}/icinga/" % (
+            u"http://{}:{}@{}/icinga/".format(
                 request.user.login,
                 # fixme, if no password is set (due to automatic login) use no_passwd
                 base64.b64decode(request.session.get("password", "no_passwd")),
