e42fe9a70029cb7dd3ddd0add6cd17c1b34d2705
Bernhard Mallinger <mallinger@init.at>
2015-01-15 11:13:44 +0100

    Up md-config-server process limit to accomodate for log parsing

--- initat/md_config_server/server.py
+++ initat/md_config_server/server.py
@@ -351,7 +351,7 @@ class server_process(threading_tools.process_pool, version_check_mixin):
         self.log("Initialising meta-server-info block")
         msi_block = process_tools.meta_server_info("md-config-server")
         msi_block.add_actual_pid(mult=3, fuzzy_ceiling=4, process_name="main")
-        msi_block.add_actual_pid(act_pid=configfile.get_manager_pid(), mult=6, process_name="manager")
+        msi_block.add_actual_pid(act_pid=configfile.get_manager_pid(), mult=7, process_name="manager")
         msi_block.start_command = "/etc/init.d/md-config-server start"
         msi_block.stop_command = "/etc/init.d/md-config-server force-stop"
         msi_block.kill_pids = True
