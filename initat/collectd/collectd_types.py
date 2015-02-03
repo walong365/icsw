2342c2bb249dd9fde669f48b6fa680ec4ff145aa
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2015-02-03 12:37:00 +0100

    added HTTPsRequest perfdata sink

--- initat/collectd/collectd_types.py
+++ initat/collectd/collectd_types.py
@@ -295,6 +295,34 @@ class HTTPRequestPerfdata(PerfdataObject):
         )
 
 
+class HTTPsRequestPerfdata(PerfdataObject):
+    PD_RE = re.compile("^time=(?P<time>\S+)\s+size=(?P<size>\S+)\s+time_connect=(?P<time_connect>\S+)\s+time_ssl=(?P<time_ssl>\S+)\s+time_headers=(?P<time_headers>\S+)\s+time_firstbyte=(?P<time_firstbyte>\S+)\s+time_transfer=(?P<time_transfer>\S+)$")
+    PD_XML_INFO = E.perfdata_info(
+        perfdata_value("time", "time needed for request", key="request.time.total", v_type="f", rrd_spec="GAUGE:0:10000").get_xml(),
+        perfdata_value("size", "size of response", key="request.size", v_type="i", rrd_spec="GAUGE:0:1000000").get_xml(),
+        perfdata_value("connect", "time needed for connect", key="request.time.connect", v_type="f", unit="s", rrd_spec="GAUGE:0:100").get_xml(),
+        perfdata_value("ssl", "time needed for SSL", key="request.time.ssl", v_type="f", rrd_spec="GAUGE:0:10000").get_xml(),
+        perfdata_value("headers", "time needed for headers", key="request.time.headers", v_type="f", unit="s", rrd_spec="GAUGE:0:100").get_xml(),
+        perfdata_value("firstbyte", "time needed until first byte", key="request.time.firstbyte", v_type="f", unit="s", rrd_spec="GAUGE:0:100").get_xml(),
+        perfdata_value("transfer", "time needed for transfer", key="request.time.transfer", v_type="f", unit="s", rrd_spec="GAUGE:0:100").get_xml(),
+    )
+
+    def build_values(self, _host_info, _xml, in_dict):
+        return self._wrap(
+            _host_info,
+            _xml,
+            [
+                float(in_dict["time"].split(";")[0][:-1]),
+                int(in_dict["size"].split(";")[0][:-1]),
+                float(in_dict["time_connect"].split(";")[0][:-1]),
+                float(in_dict["ssl"].split(";")[0][:-1]),
+                float(in_dict["time_headers"].split(";")[0][:-1]),
+                float(in_dict["time_firstbyte"].split(";")[0][:-1]),
+                float(in_dict["time_transfer"].split(";")[0][:-1]),
+            ]
+        )
+
+
 class value(object):
     # somehow resembles mvect_entry from hm_classes
     __slots__ = ["name", "sane_name", "info", "unit", "base", "value", "factor", "v_type", "last_update", "set_value", "timeout"]
