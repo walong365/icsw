25fc710e752bd419466dc6a6dde0376fff1b5b90
Bernhard Mallinger <mallinger@init.at>
2015-02-05 16:21:45 +0100

    year update

--- bonnie.py
+++ bonnie.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python-init -Otu
 #
-# Copyright (C) 2007-2008,2014 Andreas Lang-Nevyjel
+# Copyright (C) 2007-2008,2015 Andreas Lang-Nevyjel
 #
 # Send feedback to: <lang-nevyjel@init.at>
 #
