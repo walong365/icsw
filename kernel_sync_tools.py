1b442a2c7a831790319a775ed8329b8814a41f31
Bernhard Mallinger <mallinger@init.at>
2015-02-05 16:25:29 +0100

    year update

--- kernel_sync_tools.py
+++ kernel_sync_tools.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2007-2009,2012-2014 Andreas Lang-Nevyjel, init.at
+# Copyright (C) 2007-2009,2012-2015 Andreas Lang-Nevyjel, init.at
 #
 # Send feedback to: <lang-nevyjel@init.at>
 #
