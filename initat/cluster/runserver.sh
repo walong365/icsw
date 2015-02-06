58a6d93251f5e4412d2b76acd6e80b2a33ce797c
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2015-02-06 22:13:55 +0100

    unified runserver / runserver_compressed

--- initat/cluster/runserver.sh
+++ initat/cluster/runserver.sh
@@ -1,14 +1,54 @@
 #!/bin/bash
 
-export DEBUG_WEBFRONTEND=1
+_debug=1
+_localstatic=0
+_nostatic=0
+_insecure=0
+
+function print_help {
+    echo "usage:"
+    echo
+    echo "$0 [--nostatic] [--localstatic] [-h]"
+    echo
+    exit -1
+}
+
+while (( "$#" )) ; do
+    case "$1" in
+        "--nostatic")
+            _nostatic=1
+            ;;
+        "--localstatic")
+            _localstatic=1
+            _insecure=1
+            _debug=0
+            ;;
+        "-h")
+            print_help
+            ;;
+        *)
+            echo "unknown option $1"
+            print_help
+            ;;
+    esac
+    shift
+done
+
+RSOPTIONS="--traceback"
+
+[ "${_debug}" = "1" ] && export DEBUG_WEBFRONTEND=1
+[ "${_localstatic}" == "1" ] && export LOCAL_STATIC=1
+[ "${_insecure}" == "1" ] && RSOPTIONS="${RSOPTIONS} --insecure"
+
+echo "settings: DEBUG=${_debug}, LOCAL_STATIC=${_localstatic}, NOSTATIC=${_nostatic}, INSECURE=${_insecure}, RSOPTIONS='${RSOPTIONS}'"
 export NODE_PATH=$(/opt/cluster/bin/npm -g root)
 export NODE_PATH=${NODE_PATH}:${NODE_PATH}/npm/node_modules
 echo "NODE_PATH=${NODE_PATH}"
 
-if [ "$1" != "--nostatic" ] ; then
+if [ "${_nostatic}" == "0" ] ; then
      echo -ne "collecting static files ... "
     ./manage.py collectstatic --noinput -c > /dev/null
     echo "done"
 fi
 
-./manage.py runserver $* --traceback 0.0.0.0:8080 
+./manage.py runserver ${RSOPTIONS} 0.0.0.0:8080
