6db4f94ccdb23411c2e45eb711d392af46e31031
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2015-02-11 20:53:35 +0100

    removed warning mode

--- initat/cluster_config_server/main.py
+++ initat/cluster_config_server/main.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python-init -OtW default
+#!/usr/bin/python-init -Ot default
 #
-# Copyright (C) 2001-2008,2012-2014 Andreas Lang-Nevyjel, init.at
+# Copyright (C) 2001-2008,2012-2015 Andreas Lang-Nevyjel, init.at
 #
 # Send feedback to: <lang-nevyjel@init.at>
 #
