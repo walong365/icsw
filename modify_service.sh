609cd54aa3a7012e6ed260565ad5d9399ed848d9
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2015-03-06 12:23:35 +0100

    fixed behaviour for debian systems

--- modify_service.sh
+++ modify_service.sh
@@ -15,25 +15,30 @@ ret_state=0
 
 if [ "${ACTION}" = "activate" ] ; then
     if [ "${HA}" = "0" ] ; then
-	if [ -f /etc/debian_version ] ; then
-	    /usr/sbin/update-rc.d -f ${SRV_NAME} remove
-	elif [ -f /etc/redhat-release ] ; then
+        if [ -f /etc/debian_version ] ; then
+            /usr/sbin/update-rc.d ${SRV_NAME} defaults
+            /usr/sbin/update-rc.d ${SRV_NAME} enable
+            # /usr/sbin/update-rc.d -f ${SRV_NAME} remove
+        elif [ -f /etc/redhat-release ] ; then
             /opt/cluster/sbin/force_redhat_init_script.sh ${SRV_NAME}
             /sbin/chkconfig --add ${SRV_NAME}
-	else
+        else
             /sbin/insserv ${INIT}/${SRV_NAME}
-	fi
+        fi
     fi
 elif [ "${ACTION}" = "deactivate" ] ; then
     if [ "${HA}" = "0" ] ; then
-	if [ -f /etc/debian_version ] ; then
-	    /usr/sbin/update-rc.d ${SRV_NAME} start 28 2 3 5 . stop 72 0 1 4 6
-	elif [ -f /etc/redhat-release ] ; then
+        if [ -f /etc/debian_version ] ; then
+            /usr/sbin/update-rc.d ${SRV_NAME} defaults
+            /usr/sbin/update-rc.d ${SRV_NAME} disable
+            /usr/sbin/update-rc.d -f ${SRV_NAME} remove
+            # /usr/sbin/update-rc.d ${SRV_NAME} start 28 2 3 5 . stop 72 0 1 4 6
+        elif [ -f /etc/redhat-release ] ; then
             /opt/cluster/sbin/force_redhat_init_script.sh ${SRV_NAME}
             /sbin/chkconfig --del ${SRV_NAME}
-	else
+        else
             /sbin/insserv -r ${INIT}/${SRV_NAME}
-	fi
+        fi
     fi
 else
     echo "unknown action \"${ACTION}\""
