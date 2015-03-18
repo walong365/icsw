9f91a7837e651731c0d5581642c34968098d43c3
Bernhard Mallinger <mallinger@init.at>
2015-03-18 15:00:08 +0100

    translations

--- translations/extract_coffeescript_strings.sh
+++ translations/extract_coffeescript_strings.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # collect as C-code, works for gettext("to translate")
-xgettext ../initat/cluster/frontend/static/icsw/*/*.coffee -LPython --omit-header -o po/template_coffee.pot
+xgettext ../initat/cluster/frontend/static/icsw/*/*.coffee -LPython --omit-header -kgetString -o po/template_coffee.pot
