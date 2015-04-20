daf37e301dcc36b394d2ff5c48bea836ffa51318
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2015-04-20 08:10:53 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -64,8 +64,8 @@ setup(
             "/opt",
             [
                 # cbc
-                "cluster/bin/compile_libgoto.py",
-                "cluster/bin/compile_openmpi.py",
+                "/cluster/bin/compile_libgoto.py",
+                "/cluster/bin/compile_openmpi.py",
                 "cluster/bin/compile_hpl.py",
                 "cluster/bin/compile_fftw.py",
                 "cluster/bin/read_bonnie.py",
