0d6de9aa32c842925ffdd01c1631be838d0888cc
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2015-04-27 14:53:48 +0200

    updated start/stop method

--- collclient.py
+++ collclient.py
@@ -1,1 +1,29 @@
-host-monitoring-zmq.py
+#!/usr/bin/python-init -Ot
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011-2015 Andreas Lang-Nevyjel
+#
+# Send feedback to: <lang-nevyjel@init.at>
+#
+# This file is part of host-monitoring
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License Version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+""" client for host-monitoring """
+
+import sys
+from initat.host_monitoring import main
+
+sys.exit(main.main())
