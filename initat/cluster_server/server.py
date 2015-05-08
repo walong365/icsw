66b0a24ca5946dd70b3968a2d4c6855c4a387b7b
Bernhard Mallinger <mallinger@init.at>
2015-05-08 09:23:57 +0200

    Fixed cluster server msi block

--- initat/cluster_server/server.py
+++ initat/cluster_server/server.py
@@ -139,7 +139,7 @@ class server_process(threading_tools.process_pool, notify_mixin, server_mixins.n
         process_tools.append_pids(self.__pid_name, pid=configfile.get_manager_pid(), mult=1)
         if self.__msi_block:
             self.__msi_block.add_actual_pid(src_pid, mult=mult, process_name=src_process)
-            self.__msi_block.add_actual_pid(configfile.get_manager_pid(), mult=1, process_name="manager")
+            self.__msi_block.add_actual_pid(configfile.get_manager_pid(), mult=2, process_name="manager")
             self.__msi_block.save_block()
 
     def process_exit(self, src_process, src_pid):
@@ -160,7 +160,7 @@ class server_process(threading_tools.process_pool, notify_mixin, server_mixins.n
             self.log("Initialising meta-server-info block")
             msi_block = process_tools.meta_server_info(self.__pid_name)
             msi_block.add_actual_pid(mult=3, fuzzy_ceiling=4, process_name="main")
-            msi_block.add_actual_pid(act_pid=configfile.get_manager_pid(), mult=3, process_name="manager")
+            msi_block.add_actual_pid(act_pid=configfile.get_manager_pid(), mult=2, process_name="manager")
             msi_block.kill_pids = True
             msi_block.save_block()
         else:
