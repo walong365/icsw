2868958f7b0ff6c48b26a5e294d5a3d7bc9b08e5
Bernhard Mallinger <mallinger@init.at>
2015-05-21 14:17:39 +0200

    Some refactoring

--- initat/md_config_server/kpi/__init__.py
+++ initat/md_config_server/kpi/__init__.py
@@ -48,6 +48,10 @@ class KpiProcess(threading_tools.process_obj):
 
         self.register_timer(self.update, 30 if global_config["DEBUG"] else 300, instant=True)
 
+        #def a(*args, **kwargs):
+        #    print 'called with', args, kwargs
+        #self.register_func("calculate_kpi", a)
+
     def log(self, what, log_level=logging_tools.LOG_LEVEL_OK):
         self.__log_template.log(log_level, what)
 
@@ -55,66 +59,66 @@ class KpiProcess(threading_tools.process_obj):
         self.__log_template.close()
 
     def update(self):
+        """Recalculate all kpis and save result to database"""
         KpiGlobals.set_context()
-
         try:
             data = KpiData(self.log)
         except Exception as e:
             self.log("Exception when gathering kpi data: {}".format(process_tools.get_except_info()))
+        else:
+            # recalculate kpis
+            for kpi_db in Kpi.objects.filter(enabled=True):
+                result_str = self._evaluate_kpi(data, kpi_db)
+                kpi_db.set_result(result_str, django.utils.timezone.now())
+
+    def calculate_kpi(self, kpi_db):
+        """Calculate single kpi"""
+        KpiGlobals.set_context()
+        data = KpiData(self.log)
+        return self._evaluate_kpi(data, kpi_db)
+
+    def _evaluate_kpi(self, data, kpi_db):
+        """Evaluates given kpi on data returning the result as string.
+        Does not write to the database."""
+        self.log("Evaluating kpi {}".format(kpi_db))
+        # print '\nevaluating kpi', kpi_db
+        kpi_set = KpiSet(data.get_data_for_kpi(kpi_db),
+                         origin=KpiOperation(type=KpiOperation.Type.initial))
+        # print eval("return {}".format(kpi_db.formula), {'data': kpi_set})
+        eval_globals = {'data': kpi_set, 'KpiSet': KpiSet, 'KpiObject': KpiObject, 'KpiResult': KpiResult}
+        eval_locals = {}
+        result_str = None
+        try:
+            # KpiGlobals are used for evaluation, but not exposed to kpi user
+            KpiGlobals.current_kpi = kpi_db
+            exec (kpi_db.formula, eval_globals, eval_locals)
+        except Exception as e:
+            self.log(e)
+            self.log("Exception while executing kpi {} with formula {}: {}".format(kpi_db, kpi_db.formula, e))
             for line in traceback.format_exc().split("\n"):
                 self.log(line)
         else:
+            self.log("Kpi {} successfully evaluated".format(kpi_db))
+            if 'kpi' not in eval_locals:
+                self.log("Kpi {} does not define result".format(kpi_db))
+            else:
+                result = eval_locals['kpi']
 
-            # recalculate kpis
+                # print 'full result',
+                # result.dump()
+                # print_tree(result)
 
-            # calculate kpis, such that drill down data is present
-            # print '\n\nkpi evaluation\n'
+                serialized = result.serialize()
+                # print 'serialized:', serialized
+                result_str = json.dumps(serialized)
 
-            for kpi_db in Kpi.objects.filter(enabled=True):
-
-                self.log("Evaluating kpi {}".format(kpi_db))
-
-                # print '\nevaluating kpi', kpi_db
-                kpi_set = KpiSet(data.get_data_for_kpi(kpi_db),
-                                 origin=KpiOperation(type=KpiOperation.Type.initial))
-
-                # print eval("return {}".format(kpi_db.formula), {'data': kpi_set})
-                eval_globals = {'data': kpi_set, 'KpiSet': KpiSet, 'KpiObject': KpiObject, 'KpiResult': KpiResult}
-                eval_locals = {}
-                result_str = None
                 try:
-                    # KpiGlobals are used for evaluation, but not exposed to kpi user
-                    KpiGlobals.current_kpi = kpi_db
-                    exec(kpi_db.formula, eval_globals, eval_locals)
-                except Exception as e:
-                    self.log(e)
-                    self.log("Exception while executing kpi {} with formula {}: {}".format(kpi_db, kpi_db.formula, e))
-                    for line in traceback.format_exc().split("\n"):
-                        self.log(line)
-                else:
-                    self.log("Kpi {} successfully evaluated".format(kpi_db))
-                    if 'kpi' not in eval_locals:
-                        self.log("Kpi {} does not define result".format(kpi_db))
-                    else:
-                        result = eval_locals['kpi']
-
-                        # print 'full result',
-                        # result.dump()
-                        # print_tree(result)
-
-                        serialized = result.serialize()
-                        # print 'serialized:', serialized
-                        result_str = json.dumps(serialized)
-
-                        try:
-                            # there are objects which can be serialized but not deserialized, e.g. enums
-                            json.loads(result_str)
-                        except ValueError:
-                            self.log("result string can be serialized but not deserialized: {}".format(result_str),
-                                     logging_tools.LOG_LEVEL_ERROR)
-                            result_str = None
-                finally:
-                    KpiGlobals.current_kpi = None
-
-                kpi_db.set_result(result_str, django.utils.timezone.now())
-
+                    # there are objects which can be serialized but not deserialized, e.g. enums
+                    json.loads(result_str)
+                except ValueError:
+                    self.log("result string can be serialized but not deserialized: {}".format(result_str),
+                             logging_tools.LOG_LEVEL_ERROR)
+                    result_str = None
+        finally:
+            KpiGlobals.current_kpi = None
+        return result_str
