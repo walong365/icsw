81f9d319d8bc457f07e4141ca17577ae4a292bb8
Bernhard Mallinger <mallinger@init.at>
2015-06-19 11:23:42 +0200

    Implement running coverage reports

--- runtests.sh
+++ runtests.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 PATTERN="*.py"
 
+if [ "$1" = "--coverage" ]; then
+    COVERAGE=1
+    shift
+else
+    COVERAGE=0
+fi
+
 if [ -z "$*" ]; then
     DIRS=$(find . -name tests -type d)
 else
     DIRS=$@
 fi
 
-
 #echo "Running collectstatic"
 #python-init manage.py collectstatic --noinput -v 0
 
-echo "Running tests for $DIRS"
-python-init initat/cluster/manage.py test --keepdb --noinput -v 2 --pattern "$PATTERN" $DIRS
+if [ $COVERAGE -eq 0 ]; then
 
-TEST_STATUS=$?
+    echo "Running tests for $DIRS"
+    python-init initat/cluster/manage.py test --keepdb --noinput -v 2 --pattern "$PATTERN" $DIRS
+    TEST_STATUS=$?
+
+else
+
+    OMIT="initat/cluster/backbone/migrations/*"
+
+    echo "Generating coverage reports for $DIRS"
+    /opt/python-init/bin/coverage erase
+    /opt/python-init/bin/coverage run initat/cluster/manage.py test --keepdb --noinput -v 2 --pattern "$PATTERN" $DIRS --omit "${OMIT}"
+    /opt/python-init/bin/coverage html --omit "${OMIT}"
+
+fi
+
 #rm -rf static/*
 exit ${TEST_STATUS}
