e5961b39274c1e6964082f53dd477d67121351e3
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2015-06-30 11:00:34 +0200

    bugfixes

--- initat/cluster/backbone/models/model_history.py
+++ initat/cluster/backbone/models/model_history.py
@@ -23,7 +23,7 @@
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.core import serializers
-from django.db.models.signals import pre_delete, post_delete
+from django.db.models.signals import post_delete
 from django.utils.encoding import force_text
 import reversion
 from initat.cluster.backbone.middleware import thread_local_middleware
