704672eeefdb651fc9d4436e20de4673dba7a3d1
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2015-07-21 17:42:54 +0200

    fixed bug

--- initat/cluster/backbone/management/commands/fixtures/__init__.py
+++ initat/cluster/backbone/management/commands/fixtures/__init__.py
@@ -1,5 +1,27 @@
-import os
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 Andreas Lang-Nevyjel, init.at
+#
+# Send feedback to: <lang-nevyjel@init.at>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License Version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FTNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+""" creates all defined fixtures """
+
 
 from . import srv_fixtures
 
-srv_fixtures.add_fixtures(**kwargs)
+
+def add_fixtures(**kwargs):
+    srv_fixtures.add_fixtures(**kwargs)
