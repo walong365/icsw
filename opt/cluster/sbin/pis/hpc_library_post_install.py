47887aed66bab2e10084634c5654032812e2018b
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2015-08-17 07:59:10 +0200

    use ingest dirs, create xml version file

--- opt/cluster/sbin/pis/hpc_library_post_install.py
+++ opt/cluster/sbin/pis/hpc_library_post_install.py
@@ -20,12 +20,16 @@
 """ modify the openmpi_version file """
 
 import os
-import pprint
-import sys
+import shutil
+from lxml import etree
+
+from lxml.builder import E
 
 SHARE_DIR = "/opt/cluster/share"
 
 VERS_DIR = os.path.join(SHARE_DIR, "source-versions")
+SOURCES_DIR = os.path.join(SHARE_DIR, "sources")
+INGEST_DIR = os.path.join(SOURCES_DIR, "ingest")
 
 SYSTEM_LIST = ["hpl", "libgoto", "mpich", "openmpi", "sge"]
 
@@ -35,7 +39,20 @@ def main():
     for _entry in os.listdir(SHARE_DIR):
         if _entry in ["mpich_version", "openmpi_versions"]:
             os.unlink(os.path.join(SHARE_DIR, _entry))
+    # copy from ingest to sources
     for _sys in SYSTEM_LIST:
+        _src_dir = os.path.join(INGEST_DIR, _sys)
+        _dst_dir = os.path.join(SOURCES_DIR, _sys)
+        _src_list = set(os.listdir(_src_dir))
+        _dst_list = set(os.listdir(_dst_dir))
+        _copy = _src_list - _dst_list
+        for _entry in _copy:
+            print("Transfering {}/{}".format(_sys, _entry))
+            shutil.copy2(os.path.join(_src_dir, _entry), os.path.join(_dst_dir, _entry))
+    xml = E.hpc_library_versions()
+    for _sys in SYSTEM_LIST:
+        sys_xml = E.system(name=_sys)
+        xml.append(sys_xml)
         _sys_dir = os.path.join(SHARE_DIR, "sources", _sys)
         _entries = []
         _search_str = {"libgoto": "gotoblas"}.get(_sys, _sys)
@@ -43,12 +60,26 @@ def main():
             if _entry.lower().count(_search_str):
                 _parts = [_part for _part in _entry.split("-")[1].split(".") if _part.isdigit()]
                 _vers = ".".join(_parts)
-                _entries.append((_vers, os.path.join(_sys_dir, _entry)))
-        _vers_file = os.path.join(VERS_DIR, "{}_versions".format(_sys))
-        file(_vers_file, "w").write("\n".join(["{} {}".format(_vers, _entry) for _vers, _entry in _entries] + [""]))
+                _full_path = os.path.join(_sys_dir, _entry)
+                _entries.append((_vers, _full_path))
+                sys_xml.append(
+                    E.version(version=_vers, name=_entry, path=_full_path)
+                )
+        _vers_file = os.path.join(
+            VERS_DIR,
+            "{}_versions".format(_sys)
+        )
+        file(_vers_file, "w").write(
+            "\n".join(
+                [
+                    "{} {}".format(_vers, _entry) for _vers, _entry in _entries
+                ] + [""]
+            )
+        )
         print("content of version file {}:".format(_vers_file))
         for _vers, _entry in _entries:
             print("    {} -> {}".format(_vers, _entry))
+    file(os.path.join(VERS_DIR, "versions.xml"), "w").write(etree.tostring(xml, pretty_print=True))
 
 if __name__ == "__main__":
     main()
