61ac82d72d0865b671accc50a293df4b05a81f63
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2015-10-16 14:27:53 +0200

    fixed bug

--- initat/snmp/handler/instances/generic_net_handler.py
+++ initat/snmp/handler/instances/generic_net_handler.py
@@ -250,7 +250,7 @@ class handler(SNMPHandler):
                             _vector,
                             _name,
                             name=_prefix,
-                            info="if _name",
+                            info="if {}".format(_name),
                             timeout="{:d}".format(int(time.time()) + 120)
                         )
                     )
