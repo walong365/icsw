e104806764911abaacec9d57ac6c7fc13ca7596f
Georg Bahlon <bahlon@init.at>
2016-01-13 18:04:40 +0100

    added vagrant sync_files

---
+++ testutils/vagrant/synced_files/postgres_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e -x
+#
+echo "###########"
+echo "Install postgresql server and psycopg2 packages"
+yum install --assumeyes postgresql-server.x86_64 python-psycopg2-doc.x86_64
+
+echo "###########"
+echo "Initialize postgres db"
+postgresql-setup initdb
+
+echo "###########"
+echo "remove generated pg_hba.conf"
+rm -rf /var/lib/pgsql/data/pg_hba.conf
+
+echo "###########"
+echo "write pg_hba file and set permissions and owner"
+echo "#Allow any IP to connect, with a password:
+host    all         all         0.0.0.0          0.0.0.0      trust
+host    all         all         ::1/128                       trust
+local   all         all                                       trust" > /var/lib/pgsql/data/pg_hba.conf
+chown postgres:postgres /var/lib/pgsql/data/pg_hba.conf
+chmod 600 /var/lib/pgsql/data/pg_hba.conf
+
+echo "###########"
+echo "restart postgresql server"
+systemctl restart postgresql
+
+echo "###########"
+echo "create database, user and set password"
+sudo -u postgres psql --command "CREATE USER cdbuser LOGIN NOCREATEDB UNENCRYPTED PASSWORD 'init4u';"
+sudo -u postgres psql --command "CREATE DATABASE cdbase OWNER cdbuser;"
