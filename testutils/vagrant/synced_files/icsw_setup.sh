9827a5439ec7b44078dc1ec1687476e9570af961
Bernhard Mallinger <mallinger@init.at>
2016-01-15 15:25:18 +0100

    Add port info to icsw setup script

--- testutils/vagrant/synced_files/icsw_setup.sh
+++ testutils/vagrant/synced_files/icsw_setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -ex
 
-/opt/cluster/sbin/icsw setup --engine psql --user cdbuser --database cdbase --host localhost --passwd init4u
+/opt/cluster/sbin/icsw setup --engine psql --user cdbuser --database cdbase --host localhost --passwd init4u --port 5432
