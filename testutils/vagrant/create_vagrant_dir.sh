c572f46c5c70015c6ce4d32af6adc2015b4de432
Georg Bahlon <bahlon@init.at>
2016-01-18 17:27:38 +0100

    removed quotation mark for * handling

--- testutils/vagrant/create_vagrant_dir.sh
+++ testutils/vagrant/create_vagrant_dir.sh
@@ -20,12 +20,12 @@ SYNC_DIR="synced_files"
 
 mkdir -p $VAGRANT_DIR/$SYNC_DIR
 
-cp "$VAGRANT_TEST_UTILS_SOURCE/synced_files/*" "$VAGRANT_DIR/$SYNC_DIR"
+cp $VAGRANT_TEST_UTILS_SOURCE/synced_files/* "$VAGRANT_DIR/$SYNC_DIR"
 
 if [ "$1" = "centos" ] ; then
     cp "$VAGRANT_TEST_UTILS_SOURCE/Vagrantfile.centos" "$VAGRANT_DIR/Vagrantfile"
     cp "$VAGRANT_TEST_UTILS_SOURCE/vagrant_insecure_key" "$VAGRANT_DIR/vagrant_insecure_key"
-    cp "$VAGRANT_TEST_UTILS_SOURCE/synced_files_centos/*" "$VAGRANT_DIR/$SYNC_DIR"
+    cp $VAGRANT_TEST_UTILS_SOURCE/synced_files_centos/* "$VAGRANT_DIR/$SYNC_DIR"
 else
     echo "Invalid linux distribution: $1"
     exit 1
