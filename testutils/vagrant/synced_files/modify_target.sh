44e51a4cd3ce8eddf5a10f820f7738c7093d59a9
Georg Bahlon <bahlon@init.at>
2016-01-26 11:31:11 +0100

    modified post_scripts

--- testutils/vagrant/synced_files/modify_target.sh
+++ testutils/vagrant/synced_files/modify_target.sh
@@ -2,5 +2,6 @@
 
 sed -i 's/lang-nevyjel/bahlon/g' /opt/cluster/etc/cstores.d/client_config.xml
 sed -i 's/\<logging-server\>/VAGRANT_logging-server/g' /opt/cluster/etc/cstores.d/client_config.xml
+sed -i 's/\<meta-server\>/VAGRANT_meta-server/g' /opt/cluster/etc/cstores.d/client_config.xml
 sed -i 's/cluster\@init.at/bahlon\@init.at/g' /opt/cluster/etc/cstores.d/client_config.xml
 /opt/cluster/sbin/icsw service restart logging-server
