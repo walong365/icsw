b82ea343557e562a4b397e8ab6b64bec19d93105
Georg Bahlon <bahlon@init.at>
2016-01-26 17:33:04 +0100

    updates

--- testutils/vagrant/synced_files/modify_target.sh
+++ testutils/vagrant/synced_files/modify_target.sh
@@ -4,8 +4,7 @@ XML_PATH=/opt/cluster/bin
 CONF=/opt/cluster/etc/cstores.d/client_config.xml
 TMP=/opt/cluster/etc/cstores.d/client_config_tmp.xml
 
-$XML_PATH/xml ed \
- -u "//key[@name='log.mail.from.name']" -v 'VAGRANT_logging-server' -u "//key[@name='mail.target.address']" -v 'bahlon@init.at' -u "//key[@name='meta.mail.from.name']" -v 'VAGRANT_meta-server' $CONF > $TMP
+$XML_PATH/xml ed -u "//key[@name='log.mail.from.name']" -v 'VAGRANT_logging-server' -u "//key[@name='mail.target.address']" -v 'bahlon@init.at' -u "//key[@name='meta.mail.from.name']" -v 'VAGRANT_meta-server' $CONF > $TMP
 
 echo "copy $TMP to $CONF and remove $TMP"
 
--- testutils/vagrant/synced_files/setup_license.sh
+++ testutils/vagrant/synced_files/setup_license.sh
@@ -1,3 +1,3 @@
-#!/bin/sh -ex
+#!/bin/bash -ex
 
 /opt/cluster/sbin/icsw license register_cluster --user initat --password init4license --cluster-name oVirt_TEST
