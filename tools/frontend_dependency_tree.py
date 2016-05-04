d3a2b18c3e0c5b911891e3fec8e0caec5dfb9a2c
kaufmann <kaufmann@init.at>
2016-05-04 12:25:01 +0200

    * frontend_dependency_tree.py added

--- tools/frontend_dependency_tree.py
+++ tools/frontend_dependency_tree.py
@@ -1,3 +1,25 @@
+#!/usr/bin/python-init -Ot
+#
+# Copyright (C) 2016 Gregor Kaufmann
+#
+#
+# Send feedback to: <g,kaufmann@init.at>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License Version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+
 PATH = "../initat/cluster/frontend/static/icsw/"
 
 import os.path
