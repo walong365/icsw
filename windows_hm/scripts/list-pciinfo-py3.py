cff25e7b51b17d806a257ee9c5055739398eec31
kaufmann <g.kaufman@init.at>
2016-05-27 13:50:05 +0200

    * pciinfo output changed

--- windows_hm/scripts/list-pciinfo-py3.py
+++ windows_hm/scripts/list-pciinfo-py3.py
@@ -4,4 +4,7 @@ import json
 if __name__=="__main__":
     with subprocess.Popen([".\scripts\python\pciutils\lspci.exe"], stdout=subprocess.PIPE) as proc:
         output = proc.stdout.read()
-        print(output.decode())
+        
+        lines = output.decode().split("\r\n")
+        
+        print(json.dumps([line for line in lines if line]))
