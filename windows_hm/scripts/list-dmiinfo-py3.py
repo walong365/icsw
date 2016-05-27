383ec5de0fb59897c1ef77ec93d48008de862c91
kaufmann <g.kaufman@init.at>
2016-05-27 10:56:35 +0200

    *dmiinfo fixes

--- windows_hm/scripts/list-dmiinfo-py3.py
+++ windows_hm/scripts/list-dmiinfo-py3.py
@@ -1,11 +1,21 @@
 import subprocess
+import json
 
 if __name__=="__main__":
-    with subprocess.Popen([".\scripts\python\dmidecode212.exe", "--dump"], stdout=subprocess.PIPE) as proc:
+    with subprocess.Popen([".\scripts\python\dmidecode212.exe"], stdout=subprocess.PIPE) as proc:
         output = proc.stdout.read()
         
-        print(output)
-    
-    
+        lines = []
         
-    
+        sh_first_line = True
+        
+        for line in str(output).split("\\r\\n"):
+            if sh_first_line:
+                lines.append(line[2:])
+                sh_first_line = False
+            else:
+                lines.append(line)
+            if line == "End Of Table":
+                break
+        
+        print(json.dumps(lines))
