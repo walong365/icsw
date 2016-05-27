f388a6b2d724a3d18d59d38db3487802da610524
kaufmann <g.kaufman@init.at>
2016-05-27 14:22:39 +0200

    * pciinfo output changed

--- windows_hm/scripts/list-pciinfo-py3.py
+++ windows_hm/scripts/list-pciinfo-py3.py
@@ -1,10 +1,32 @@
 import subprocess
 import json
 
+       
 if __name__=="__main__":
-    with subprocess.Popen([".\scripts\python\pciutils\lspci.exe"], stdout=subprocess.PIPE) as proc:
+    with subprocess.Popen([".\scripts\python\pciutils\lspci.exe", "-v", "-mm"], stdout=subprocess.PIPE) as proc:
         output = proc.stdout.read()
         
-        lines = output.decode().split("\r\n")
+        items = []
         
-        print(json.dumps([line for line in lines if line]))
+        info_dict = {}
+        for line in output.decode().split("\r\n"):
+            if len(line) == 0:
+                if len(info_dict) > 0:
+                    items.append(info_dict)
+                    info_dict = {}
+            if line.startswith("Slot:"):
+                info_dict['slot'] = line.split("\t", 1)[1]
+            elif line.startswith("Class:"):
+                info_dict['class'] = line.split("\t", 1)[1]
+            elif line.startswith("Vendor:"):
+                info_dict['vendor'] = line.split("\t", 1)[1]
+            elif line.startswith("Device:"):
+                info_dict['device'] = line.split("\t", 1)[1]
+            elif line.startswith("SVendor:"):
+                info_dict['svendor'] = line.split("\t", 1)[1]
+            elif line.startswith("SDevice:"):
+                info_dict['sdevice'] = line.split("\t", 1)[1]
+            elif line.startswith("Rev:"):
+                info_dict['rev'] = line.split("\t", 1)[1]
+            
+        print(json.dumps(items))
