f8498c6172e9b49c4dacf247405aad79775a30be
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2016-07-15 21:47:32 +0200

    added md_interface_server

---
+++ initat/md_interface_server/main.py
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2016 Andreas Lang-Nevyjel, init.at
+#
+# this file is part of icsw-server
+#
+# Send feedback to: <lang-nevyjel@init.at>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License Version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+""" main process for md-interface-server """
+
+import os
+#from initat.md_config_server.constants import SERVER_COM_PORT, IDOMOD_PROCESS_TIMED_EVENT_DATA, \
+#    IDOMOD_PROCESS_SERVICE_CHECK_DATA, IDOMOD_PROCESS_HOST_CHECK_DATA, BROKER_TIMED_EVENTS, \
+#    BROKER_SERVICE_CHECKS, BROKER_HOST_CHECKS, CACHE_MODES
+from initat.server_version import VERSION_STRING
+from initat.tools import cluster_location, configfile, process_tools
+from initat.md_interface_server.config import global_config
+
+
+def run_code():
+    from initat.md_interface_server.server import server_process
+    server_process().loop()
+
+
+def main():
+    prog_name = global_config.name()
+    global_config.add_config_entries(
+        [
+            ("DEBUG", configfile.bool_c_var(False, help_string="enable debug mode [%(default)s]", short_options="d", only_commandline=True)),
+            ("VERBOSE", configfile.int_c_var(0, help_string="set verbose level [%(default)d]", short_options="v", only_commandline=True)),
+            ("MEMCACHE_ADDRESS", configfile.str_c_var("127.0.0.1", help_string="memcache address")),
+        ]
+    )
+    _options = global_config.handle_commandline(
+        description="{}, version is {}".format(
+            prog_name,
+            VERSION_STRING
+        ),
+        positional_arguments=False
+    )
+    # enable connection debugging
+    run_code()
+    # exit
+    os._exit(0)
