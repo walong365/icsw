fa30b9375d5e3609703a883cda0453d5f35448dc
kaufmann <kaufmann@init.at>
2016-09-08 10:49:33 +0200

    [CSW-683] buildscript made more portable (darwin/osx <-> linux)

--- osx_hm/build.sh
+++ osx_hm/build.sh
@@ -15,10 +15,10 @@ sed -i "s/VERSION_PLACEHOLDER/\"$VERSION\"/" ./build/icsw-host-monitoring-$VERSI
 
 tar -czpf ./build/icsw-host-monitoring-$VERSION.tar.gz -C ./build icsw-host-monitoring-$VERSION
 
-SHA256SUM="$(sha256sum ./build/icsw-host-monitoring-$VERSION.tar.gz | head -c 64)"
+SHA256SUM="$(shasum -a256 ./build/icsw-host-monitoring-$VERSION.tar.gz | head -c 64)"
 
 cp icsw-host-monitoring_template.rb ./build/icsw-host-monitoring.rb
 
-sed -i "s/URL_PLACEHOLDER/\"http:\/\/192.168.1.178\/icsw\/static\/icsw-host-monitoring-$VERSION.tar.gz\"/" ./build/icsw-host-monitoring.rb
-sed -i "s/VERSION_PLACEHOLDER/\"$VERSION\"/" ./build/icsw-host-monitoring.rb
-sed -i "s/SHA256_PLACEHOLDER/\"$SHA256SUM\"/" ./build/icsw-host-monitoring.rb
+sed -i '' "s/URL_PLACEHOLDER/\"http:\/\/192.168.1.178\/icsw\/static\/icsw-host-monitoring-$VERSION.tar.gz\"/" ./build/icsw-host-monitoring.rb
+sed -i '' '"s/VERSION_PLACEHOLDER/\"$VERSION\"/" ./build/icsw-host-monitoring.rb
+sed -i '' '"s/SHA256_PLACEHOLDER/\"$SHA256SUM\"/" ./build/icsw-host-monitoring.rb
