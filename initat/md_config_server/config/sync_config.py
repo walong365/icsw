a9ad79a323bd17e19ceade935eaec368049a8ac6
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2016-10-04 11:05:33 +0200

    fixed typo

--- initat/md_config_server/config/sync_config.py
+++ initat/md_config_server/config/sync_config.py
@@ -150,15 +150,15 @@ class SyncConfig(object):
 
     def handle_info_action(self, action, srv_com):
         if action == "sync_start":
-            self.__md_struct.sync_start = cluster_timezone.localize(datetime.datetime.now()),
+            self.__md_struct.sync_start = cluster_timezone.localize(datetime.datetime.now())
             self.__md_struct.num_files = int(srv_com["*num_files"])
             self.__md_struct.size_data = int(srv_com["*size_data"])
             self.__md_struct.num_transfers = 1
             self.__md_struct.num_runs += 1
-            self.__md_struct.save()
+            self.__md_struct.save(update_fields=["sync_start", "num_files", "size_data", "num_transfers", "num_runs"])
         elif action == "sync_end":
             self.__md_struct.sync_end = cluster_timezone.localize(datetime.datetime.now()),
-            self.__md_struct.save()
+            self.__md_struct.save(update_fields=["sync_end"])
         else:
             self.log("unknown action {} in handle_info_action()".format(action), logging_tools.LOG_LEVEL_ERROR)
 
