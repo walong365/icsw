86884cc54b8f9c829f1de38ef894fdee446229a8
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2016-10-15 09:09:27 +0200

    moved livestatus enum handling to livestatus object

--- initat/md_sync_server/common.py
+++ initat/md_sync_server/common.py
@@ -23,6 +23,8 @@ import csv
 import os.path
 import socket
 
+from enum import Enum
+
 from .config import global_config
 
 
@@ -35,12 +37,17 @@ class LiveQuery(object):
 
     def call(self):
         if self._columns:
-            return self._conn.call(str(self), self._columns)
+            _res = self._conn.call(str(self), self._columns)
         else:
-            return self._conn.call(str(self))
+            _res = self._conn.call(str(self))
+        if LiveSocket.livestatus_enum:
+            _res = LiveSocket.map_result(self._resource, _res)
+        return _res
 
     def __str__(self):
-        r_field = ["GET {}".format(self._resource)]
+        r_field = [
+            "GET {}".format(self._resource)
+        ]
         if self._columns:
             r_field.append("Columns: {}".format(" ".join(self._columns)))
         r_field.extend(self._filters)
@@ -48,7 +55,10 @@ class LiveQuery(object):
         return "\n".join(r_field + ["", ""])
 
     def columns(self, *args):
-        self._columns = args
+        if LiveSocket.livestatus_enum:
+            self._columns = LiveSocket.lookup_enum(self._resource, *args)
+        else:
+            self._columns = args
         return self
 
     def filter_raw(self, filter_list):
@@ -67,6 +77,8 @@ class LiveQuery(object):
 
 
 class LiveSocket(object):
+    livestatus_enum = None
+
     def __init__(self, peer_name):
         self.peer = peer_name
 
@@ -93,9 +105,90 @@ class LiveSocket(object):
         return _result
 
     @classmethod
+    def lookup_enum(cls, t_type, *keys):
+        _enum = getattr(cls.livestatus_enum, t_type).value
+        return [getattr(_enum, _key).value["name"] for _key in keys]
+
+    @classmethod
+    def map_result(cls, t_type, res_list):
+        def _parse_icinga_dict(value):
+            # start with empty dict
+            _loc_dict = {}
+            # dictonary
+            if value.strip():
+                for _kv in value.split(","):
+                    if _kv.count("|"):
+                        _lkey, _kv = _kv.split("|")
+                        _lkey = _lkey.lower()
+                        _loc_dict[_lkey] = []
+                    if _kv.isdigit():
+                        _kv = int(_kv)
+                    _loc_dict[_lkey].append(_kv)
+                    if _lkey in {"check_command_pk", "device_pk", "uuid"}:
+                        _loc_dict[_lkey] = _loc_dict[_lkey][0]
+            # print value, _loc_dict
+            return _loc_dict
+
+        _enum = getattr(cls.livestatus_enum, t_type).value
+        for entry in res_list:
+            for _key, _value in entry.iteritems():
+                try:
+                    _type = getattr(_enum, _key).value["type"]
+                    if _type == "integer":
+                        # integer
+                        entry[_key] = int(_value)
+                    elif _type == "time":
+                        # unix time in seconds
+                        entry[_key] = int(_value)
+                    elif _type == "string":
+                        pass
+                    elif _type == "dict":
+                        # special icinga dict
+                        entry[_key] = _parse_icinga_dict(_value)
+                    else:
+                        # print "*", _key, _type, _value
+                        pass
+                except KeyError:
+                    raise
+        return res_list
+
+    @classmethod
+    def init_enum(cls, sock_name):
+        # init enum for livestatus queries
+        _result = LiveSocket(sock_name).columns.call()
+        _enum_list = []
+        _dict = {}
+        for entry in _result:
+            _dict.setdefault(entry["table"], []).append(entry)
+        # self.log("tables found: {:d}".format(len(_dict.keys())))
+        for _table in sorted(_dict.keys()):
+            # self.log("dump for table {} ({}):".format(_table, len(_dict[_table])))
+            _enum_name_list = []
+            for _entry in _dict[_table]:
+                _enum_name_list.append(
+                    (
+                        _entry["name"],
+                        {"type": _entry["type"], "name": _entry["name"]}
+                    )
+                )
+                # self.log("    [{:<6s}] {}: {})".format(_entry["type"], _entry["name"], _entry["description"]))
+            table_enum = Enum(
+                value="Livestatus{}TableEnum".format(_table.title()),
+                names=_enum_name_list
+            )
+            _enum_list.append((_table, table_enum))
+            # print table_enum, len(table_enum), _enum_name_list
+        cls.livestatus_enum = Enum(
+            value="LivestatusEnum",
+            names=_enum_list,
+        )
+
+    @classmethod
     def get_mon_live_socket(cls):
         sock_name = os.path.join(global_config["MD_BASEDIR"], "var", "live")
         if os.path.exists(sock_name):
+            if not cls.livestatus_enum:
+                cls.init_enum(sock_name)
             return LiveSocket(sock_name)
         else:
             raise IOError("socket '{}' does not exist".format(sock_name))
--- initat/md_sync_server/status.py
+++ initat/md_sync_server/status.py
@@ -22,15 +22,47 @@
 import json
 import time
 
-from .common import LiveSocket
-from .config import global_config
 from initat.tools import logging_tools, process_tools, server_command, \
     threading_tools
+from .common import LiveSocket
+from .config import global_config
 
 __all__ = [
     "LiveSocket",
     "StatusProcess",
 ]
+
+
+class LivstatusFetch(dict):
+    def __init__(self, log_com, live_socket):
+        dict.__init__(self)
+        self.__log_com = log_com
+        self._socket = live_socket
+        self.__start_time = time.time()
+
+    def log(self, what, log_level=logging_tools.LOG_LEVEL_OK):
+        self.__log_com(u"[LF] {}".format(what), log_level)
+
+    def fetch(self):
+        _src_keys = list(self.keys())
+        _res_len = 0
+        for _src_key in _src_keys:
+            _dst_key = "{}_result".format(_src_key)
+            self[_dst_key] = self[_src_key].call()
+            _res_len += len(self[_dst_key])
+        self._num_results = _res_len
+
+    @property
+    def info_str(self):
+        self.__end_time = time.time()
+        _query_keys = [key for key in self.iterkeys() if not key.endswith("_result")]
+        log_str = "{} gave {} in {}".format(
+            logging_tools.get_plural("query", len(_query_keys)),
+            logging_tools.get_plural("result", self._num_results),
+            logging_tools.get_diff_time_str(self.__end_time - self.__start_time),
+        )
+        self.log(log_str)
+        return log_str
 
 
 class StatusProcess(threading_tools.process_obj):
@@ -44,8 +76,6 @@ class StatusProcess(threading_tools.process_obj):
             init_logger=True,
         )
         self.register_func("get_node_status", self._get_node_status)
-        self.__columns_fetched = False
-        self.register_timer(self.get_all_columns, 60, first_timeout=3)
         self.__socket = None
 
     def log(self, what, log_level=logging_tools.LOG_LEVEL_OK):
@@ -54,27 +84,6 @@ class StatusProcess(threading_tools.process_obj):
     def loop_post(self):
         self._close()
         self.__log_template.close()
-
-    def get_all_columns(self):
-        if not self.__columns_fetched:
-            cur_sock = self._open()
-            if cur_sock:
-                col_query = cur_sock.columns
-                _result = col_query.call()
-                _type_dict = {}
-                _dict = {}
-                for entry in _result:
-                    _dict.setdefault(entry["table"], []).append(entry)
-                self.log("tables found: {:d}".format(len(_dict.keys())))
-                for _table in sorted(_dict.keys()):
-                    self.log("dump for table {} ({}):".format(_table, len(_dict[_table])))
-                    _type_dict[_table] = {}
-                    for _entry in _dict[_table]:
-                        self.log("    [{:<6s}] {}: {})".format(_entry["type"], _entry["name"], _entry["description"]))
-                        _type_dict[_table][_entry["name"]] = _entry["type"][0]
-                self.__columns_fetched = True
-                # store type dict
-                self.__type_dict = _type_dict
 
     def _close(self):
         if self.__socket:
@@ -89,48 +98,6 @@ class StatusProcess(threading_tools.process_obj):
                 self.log(unicode(e), logging_tools.LOG_LEVEL_ERROR)
         return self.__socket
 
-    def _parse_icinga_dict(self, value):
-        # start with empty dict
-        _loc_dict = {}
-        # dictonary
-        if value.strip():
-            for _kv in value.split(","):
-                if _kv.count("|"):
-                    _lkey, _kv = _kv.split("|")
-                    _lkey = _lkey.lower()
-                    _loc_dict[_lkey] = []
-                if _kv.isdigit():
-                    _kv = int(_kv)
-                _loc_dict[_lkey].append(_kv)
-                if _lkey in {"check_command_pk", "device_pk", "uuid"}:
-                    _loc_dict[_lkey] = _loc_dict[_lkey][0]
-        # print value, _loc_dict
-        return _loc_dict
-
-    def map_values(self, t_type, res_list):
-        _type_dict = self.__type_dict[t_type]
-        for entry in res_list:
-            for _key, _value in entry.iteritems():
-                try:
-                    _type = _type_dict[_key]
-                    if _type == "i":
-                        # integer
-                        entry[_key] = int(_value)
-                    elif _type == "t":
-                        # unix time in seconds
-                        entry[_key] = int(_value)
-                    elif _type == "s":
-                        pass
-                    elif _type == "d":
-                        # special icinga dict
-                        entry[_key] = self._parse_icinga_dict(_value)
-                    else:
-                        # print "*", _key, _type, _value
-                        pass
-                except KeyError:
-                    self.log("unknown key {} for table {}".format(_key, t_type), logging_tools.LOG_LEVEL_ERROR)
-        return res_list
-
     def _get_node_status(self, srv_com_str, **kwargs):
         srv_com = server_command.srv_command(source=srv_com_str)
         # overview mode if overview is a top-level element
@@ -144,10 +111,9 @@ class StatusProcess(threading_tools.process_obj):
         try:
             cur_sock = self._open()
             if cur_sock:
-                if not self.__columns_fetched:
-                    self.get_all_columns()
+                fetch_dict = LivstatusFetch(self.log, cur_sock)
                 if _host_overview:
-                    host_query = cur_sock.hosts.columns(
+                    fetch_dict["host"] = cur_sock.hosts.columns(
                         "name",
                         "address",
                         "state",
@@ -155,18 +121,15 @@ class StatusProcess(threading_tools.process_obj):
                         "custom_variables",
                     )
                     if _service_overview:
-                        service_query = cur_sock.services.columns(
-                            "host_name",
+                        fetch_dict["service"] = cur_sock.services.columns(
                             "description",
                             "state",
                             "plugin_output",
                             "custom_variables",
                         )
-                    else:
-                        service_query = None
                 else:
                     if dev_names:
-                        service_query = cur_sock.services.columns(
+                        fetch_dict["service"] = cur_sock.services.columns(
                             "host_name",
                             "description",
                             "state",
@@ -182,7 +145,7 @@ class StatusProcess(threading_tools.process_obj):
                             "acknowledged",
                             "acknowledgement_type",
                         ).filter("host_name", "=", dev_names)
-                        host_query = cur_sock.hosts.columns(
+                        fetch_dict["host"] = cur_sock.hosts.columns(
                             "name",
                             "address",
                             "state",
@@ -197,7 +160,7 @@ class StatusProcess(threading_tools.process_obj):
                             "acknowledged",
                             "acknowledgement_type",
                         ).filter("name", "=", dev_names)
-                        host_comment_query = cur_sock.comments.columns(
+                        fetch_dict["host_comment"] = cur_sock.comments.columns(
                             "host_name",
                             "author",
                             "comment",
@@ -211,7 +174,7 @@ class StatusProcess(threading_tools.process_obj):
                                 "And: 2"
                             ]
                         )
-                        service_comment_query = cur_sock.comments.columns(
+                        fetch_dict["service_comment"] = cur_sock.comments.columns(
                             "host_name",
                             "author",
                             "comment",
@@ -225,45 +188,17 @@ class StatusProcess(threading_tools.process_obj):
                                 "And: 2"
                             ]
                         )
-                    else:
-                        service_query, host_query, host_comment_query, service_comment_query = (None, None, None, None)
-                if host_query is not None:
-                    host_result = self.map_values("hosts", host_query.call())
-                else:
-                    host_result = []
-                if service_query is not None:
-                    service_result = self.map_values("services", service_query.call())
-                else:
-                    service_result = []
-                if host_comment_query is not None:
-                    host_comment_result = self.map_values("comments", host_comment_query.call())
-                else:
-                    host_comment_result = []
-                if service_comment_query is not None:
-                    service_comment_result = self.map_values("comments", service_comment_query.call())
-                else:
-                    service_comment_result = []
-                if _host_overview:
-                    # get dev_names from result
-                    dev_names = [_entry["name"] for _entry in host_result]
+                fetch_dict.fetch()
                 srv_com["service_result"] = json.dumps(
                     [
-                        _line for _line in service_result if _line.get("host_name", "")
+                        _line for _line in fetch_dict["service_result"] if _line.get("host_name", "")
                     ]
                 )
                 srv_com["host_result"] = json.dumps(
-                    host_result
+                    fetch_dict["host_result"]
                 )
-                # todo: add host comments to host / service results
-                # import pprint
-                # pprint.pprint(host_comment_result)
-                # pprint.pprint(service_comment_result)
                 srv_com.set_result(
-                    "query for {} gave {} and {}".format(
-                        logging_tools.get_plural("device", len(dev_names)),
-                        logging_tools.get_plural("host result", len(host_result)),
-                        logging_tools.get_plural("service result", len(service_result)),
-                    )
+                    fetch_dict.info_str
                 )
             else:
                 srv_com.set_result("cannot connect to socket", server_command.SRV_REPLY_STATE_CRITICAL)
@@ -276,13 +211,5 @@ class StatusProcess(threading_tools.process_obj):
             srv_com.set_result(
                 "exception during fetch",
                 server_command.SRV_REPLY_STATE_CRITICAL
-            )
-        else:
-            self.log(
-                "queried {} in {}".format(
-                    logging_tools.get_plural("device", len(dev_names)),
-                    logging_tools.get_diff_time_str(time.time() - s_time),
-                    # u", ".join(sorted(dev_names))
-                )
             )
         self.send_pool_message("remote_call_async_result", unicode(srv_com))
