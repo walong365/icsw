28a98442acc6a556fe5e14bddae4fb27ceaf2cfa
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2016-10-18 16:46:17 +0200

    updated pci.ids

--- initat/tools/pci_database.py
+++ initat/tools/pci_database.py
@@ -16,7 +16,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-""" interpret pci layout """
+""" interpret PCI layout """
 
 from __future__ import unicode_literals, print_function
 
