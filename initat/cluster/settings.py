b69b8b2adbc58be5ea0d850463c92d3dcfa70b1a
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2016-10-20 14:09:16 +0200

    Improved handling of changing model names during update

--- initat/cluster/settings.py
+++ initat/cluster/settings.py
@@ -436,6 +436,8 @@ rest_renderers = (
     "rest_framework_xml.renderers.XMLRenderer",
 ]
 
+DATA_UPLOAD_MAX_NUMBER_FIELDS = None
+
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': tuple(rest_renderers),
     "DEFAULT_PARSER_CLASSES": (
@@ -450,59 +452,61 @@ REST_FRAMEWORK = {
     "ID_FIELD_NAME": "idx",
 }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        "initat": {
-            "()": "initat.tools.logging_net.icswInitFormatter",
+try:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            "initat": {
+                "()": "initat.tools.logging_net.icswInitFormatter",
+            },
+            'verbose': {
+                'format': '%(levelname)s %(asctime)s %(message)s'
+            },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+            },
         },
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(message)s'
+        'handlers': {
+            "console": {
+                "level": "INFO" if DEBUG else "WARN",
+                "class": "logging.StreamHandler",
+                "formatter": "verbose",
+            },
+            "init_unified": {
+                "level": "INFO" if DEBUG else "WARN",
+                "class": "initat.tools.logging_net.icswInitHandlerUnified",
+                "formatter": "initat",
+            },
+            "init": {
+                "level": 'INFO' if DEBUG else "WARN",
+                "class": "initat.tools.logging_net.icswInitHandler",
+                "formatter": "initat",
+            },
+            "init_mail": {
+                "level": "ERROR",
+                "class": "initat.tools.logging_net.icswInitEmailHandler",
+                "formatter": "initat",
+            },
         },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        "console": {
-            "level": "INFO" if DEBUG else "WARN",
-            "class": "logging.StreamHandler",
-            "formatter": "verbose",
-        },
-        "init_unified": {
-            "level": "INFO" if DEBUG else "WARN",
-            "class": "initat.tools.logging_net.icswInitHandlerUnified",
-            "formatter": "initat",
-        },
-        "init": {
-            "level": 'INFO' if DEBUG else "WARN",
-            "class": "initat.tools.logging_net.icswInitHandler",
-            "formatter": "initat",
-        },
-        "init_mail": {
-            "level": "ERROR",
-            "class": "initat.tools.logging_net.icswInitEmailHandler",
-            "formatter": "initat",
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['init_unified', "init_mail", "console"],
-            'propagate': True,
-            'level': 'INFO',
-        },
-        'initat': {
-            'handlers': ['init_unified', "init_mail"],
-            'propagate': True,
-            'level': 'WARN',
-        },
-        'cluster': {
-            'handlers': ["init_unified", "init", "init_mail"],
-            'propagate': True,
-            'level': 'INFO' if DEBUG else "WARN",
-        },
+        'loggers': {
+            'django': {
+                'handlers': ['init_unified', "init_mail", "console"],
+                'propagate': True,
+                'level': 'INFO',
+            },
+            'initat': {
+                'handlers': ['init_unified', "init_mail"],
+                'propagate': True,
+                'level': 'WARN',
+            },
+            'cluster': {
+                'handlers': ["init_unified", "init", "init_mail"],
+                'propagate': True,
+                'level': 'INFO' if DEBUG else "WARN",
+            },
+        }
     }
-}
-
-DATA_UPLOAD_MAX_NUMBER_FIELDS = None
+except ValueError:
+    # may happen during update
+    pass
