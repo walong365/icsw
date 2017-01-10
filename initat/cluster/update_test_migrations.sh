96f750b2a102f5d31f9a09c08b2a12b354378836
kaufmann <kaufmann@init.at>
2017-01-10 14:13:10 +0100

    [ICSW-921] Test migrations fixed [ICSW-921] Added script to easily fix (test) migrations in future

---
+++ initat/cluster/update_test_migrations.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mv ./backbone/migrations ./backbone/old_migrations
+
+mv ./backbone/test_migrations/ ./backbone/migrations
+
+rm ./backbone/migrations/0001_initial.py*
+
+./manage.py makemigrations
+
+mv ./backbone/migrations ./backbone/test_migrations
+
+mv ./backbone/old_migrations ./backbone/migrations
