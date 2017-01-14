70ae826c8948a939b38e9030e1f5ee99f4ec05af
Andreas Lang-Nevyjel <lang-nevyjel@init.at>
2017-01-14 12:53:52 +0100

    bugfix

--- initat/tools/pci_database.py
+++ initat/tools/pci_database.py
@@ -39,7 +39,7 @@ def get_pci_dicts(fname=None):
     for search_name in search_names:
         if os.path.isfile(search_name):
             with open(search_name, "r") as file_:
-                lines = file_.read().split('\n')
+                lines = file_.read().decode("utf-8").split('\n')
                 break
     classline = re.compile("^C (..)\s+(.*)$")
     subclassline = re.compile("^\t(..)\s+(.*)$")
