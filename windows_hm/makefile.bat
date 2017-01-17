cea3e90f191ae4e456dc01f59c96c640ddcb592f
kaufmann <g.kaufmann@init.at>
2017-01-17 10:07:36 +0100

    [ICSW-983] icsw-windows-client version bump to 3.0.0 [ICSW-983] enabled check_nt checks for icsw-windows-client

--- windows_hm/makefile.bat
+++ windows_hm/makefile.bat
@@ -1,9 +1,8 @@
-:: SET CPUZ_VERSION=1.77
 SET PCIUTILS_VERSION=3.4.0
-SET NSCP_VERSION=0.5.0.61
+SET NSCP_VERSION=0.5.0.62
 SET WINPYTH_MAJOR=5
 SET WINPYTH_MINOR=2
-SET WINPYTH_MINORFIX=2
+SET WINPYTH_MINORFIX=3
 SET WIX_BIN_PATH=C:\Program Files (x86)\WiX Toolset v3.10\bin\
 
 :: Fetch pciutils and move into script directory
@@ -18,9 +17,6 @@ bin\7z -o.\nscp x nscp-%NSCP_VERSION%-Win32.zip
 bin\wget -nc https://sourceforge.net/projects/winpython/files/WinPython_3.%WINPYTH_MAJOR%/3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%/WinPython-64bit-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%.exe
 bin\wget -nc https://sourceforge.net/projects/winpython/files/WinPython_3.%WINPYTH_MAJOR%/3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%/WinPython-64bit-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%Zero.exe
 
-:: Fetch cpuz
-:: bin\wget -nc http://download.cpuid.com/cpu-z/cpu-z_%CPUZ_VERSION%-en.zip
-:: bin\7z -o.\tmp x cpu-z_%CPUZ_VERSION%-en.zip
 
 bin\7z -o.\tmp\zero x WinPython-64bit-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%Zero.exe
 bin\7z -o.\tmp\full x WinPython-64bit-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%.exe
@@ -37,20 +33,18 @@ COPY .\tmp\full\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages
 
 MOVE .\tmp\full\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages\pythonwin  .\tmp\zero\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages\
 MOVE .\tmp\full\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages\pywin32_system32  .\tmp\zero\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages\
-MOVE .\tmp\full\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages\pywin32-220.dist-info  .\tmp\zero\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages\
 MOVE .\tmp\full\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages\win32  .\tmp\zero\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages\
 MOVE .\tmp\full\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages\win32com  .\tmp\zero\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages\
 MOVE .\tmp\full\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages\win32comext  .\tmp\zero\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages\
 
 
 MOVE .\tmp\zero\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64 .\nscp\python
-COPY .\nscp\python\pywintypes3%WINPYTH_MAJOR%.dll .\nscp\
-COPY .\scripts\* .\nscp\scripts\python\
-COPY .\scripts\finalize-install.py .\nscp\
-COPY .\nsclient.ini .\nscp\
+XCOPY .\nscp\python\pywintypes3%WINPYTH_MAJOR%.dll .\nscp\
+XCOPY .\scripts\* .\nscp\scripts\python\
+XCOPY .\scripts\finalize-install.py .\nscp\
+XCOPY .\nsclient.ini .\nscp\
 
 MOVE .\tmp\pciutils-%PCIUTILS_VERSION%-win32 .\nscp\scripts\python\pciutils
-MOVE .\tmp\cpuz_x64.exe .\nscp\scripts\python\
 
 SET nscp_path=nscp
 "%WIX_BIN_PATH%heat.exe" dir nscp -cg NscpFiles -dr INSTALLDIR -gg -scom -sreg -sfrag -srd -var env.nscp_path -out "Components.wxs"
