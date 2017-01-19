de4621da69af1ef99f333adb931c063abe6227a8
kaufmann <g.kaufmann@init.at>
2017-01-19 13:26:04 +0100

    service installer added

--- windows_hm/makefile_old.bat
+++ windows_hm/makefile_old.bat
@@ -1,14 +1,19 @@
 SET PCIUTILS_VERSION=3.4.0
+SET NSCP_VERSION=0.5.0.62
 SET WINPYTH_MAJOR=5
 SET WINPYTH_MINOR=2
 SET WINPYTH_MINORFIX=3
 SET WIX_BIN_PATH=C:\Program Files (x86)\WiX Toolset v3.10\bin\
 
 :: Fetch pciutils and move into script directory
-:: bin\wget -nc https://eternallybored.org/misc/pciutils/releases/pciutils-%PCIUTILS_VERSION%-win32.zip
-:: bin\7z -o.\tmp\ x pciutils-%PCIUTILS_VERSION%-win32.zip
+bin\wget -nc https://eternallybored.org/misc/pciutils/releases/pciutils-%PCIUTILS_VERSION%-win32.zip
+bin\7z -o.\tmp\ x pciutils-%PCIUTILS_VERSION%-win32.zip
 
-:: Fetch full and zero version of (portable) python
+:: Fetch nscp client
+bin\wget -nc https://github.com/mickem/nscp/releases/download/%NSCP_VERSION%/nscp-%NSCP_VERSION%-Win32.zip
+bin\7z -o.\nscp x nscp-%NSCP_VERSION%-Win32.zip
+
+:: Fetch full and zero versionf of (portable) python
 bin\wget -nc https://sourceforge.net/projects/winpython/files/WinPython_3.%WINPYTH_MAJOR%/3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%/WinPython-64bit-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%.exe
 bin\wget -nc https://sourceforge.net/projects/winpython/files/WinPython_3.%WINPYTH_MAJOR%/3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%/WinPython-64bit-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%Zero.exe
 
@@ -33,49 +38,20 @@ MOVE .\tmp\full\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages
 MOVE .\tmp\full\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages\win32comext  .\tmp\zero\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64\Lib\site-packages\
 
 
-MOVE .\tmp\zero\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64 .\host_monitor_windows
-.\host_monitor_windows\python.exe -m pip install --upgrade pip
-.\host_monitor_windows\python.exe -m pip install lxml
-.\host_monitor_windows\python.exe -m pip install python-memcached
-.\host_monitor_windows\python.exe -m pip install netifaces
-.\host_monitor_windows\python.exe -m pip install setproctitle
-.\host_monitor_windows\python.exe -m pip install zmq
-.\host_monitor_windows\python.exe -m pip install psutil
+MOVE .\tmp\zero\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.amd64 .\nscp\python
+XCOPY .\nscp\python\pywintypes3%WINPYTH_MAJOR%.dll .\nscp\
+XCOPY .\scripts\* .\nscp\scripts\python\
+XCOPY .\scripts\finalize-install.py .\nscp\
+XCOPY .\nsclient.ini .\nscp\
 
-XCOPY .\syslog.py .\host_monitor_windows\Lib\site-packages\
-
-MKDIR .\host_monitor_windows\Lib\site-packages\initat
-MKDIR .\host_monitor_windows\Lib\site-packages\initat\host_monitoring
-MKDIR .\host_monitor_windows\Lib\site-packages\initat\tools
-MKDIR .\host_monitor_windows\Lib\site-packages\initat\icsw
-MKDIR .\host_monitor_windows\Lib\site-packages\opt
-XCOPY ..\initat\icsw .\host_monitor_windows\Lib\site-packages\initat\icsw\ /E
-XCOPY ..\initat\host_monitoring .\host_monitor_windows\Lib\site-packages\initat\host_monitoring\ /E
-XCOPY ..\initat\tools .\host_monitor_windows\Lib\site-packages\initat\tools\ /E
-XCOPY ..\initat\*.py .\host_monitor_windows\Lib\site-packages\initat\
-XCOPY ..\opt .\host_monitor_windows\Lib\site-packages\opt /E 
-
-RMDIR /s /q .\host_monitor_windows\tcl
-RMDIR /s /q .\host_monitor_windows\Tools
-RMDIR /s /q .\host_monitor_windows\Doc
-
-XCOPY .\bin\nssm.exe .\host_monitor_windows\
-
-:: XCOPY .\nscp\python\pywintypes3%WINPYTH_MAJOR%.dll .\nscp\
-:: XCOPY .\scripts\* .\nscp\scripts\python\
-:: XCOPY .\scripts\finalize-install.py .\nscp\
-:: XCOPY .\nsclient.ini .\nscp\
-
-:: MOVE .\tmp\pciutils-%PCIUTILS_VERSION%-win32 .\nscp\scripts\python\pciutils
-
-MOVE .\host_monitor_windows .\nscp
+MOVE .\tmp\pciutils-%PCIUTILS_VERSION%-win32 .\nscp\scripts\python\pciutils
 
 SET nscp_path=nscp
 "%WIX_BIN_PATH%heat.exe" dir nscp -cg NscpFiles -dr INSTALLDIR -gg -scom -sreg -sfrag -srd -var env.nscp_path -out "Components.wxs"
 "%WIX_BIN_PATH%candle.exe" ICSW_Windows_Client.wxs
 "%WIX_BIN_PATH%candle.exe" Components.wxs
-::"%WIX_BIN_PATH%candle.exe" HostEditingDlg.wxs
-"%WIX_BIN_PATH%light.exe" -ext WixUIExtension -dWixUILicenseRtf=legal_text.rtf -dWixUIBannerBmp=WixUIBannerBmp.bmp -dWixUIExclamationIco=WixUIExclamationIco.ico -dWixUIDialogBmp=WixUIDialogBmp.bmp ICSW_Windows_Client.wixobj Components.wixobj -o ICSW_Windows_Client.msi
+"%WIX_BIN_PATH%candle.exe" HostEditingDlg.wxs
+"%WIX_BIN_PATH%light.exe" -ext WixUIExtension -dWixUILicenseRtf=legal_text.rtf -dWixUIBannerBmp=WixUIBannerBmp.bmp -dWixUIExclamationIco=WixUIExclamationIco.ico -dWixUIDialogBmp=WixUIDialogBmp.bmp ICSW_Windows_Client.wixobj Components.wixobj HostEditingDlg.wixobj -o ICSW_Windows_Client.msi
 
 :: Cleanup temporary files
 RMDIR /s /q .\tmp
