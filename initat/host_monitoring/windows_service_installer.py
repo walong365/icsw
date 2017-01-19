0162627b6333f11cc07b6e6de8022d81cde37c45
kaufmann <kaufmann@init.at>
2017-01-19 11:00:51 +0100

    [ICSW-984] service handler added

--- initat/host_monitoring/windows_service_installer.py
+++ initat/host_monitoring/windows_service_installer.py
@@ -1,61 +1,53 @@
-import win32service
 import win32serviceutil
+import win32service
 import win32event
+import servicemanager
+import socket
+import time
+import logging
 
-from initat.tools import configfile, process_tools
-from initat.host_monitoring.server import ServerCode
-from initat.client_version import VERSION_STRING
+logging.basicConfig(
+    filename='c:\\Temp\\hello-service.log',
+    level=logging.DEBUG,
+    format='[helloworld-service] %(levelname)-7.7s %(message)s'
+)
 
-class PySvc(win32serviceutil.ServiceFramework):
-    # you can NET START/STOP the service by the following name
-    _svc_name_ = "NoctuaHostMonitoring"
-    # this text shows up as the service name in the Service
-    # Control Manager (SCM)
-    _svc_display_name_ = "Noctua Host Monitoring"
-    # this text shows up as the description in the SCM
-    _svc_description_ = "Noctua Host Monitoring Service"
+
+class HelloWorldSvc(win32serviceutil.ServiceFramework):
+    _svc_name_ = "HelloWorld-Service"
+    _svc_display_name_ = "HelloWorld Service"
 
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
-        # create an event to listen for stop requests on
-        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
-
-        # core logic of the service
-
-    def SvcDoRun(self):
-        global_config = configfile.get_global_config(
-            process_tools.get_programm_name(),
-            single_process_mode=True
-        )
-        prog_name = "collserver"
-        global_config.add_config_entries(
-            [
-                ("DEBUG", configfile.bool_c_var(False, help_string="enable debug mode [%(default)s]", short_options="d",
-                    only_commandline=True)),
-                ("VERBOSE", configfile.int_c_var(0, help_string="set verbose level [%(default)d]", short_options="v",
-                    only_commandline=True)),
-            ]
-        )
-        options = global_config.handle_commandline(
-            description="{}, version is {}".format(
-                prog_name,
-                VERSION_STRING
-            ),
-            positional_arguments=prog_name in ["collclient"],
-            partial=prog_name in ["collclient"],
-        )
-
-        ret_state = ServerCode(global_config).loop()
-        return ret_state
-
-        # called when we're being shut down
+        self.stop_event = win32event.CreateEvent(None, 0, 0, None)
+        socket.setdefaulttimeout(60)
+        self.stop_requested = False
 
     def SvcStop(self):
-        # tell the SCM we're shutting down
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-        # fire the stop event
-        win32event.SetEvent(self.hWaitStop)
+        win32event.SetEvent(self.stop_event)
+        logging.info('Stopping service ...')
+        self.stop_requested = True
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(
+            servicemanager.EVENTLOG_INFORMATION_TYPE,
+            servicemanager.PYS_SERVICE_STARTED,
+            (self._svc_name_, '')
+        )
+        self.main()
+
+    def main(self):
+        logging.info(' ** Hello PyWin32 World ** ')
+        # Simulate a main loop
+        for i in range(0, 50):
+            if self.stop_requested:
+                logging.info('A stop signal was received: Breaking main loop ...')
+                break
+            time.sleep(5)
+            logging.info("Hello at %s" % time.ctime())
+        return
 
 
 if __name__ == '__main__':
-    win32serviceutil.HandleCommandLine(PySvc)
+    win32serviceutil.HandleCommandLine(HelloWorldSvc)
