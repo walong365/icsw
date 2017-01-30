db9e042577d3746414de26de0fc9a4ac45b988d2
kaufmann <kaufmann@init.at>
2017-01-30 10:48:26 +0100

    pep8 fixes

--- windows_hm/service_manager.py
+++ windows_hm/service_manager.py
@@ -7,6 +7,7 @@ NSSM_BIN = "nssm.exe"
 
 SERVICE_STOPPED = b"S\x00E\x00R\x00V\x00I\x00C\x00E\x00_\x00S\x00T\x00O\x00P\x00P\x00E\x00D\x00\r\x00\n\x00"
 
+
 def cleanup_service(service_name):
     # stop service, if running
     try:
@@ -30,6 +31,7 @@ def cleanup_service(service_name):
     except subprocess.CalledProcessError:
         pass
 
+
 def install_service(service_name):
     cleanup_service(service_name)
 
@@ -40,13 +42,20 @@ def install_service(service_name):
     subprocess.check_output([NSSM_BIN, "install", service_name, python_path, module_path], stderr=subprocess.STDOUT)
 
     # set various logging settings
-    subprocess.check_output([NSSM_BIN, "set", service_name, "AppStdout", os.path.join(os.getcwd(), "log.txt")], stderr=subprocess.STDOUT)
-    subprocess.check_output([NSSM_BIN, "set", service_name, "AppStderr", os.path.join(os.getcwd(), "log.txt")], stderr=subprocess.STDOUT)
-    subprocess.check_output([NSSM_BIN, "set", service_name, "AppStdoutCreationDisposition", "4"], stderr=subprocess.STDOUT)
-    subprocess.check_output([NSSM_BIN, "set", service_name, "AppStderrCreationDisposition", "4"], stderr=subprocess.STDOUT)
-    subprocess.check_output([NSSM_BIN, "set", service_name, "AppRotateFiles", "1"], stderr=subprocess.STDOUT)
-    subprocess.check_output([NSSM_BIN, "set", service_name, "AppRotateOnline", "1"], stderr=subprocess.STDOUT)
-    subprocess.check_output([NSSM_BIN, "set", service_name, "AppRotateBytes", "1000000"], stderr=subprocess.STDOUT)
+    subprocess.check_output([NSSM_BIN, "set", service_name, "AppStdout", os.path.join(os.getcwd(), "log.txt")],
+                            stderr=subprocess.STDOUT)
+    subprocess.check_output([NSSM_BIN, "set", service_name, "AppStderr", os.path.join(os.getcwd(), "log.txt")],
+                            stderr=subprocess.STDOUT)
+    subprocess.check_output([NSSM_BIN, "set", service_name, "AppStdoutCreationDisposition", "4"],
+                            stderr=subprocess.STDOUT)
+    subprocess.check_output([NSSM_BIN, "set", service_name, "AppStderrCreationDisposition", "4"],
+                            stderr=subprocess.STDOUT)
+    subprocess.check_output([NSSM_BIN, "set", service_name, "AppRotateFiles", "1"],
+                            stderr=subprocess.STDOUT)
+    subprocess.check_output([NSSM_BIN, "set", service_name, "AppRotateOnline", "1"],
+                            stderr=subprocess.STDOUT)
+    subprocess.check_output([NSSM_BIN, "set", service_name, "AppRotateBytes", "1000000"],
+                            stderr=subprocess.STDOUT)
     
     try:
         subprocess.check_output([NSSM_BIN, "start", service_name], stderr=subprocess.STDOUT)
@@ -54,12 +63,12 @@ def install_service(service_name):
         pass
     
 if __name__ == "__main__":
-    service_name = sys.argv[1]
+    _service_name = sys.argv[1]
     install = sys.argv[2] == "install"
 
     if install:
-        install_service(service_name)
+        install_service(_service_name)
     else:
-        cleanup_service(service_name)
+        cleanup_service(_service_name)
 
     sys.exit(0)
