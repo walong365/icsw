9e85904d978d65301e5628bfd76aba6bfc0599d1
kaufmann <kaufmann@init.at>
2017-03-08 14:16:17 +0100

    [ICSW-1106] windows_hm, windows_remote_viewer makefile fixes

--- windows/windows_hm/makefile_32bit.bat
+++ windows/windows_hm/makefile_32bit.bat
@@ -7,12 +7,12 @@ SET WINPYTH_MINORFIX=1
 SET WIX_BIN_PATH=C:\Program Files (x86)\WiX Toolset v3.10\bin\
 
 :: Fetch pciutils and move into script directory
-bin\wget --no-check-certificate -nc https://eternallybored.org/misc/pciutils/releases/pciutils-%PCIUTILS_VERSION%-win32.zip
-bin\7z -o.\tmp\ x pciutils-%PCIUTILS_VERSION%-win32.zip
+..\windows_common_files\bin\wget --no-check-certificate -nc https://eternallybored.org/misc/pciutils/releases/pciutils-%PCIUTILS_VERSION%-win32.zip
+..\windows_common_files\bin\7z -o.\tmp\ x pciutils-%PCIUTILS_VERSION%-win32.zip
 
 :: Fetch full and zero version of (portable) python
-bin\wget --no-check-certificate -nc https://sourceforge.net/projects/winpython/files/WinPython_3.%WINPYTH_MAJOR%/3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%/WinPython-32bit-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%Zero.exe
-bin\7z -o.\tmp\zero x WinPython-32bit-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%Zero.exe
+..\windows_common_files\bin\wget --no-check-certificate -nc https://sourceforge.net/projects/winpython/files/WinPython_3.%WINPYTH_MAJOR%/3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%/WinPython-32bit-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%Zero.exe
+..\windows_common_files\bin\7z -o.\tmp\zero x WinPython-32bit-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR%.%WINPYTH_MINORFIX%Zero.exe
 
 :: install missing/needed modules
 MOVE .\tmp\zero\python-3.%WINPYTH_MAJOR%.%WINPYTH_MINOR% .\host_monitor_windows
@@ -46,8 +46,8 @@ RMDIR /s /q .\host_monitor_windows\tcl
 RMDIR /s /q .\host_monitor_windows\Tools
 RMDIR /s /q .\host_monitor_windows\Doc
 
-XCOPY .\bin\win32\nssm.exe .\host_monitor_windows\
-XCOPY .\bin\dmidecode212.exe .\host_monitor_windows\
+XCOPY ..\windows_common_files\bin\win32\nssm.exe .\host_monitor_windows\
+XCOPY ..\windows_common_files\bin\dmidecode212.exe .\host_monitor_windows\
 MOVE .\tmp\pciutils-%PCIUTILS_VERSION%-win32 .\host_monitor_windows\pciutils
 type nul > .\host_monitor_windows\hm_icsw_w32
 
--- windows/windows_remote_viewer/makefile.bat
+++ windows/windows_remote_viewer/makefile.bat
@@ -1,4 +1,13 @@
 SET WIX_BIN_PATH=C:\Program Files (x86)\WiX Toolset v3.10\bin\
+SET PYTHON27_PATH=C:\Python27\
+
+:: Generate/fetch required executables
+..\windows_common_files\bin\wget --no-check-certificate -nc https://the.earth.li/~sgtatham/putty/latest/w32/putty.exe
+
+"%PYTHON27_PATHpython.exe" -m pip install pyinstaller
+"%PYTHON27_PATHpython.exe" -m PyInstaller -F -w icsw_remote_viewer_windows.py
+
+MOVE .\dist\icsw_remote_viewer_windows.exe .\
 
 "%WIX_BIN_PATH%candle.exe" ICSW_Remote_Viewer.wxs
 "%WIX_BIN_PATH%light.exe" -ext WixUIExtension -dWixUILicenseRtf=..\windows_common_files\legal_text.rtf -dWixUIBannerBmp=..\windows_common_files\WixUIBannerBmp.bmp -dWixUIExclamationIco=..\windows_common_files\WixUIExclamationIco.ico -dWixUIDialogBmp=..\windows_common_files\WixUIDialogBmp.bmp ICSW_Remote_Viewer.wixobj -o ICSW_Remote_Viewer.msi
@@ -6,5 +15,7 @@ SET WIX_BIN_PATH=C:\Program Files (x86)\WiX Toolset v3.10\bin\
 :: Cleanup temporary files
 DEL .\*.wixobj
 DEL .\*.wixpdb
+RMDIR /s /q .\dist
+RMDIR /s /q .\build
 
 @pause
